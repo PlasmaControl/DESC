01fe4949271fd112417752808c68769cc289f83e
Dario Panici <dpanici@princeton.edu>
2020-12-12 17:42:27 -0500

    Changed continutation.py so a copy of equilibrium is added to equil_fam, not a reference to the equil object

--- desc/continuation.py
+++ desc/continuation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.optimize
 import warnings
+import copy
 
 from desc.backend import jit, use_jax, Timer, TextColors, Tristate
 from desc.backend import jacfwd, grad
@@ -297,7 +298,7 @@ def solve_eq_continuation(inputs, checkpoint_filename=None, device=None):
         timer.stop("Iteration {} solution".format(ii+1))
 
         equil.x = out['x']
-        equil_fam.insert(ii, equil)
+        equil_fam.insert(ii, copy.deepcopy(equil))
 
         if verbose > 1:
             timer.disp("Iteration {} solution".format(ii+1))

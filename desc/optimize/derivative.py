e0cf1454891800996d4bd42afcb97baa6485b6fb
Rory Conlin <roryconlin@gmail.com>
2020-12-28 18:54:31 -0500

    Add optimze module
    - Add fmin_scalar using newton trust region method for minimizing
    scalar functions of 1 or more variables
    - Add utils for general trust region optimization
    - Also added a wrapper class for hessian/jacobian matrices
    - Should be used on top of autodiff or something to do updates,
    efficiently compute some stuff.

---
+++ desc/optimize/derivative.py
@@ -0,0 +1,232 @@
+import numpy as np
+import functools
+from desc.backend import jnp, cho_factor, cho_solve, conditional_decorator, jit, use_jax
+from abc import ABC, abstractmethod
+from .utils import make_spd, chol_U_update
+
+
+class OptimizerDerivative(ABC):
+    """Abstract base class for hessians and jacobians used in the optimizer"""
+    shape = None
+    min_eig = None
+    is_pos_def = None
+    negative_curvature_direction=None
+
+    @abstractmethod
+    def update(self, x_new, x_old, grad_new, grad_old, iteration):
+        """Update the internal matrix A"""
+        pass
+    
+    @abstractmethod
+    def recompute(self,x):
+        """Recompute the full correct internal matrix at the point x"""
+        pass
+    
+    @abstractmethod
+    def get_matrix(self):
+        """Return the internal matrix A"""
+        pass
+    
+    @abstractmethod
+    def get_inverse(self):
+        """Return the inverse of the internal matrix A^-1"""
+        pass
+
+    @abstractmethod
+    def dot(self,x):
+        """Compute dot(A,x)"""
+        pass
+    
+    @abstractmethod
+    def solve(self,b):
+        """Solve A*x = b for x"""
+        pass
+
+    @abstractmethod
+    def get_scale(self):
+        """Compute scaling vector"""
+        pass
+
+        
+class CholeskyHessian(OptimizerDerivative):
+    
+    def __init__(self, n, init_hess='auto', hessfun=None, hessfun_args=(), exception_strategy='damp_update', min_curvature=None, damp_ratio=0.2):
+        self._n = n
+        self.shape = (n,n)
+        self.is_pos_def = True
+        self.min_eig = None
+        self.negative_curvature_direction = None
+        self._damp_ratio = damp_ratio
+        
+        if hessfun is not None:
+            if callable(hessfun):
+                self._hessfun = hessfun
+                self._hessfun_args = hessfun_args
+            else:
+                raise ValueError('hessfun should be callable or None')
+        else:
+            self._hessfun = None
+            self._hessfun_args = ()
+            
+        if exception_strategy == 'skip_update':
+            if min_curvature is not None:
+                self.min_curvature = min_curvature
+            else:
+                self.min_curvature = 1e-8
+        elif exception_strategy == 'damp_update':
+            if min_curvature is not None:
+                self.min_curvature = min_curvature
+            else:
+                self.min_curvature = 0.2
+        else:
+            raise ValueError("`exception_strategy` must be 'skip_update' "
+                             "or 'damp_update'.")   
+        self.exception_strategy = exception_strategy
+        
+        
+        if init_hess is None and hessfun is None:
+            self._U = np.eye(n)
+            self._initialized = True
+            self._initialization = 'eye'
+        elif init_hess is None and hessfun is not None:
+            self._U = np.eye(n)
+            self._initialized = False
+            self._initialization = 'hessfun'
+        elif isinstance(init_hess,str) and init_hess=='auto':
+            self._U = np.eye(n)
+            self._initialized = False
+            self._initialization = 'auto'
+        elif isinstance(init_hess, str): 
+            raise ValueError('unknown hessian initialization')
+        else:
+            init_hess = make_spd(init_hess, delta=self.min_curvature, tol=0.1)
+            self._U = self._cholesky(init_hess).T
+            self._initialized = True
+            self._initialization = 'init_hess'
+        
+
+            
+    def _auto_scale_init(self, delta_x, delta_grad):
+        # Heuristic to scale matrix at first iteration.
+        # Described in Nocedal and Wright "Numerical Optimization"
+        # p.143 formula (6.20).
+        s_norm2 = np.dot(delta_x, delta_x)
+        y_norm2 = np.dot(delta_grad, delta_grad)
+        ys = np.abs(np.dot(delta_grad, delta_x))
+        if ys == 0.0 or y_norm2 == 0 or s_norm2 == 0:
+            scale = 1
+        else:
+            scale = y_norm2 / ys    
+        self._U = self._U * np.sqrt(scale)
+        self._initialized = True
+        
+    def recompute(self,x):
+        H = self._hessfun(x,*self._hessfun_args)
+        H = make_spd(H, delta=self.min_curvature, tol=0.1)
+        U = self._cholesky(H)
+        self._U = U #self._damp_ratio*self._U + (1-self._damp_ratio)*U
+        
+
+    def update(self, x_new, x_old, grad_new, grad_old):
+        """Update internal matrix.
+        Update Hessian matrix or its inverse (depending on how 'approx_type'
+        is defined) using information about the last evaluated points.
+        Parameters
+        ----------
+        delta_x : ndarray
+            The difference between two points the gradient
+            function have been evaluated at: ``delta_x = x2 - x1``.
+        delta_grad : ndarray
+            The difference between the gradients:
+            ``delta_grad = grad(x2) - grad(x1)``.
+        """
+        x_new = np.asarray(x_new)
+        x_old = np.asarray(x_old)
+        grad_new = np.asarray(grad_new)
+        grad_old = np.asarray(grad_old)
+        
+        delta_x = x_new - x_old
+        delta_grad = grad_new - grad_old
+        
+        if np.all(delta_x == 0.0):
+            return
+        if np.all(delta_grad == 0.0):
+            return
+        if not self._initialized:
+            if self._initialization == 'auto':
+                self._auto_scale_init(delta_x, delta_grad)
+            elif self._initialization == 'hessfun':
+                self.recompute(x_new)
+        
+        self._bfgs_update(delta_x,delta_grad)
+        
+    def _bfgs_update(self,delta_x, delta_grad):
+            
+        if np.all(delta_x == 0.0):
+            return
+        if np.all(delta_grad == 0.0):
+            return
+        
+        s = delta_x
+        y = delta_grad
+
+        # Do some common operations
+        sy = np.dot(s, y)
+        Bs = self.dot(s)
+        sBs = Bs.dot(s)
+
+        # Check if curvature condition is violated
+        if sy <= self.min_curvature * sBs:
+
+            if self.exception_strategy == 'skip_update':
+                return
+            # interpolate between the actual BFGS
+            # result and the unmodified matrix.
+            elif self.exception_strategy == 'damp_update':
+                update_factor = (1-self.min_curvature) / (1 - sy/sBs)
+                y = update_factor*y + (1-update_factor)*Bs
+                sy = np.dot(s, y)
+ 
+        u = np.asarray(y)
+        v = np.asarray(Bs)
+        alpha = np.asarray(1/sy)
+        beta = np.asarray(-1/sBs)
+        
+        self._U = chol_U_update(np.asarray(self._U), u, alpha)
+        self._U = chol_U_update(np.asarray(self._U), v, beta)
+    
+    def get_matrix(self):
+        return jnp.dot(self._U.T,self._U)
+    
+    def get_inverse(self):
+        return cho_solve((self._U, False), jnp.eye(self._n))
+    
+    def dot(self,x):
+        return self._dot(self._U.T, self._dot(self._U, x))
+    
+    def solve(self,x):
+        return self._cho_solve(self._U, x)
+    
+    @conditional_decorator(functools.partial(jit, static_argnums=(0,)), use_jax)
+    def _dot(self, A, b):
+        return jnp.dot(A,b)
+    
+    @conditional_decorator(functools.partial(jit, static_argnums=(0,)), use_jax)    
+    def _cho_solve(self, U, x):
+        return cho_solve((U, False), x)
+
+    @conditional_decorator(functools.partial(jit, static_argnums=(0,)), use_jax) 
+    def _cholesky(self, A):
+        return jnp.linalg.cholesky(A).T
+    
+    def get_scale(self):
+
+        return compute_jac_scale(self._U)        
+
+
+def compute_jac_scale(A):
+    d = jnp.sum(A**2, axis=0)**0.5
+    d = jnp.where(d == 0, 1, d)
+
+    return 1/d, d 
+        
---
+++ desc/optimize/tr_subproblems.py
@@ -0,0 +1,314 @@
+import numpy as np
+from desc.backend import jnp, cho_factor, cho_solve, qr
+
+def solve_trust_region_dogleg(g, hess, scale, trust_radius):
+    """
+    Solve trust region subproblem the dog-leg method.
+    
+    Parameters
+    ----------
+    g : ndarray
+        gradient of objective function
+    hess : Hessian
+        Hessian with dot and solve methods
+    scale : ndarray
+        scaling array for gradient and hessian
+    trust_radius : float
+        We are allowed to wander only this far away from the origin.
+        
+    Returns
+    -------
+    p : ndarray
+        The proposed step.
+    hits_boundary : bool
+        True if the proposed step is on the boundary of the trust region.
+        
+    Notes
+    -----
+    The Hessian is required to be positive definite.
+
+    """
+
+    # This is the optimum for the quadratic model function.
+    # If it is inside the trust radius then return this point.
+    p_newton = -1/scale*hess.solve(g)
+    if jnp.linalg.norm(p_newton) < trust_radius:
+        hits_boundary = False
+        return p_newton, hits_boundary
+
+    # This is the predicted optimum along the direction of steepest descent.
+    Bg = hess.dot(scale**2 * g)
+    p_cauchy = -(jnp.dot(scale*g, scale*g) / jnp.dot(scale*g, scale*Bg)) * scale * g
+
+    # If the Cauchy point is outside the trust region,
+    # then return the point where the path intersects the boundary.
+    p_cauchy_norm = jnp.linalg.norm(p_cauchy)
+    if p_cauchy_norm >= trust_radius:
+        p_boundary = p_cauchy * (trust_radius / p_cauchy_norm)
+        hits_boundary = True
+        return p_boundary, hits_boundary
+
+    # Compute the intersection of the trust region boundary
+    # and the line segment connecting the Cauchy and Newton points.
+    # This requires solving a quadratic equation.
+    # ||p_u + t*(p_best - p_u)||**2 == trust_radius**2
+    # Solve this for positive time t using the quadratic formula.
+    delta = p_newton - p_cauchy
+    _, tb = get_boundaries_intersections(p_cauchy, delta,
+                                              trust_radius)
+    p_boundary = p_cauchy + tb * delta
+    hits_boundary = True
+
+    return p_boundary, hits_boundary
+      
+
+def solve_trust_region_2d_subspace(grad, hess, scale, trust_radius, verbose=0):
+    """Solve a trust region problem over the 2d subspace spanned by the gradient
+    and Newton direction
+    
+    Parameters
+    ----------
+    grad : ndarray
+        gradient of objective function
+    hess : OptimizerDerivative
+        hessian of objective function
+    scale : ndarray
+        scaling array for gradient and hessian
+    trust_radius : float
+        We are allowed to wander only this far away from the origin.
+
+    Returns
+    -------
+    p : ndarray
+        The proposed step.
+    hits_boundary : bool
+        True if the proposed step is on the boundary of the trust region.
+
+    """
+    
+    if hess.is_pos_def:
+        p_newton = -1/scale*hess.solve(grad)
+    else:
+        p_newton = hess.negative_curvature_direction
+    S = np.vstack([grad,p_newton]).T
+    S, _ = qr(S,mode='economic')
+    g = S.T.dot(scale * grad)
+    B = S.T.dot(scale * hess.dot(scale * S))
+    
+    # B = [a b]  g = [d f]
+    #     [b c]  q = [x y]
+    # p = Sq
+    
+    try:
+        R, lower = cho_factor(B)
+        q = -cho_solve((R, lower), g)
+        if np.dot(q, q) <= trust_radius**2:
+            return S.dot(q), True
+    except LinAlgError:
+        pass
+ 
+    a = B[0, 0] * trust_radius ** 2
+    b = B[0, 1] * trust_radius ** 2
+    c = B[1, 1] * trust_radius ** 2
+ 
+    d = g[0] * trust_radius
+    f = g[1] * trust_radius
+ 
+    coeffs = np.array([-b + d, 2 * (a - c + f), 6 * b, 2 * (-a + c + f), -b - d])
+    t = np.roots(coeffs)  # Can handle leading zeros.
+    t = np.real(t[np.isreal(t)])
+ 
+    q = trust_radius * np.vstack((2 * t / (1 + t**2), (1 - t**2) / (1 + t**2)))
+    value = 0.5 * np.sum(q * B.dot(q), axis=0) + np.dot(g, q)
+    i = np.argmin(value)
+    q = q[:, i]
+    p = S.dot(q)
+    
+    return p, False
+
+
+def solve_lsq_trust_region(n, m, uf, s, V, trust_radius, initial_alpha=None,
+                           rtol=0.01, max_iter=10):
+    """Solve a trust-region problem arising in least-squares minimization.
+    This function implements a method described by J. J. More [1]_ and used
+    in MINPACK, but it relies on a single SVD of Jacobian instead of series
+    of Cholesky decompositions. Before running this function, compute:
+    ``U, s, VT = svd(J, full_matrices=False)``.
+    Parameters
+    ----------
+    n : int
+        Number of variables.
+    m : int
+        Number of residuals.
+    uf : ndarray
+        Computed as U.T.dot(f).
+    s : ndarray
+        Singular values of J.
+    V : ndarray
+        Transpose of VT.
+    trust_radius : float
+        Radius of a trust region.
+    initial_alpha : float, optional
+        Initial guess for alpha, which might be available from a previous
+        iteration. If None, determined automatically.
+    rtol : float, optional
+        Stopping tolerance for the root-finding procedure. Namely, the
+        solution ``p`` will satisfy ``abs(norm(p) - trust_radius) < rtol * trust_radius``.
+    max_iter : int, optional
+        Maximum allowed number of iterations for the root-finding procedure.
+    Returns
+    -------
+    p : ndarray, shape (n,)
+        Found solution of a trust-region problem.
+    alpha : float
+        Positive value such that (J.T*J + alpha*I)*p = -J.T*f.
+        Sometimes called Levenberg-Marquardt parameter.
+    n_iter : int
+        Number of iterations made by root-finding procedure. Zero means
+        that Gauss-Newton step was selected as the solution.
+    References
+    ----------
+    .. [1] More, J. J., "The Levenberg-Marquardt Algorithm: Implementation
+           and Theory," Numerical Analysis, ed. G. A. Watson, Lecture Notes
+           in Mathematics 630, Springer Verlag, pp. 105-116, 1977.
+    """
+    def phi_and_derivative(alpha, suf, s, trust_radius):
+        """Function of which to find zero.
+        It is defined as "norm of regularized (by alpha) least-squares
+        solution minus `trust_radius`". Refer to [1]_.
+        """
+        denom = s**2 + alpha
+        p_norm = np.linalg.norm(suf / denom)
+        phi = p_norm - trust_radius
+        phi_prime = -np.sum(suf ** 2 / denom**3) / p_norm
+        return phi, phi_prime
+
+    suf = s * uf
+
+    # Check if J has full rank and try Gauss-Newton step.
+    threshold = EPS * m * s[0]
+    full_rank = s[-1] > threshold
+    if full_rank:
+        p = -V.dot(uf / s)
+        if norm(p) <= trust_radius:
+            return p, 0.0, 0
+
+    alpha_upper = norm(suf) / trust_radius
+
+    if full_rank:
+        phi, phi_prime = phi_and_derivative(0.0, suf, s, trust_radius)
+        alpha_lower = -phi / phi_prime
+    else:
+        alpha_lower = 0.0
+
+    if initial_alpha is None or not full_rank and initial_alpha == 0:
+        alpha = max(0.001 * alpha_upper, (alpha_lower * alpha_upper)**0.5)
+    else:
+        alpha = initial_alpha
+
+    for it in range(max_iter):
+        if alpha < alpha_lower or alpha > alpha_upper:
+            alpha = max(0.001 * alpha_upper, (alpha_lower * alpha_upper)**0.5)
+
+        phi, phi_prime = phi_and_derivative(alpha, suf, s, trust_radius)
+
+        if phi < 0:
+            alpha_upper = alpha
+
+        ratio = phi / phi_prime
+        alpha_lower = max(alpha_lower, alpha - ratio)
+        alpha -= (phi + trust_radius) * ratio / trust_radius
+
+        if np.abs(phi) < rtol * trust_radius:
+            break
+
+    p = -V.dot(suf / (s**2 + alpha))
+
+    # Make the norm of p equal to trust_radius, p is changed only slightly during
+    # this. It is done to prevent p lie outside the trust region (which can
+    # cause problems later).
+    p *= trust_radius / norm(p)
+
+    return p, alpha, it + 1
+
+
+def update_tr_radius(trust_radius, actual_reduction, predicted_reduction,
+                     step_norm, bound_hit, max_tr=np.inf, min_tr=0, 
+                     increase_threshold=0.75, increase_ratio=2, 
+                     decrease_threshold=0.25, decrease_ratio=0.25,
+                     ga_ratio=0, ga_accept_threshold=1):
+    """Update the radius of a trust region based on the cost reduction.
+    
+    Parameters
+    ----------
+    trust_radius : float
+        current trust region radius
+    actual_reduction : float
+        actual cost reduction from the proposed step
+    predicted_reduction : float
+        cost reduction predicted by quadratic model
+    step_norm : float
+        size of the proposed step
+    bound_hit : bool
+        whether the current step hits the trust region bound
+    max_tr : float
+        maximum allowed trust region radius
+    min_tr : float
+        minimum allowed trust region radius
+    increase_threshold, increase_ratio : float
+        if ratio > inrease_threshold, trust radius is increased by a factor of increase_ratio
+    decrease_threshold, decrease_ratio : float
+        if ratio < decrease_threshold, trust radius is decreased by a factor of decrease_ratio
+    ga_ratio : float
+        ratio of geodesic acceleration step size to original step size
+    ga_accept_threshold: float
+        only accept step if ga_ratio < ga_accept_threshold
+    
+    
+    Returns
+    -------
+    trust_radius : float
+        New radius.
+    ratio : float
+        Ratio between actual and predicted reductions.
+    """
+    if predicted_reduction > 0:
+        ratio = actual_reduction / predicted_reduction
+    elif predicted_reduction == actual_reduction == 0:
+        ratio = 1
+    else:
+        ratio = 0
+
+    if ratio < decrease_threshold:
+        trust_radius = decrease_ratio * step_norm
+    elif ratio > increase_threshold and bound_hit:
+        trust_radius *= increase_ratio
+    
+    trust_radius = np.clip(trust_radius, min_tr, max_tr)
+
+    return trust_radius, ratio
+
+
+
+def get_boundaries_intersections(z, d, trust_radius):
+    """
+    Solve the scalar quadratic equation ||z + t d|| == trust_radius.
+    This is like a line-sphere intersection.
+    Return the two values of t, sorted from low to high.
+    """
+    a = jnp.dot(d, d)
+    b = 2 * jnp.dot(z, d)
+    c = jnp.dot(z, z) - trust_radius**2
+    sqrt_discriminant = jnp.sqrt(b*b - 4*a*c)
+
+    # The following calculation is mathematically
+    # equivalent to:
+    # ta = (-b - sqrt_discriminant) / (2*a)
+    # tb = (-b + sqrt_discriminant) / (2*a)
+    # but produce smaller round off errors.
+    # Look at Matrix Computation p.97
+    # for a better justification.
+    aux = b + jnp.copysign(sqrt_discriminant, b)
+    ta = -aux / (2*a)
+    tb = -2*c / aux
+    return np.sort(np.array([ta, tb]))

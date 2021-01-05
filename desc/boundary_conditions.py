00b7af6823355adfa0ce427b7e1a20faa5f89bbd
Rory Conlin <roryconlin@gmail.com>
2021-01-05 00:58:55 -0500

    Add functions and class for enforcing boundary conditions
    -BC are linear equality constraints, so use LinearEqualityConstraint.
    -separate functions for computing matrices for axis BC, last closed
    flux surface BC, and lambda gauge freedom.

--- desc/boundary_conditions.py
+++ desc/boundary_conditions.py
@@ -3,8 +3,268 @@ import numpy as np
 from desc.backend import jnp
 from desc.utils import Tristate
 from desc.grid import Grid
-from desc.basis import DoubleFourierSeries
+from desc.basis import DoubleFourierSeries, jacobi_coeffs
 from desc.transform import Transform
+from desc.optimize.constraint import LinearEqualityConstraint
+
+
+class BoundaryConstraint(LinearEqualityConstraint):
+    """Linear equality constraint for boundary conditions and gauge freedom
+
+    enforces:
+    r(0,theta,zeta) == 0
+    lambda(0,theta,zeta) == 0
+    R0 + r(1,theta,zeta)*cos(theta) == Rb
+    Z0 + r(1,theta,zeta)*sin(theta) == Zb
+    lambda(rho,0,0) == 0
+
+    Parameters
+    ----------
+    R0_basis : Basis
+        Fourier basis for R0
+    Z0_basis : Basis
+        Fourier basis for Z0
+    r_basis : Basis
+        Fourier-Zernike basis for r
+    l_basis : Basis
+        Fourier-Zernike basis for lambda
+    Rb_basis : Basis
+        Double Fourier basis for boundary R
+    Zb_basis : Basis
+        Double Fourier basis for boundary Z
+    cRb : ndarray
+        Array of spectral coefficients for boundary R
+    cZb : ndarray
+        Array of spectral coefficients for boundary Z
+    x0 : ndarray, optional
+        particular solution for Ax=b. If not supplied it will
+        be calculated using the least norm solution of the constraint.
+
+    """
+
+    def __init__(self, R0_basis, Z0_basis, r_basis, l_basis,
+                 Rb_basis, Zb_basis, cRb, cZb, x0=None):
+
+        Aaxis, baxis = get_axis_bc_matrices(
+            R0_basis, Z0_basis, r_basis, l_basis)
+        Alcfs, blcfs = get_lcfs_bc_matrices(
+            R0_basis, Z0_basis, r_basis, l_basis, Rb_basis, Zb_basis, cRb, cZb)
+        Agauge, bgauge = get_gauge_bc_matrices(
+            R0_basis, Z0_basis, r_basis, l_basis)
+
+        A = np.vstack([Aaxis, Alcfs, Agauge])
+        b = np.vstack([baxis, blcfs, bgauge])
+
+        super().__init__(A, b)
+
+
+def get_gauge_bc_matrices(R0_basis, Z0_basis, r_basis, l_basis):
+    """Compute constraint matrices for gauge freedom of lambda
+
+    enforces lambda(rho,0,0) == 0
+
+    Parameters
+    ----------
+    R0_basis : Basis
+        Fourier basis for R0
+    Z0_basis : Basis
+        Fourier basis for Z0
+    r_basis : Basis
+        Fourier-Zernike basis for r
+    l_basis : Basis
+        Fourier-Zernike basis for lambda
+
+    Returns
+    -------
+    A, b : ndarray
+        Constraint matrix and vector such that the constraint is satisfied
+        if and only if Ax=b
+    """
+    R0_modes = R0_basis.modes
+    Z0_modes = Z0_basis.modes
+    r_modes = r_basis.modes
+    l_modes = l_basis.modes
+    dim_R0 = len(R0_modes)
+    dim_Z0 = len(Z0_modes)
+    dim_r = len(r_modes)
+    dim_l = len(l_modes)
+    dimx = dim_R0 + dim_Z0 + dim_r + dim_l
+
+    mnpos = np.where(np.logical_and(l_modes[:, 1] >= 0, l_modes[:, 2] >= 0))[0]
+    l_lmn = l_modes[mnpos, :]
+    if len(l_lmn) > 0:
+        c = jacobi_coeffs(l_lmn[:, 0], l_lmn[:, 1])
+    else:
+        c = np.zeros((0, 0))
+
+    A = np.zeros((c.shape[1], dimx))
+    A[:, mnpos+dim_R0 + dim_Z0 + dim_r] = c.T
+    b = np.zeros((c.shape[1], 1))
+
+    return A, b
+
+
+def get_lcfs_bc_matrices(R0_basis, Z0_basis, r_basis, l_basis,
+                         Rb_basis, Zb_basis, cRb, cZb):
+    """Compute constraint matrices for the shape of the last closed flux surface.
+
+    enforces R0 + r(1,theta,zeta)*cos(theta) == Rb and
+    Z0 + r(1,theta,zeta)*sin(theta) == Zb
+
+    Parameters
+    ----------
+    R0_basis : Basis
+        Fourier basis for R0
+    Z0_basis : Basis
+        Fourier basis for Z0
+    r_basis : Basis
+        Fourier-Zernike basis for r
+    l_basis : Basis
+        Fourier-Zernike basis for lambda
+    Rb_basis : Basis
+        Double Fourier basis for boundary R
+    Zb_basis : Basis
+        Double Fourier basis for boundary Z
+    cRb : ndarray
+        Array of spectral coefficients for boundary R
+    cZb : ndarray
+        Array of spectral coefficients for boundary Z
+
+    Returns
+    -------
+    A, b : ndarray
+        Constraint matrix and vector such that the constraint is satisfied
+        if and only if Ax=b
+    """
+
+    R0_modes = R0_basis.modes
+    Z0_modes = Z0_basis.modes
+    r_modes = r_basis.modes
+    l_modes = l_basis.modes
+    Rb_modes = Rb_basis.modes
+    Zb_modes = Zb_basis.modes
+
+    dim_R0 = len(R0_modes)
+    dim_Z0 = len(Z0_modes)
+    dim_r = len(r_modes)
+    dim_l = len(l_modes)
+    dimx = dim_R0 + dim_Z0 + dim_r + dim_l
+    dim_Rb = len(Rb_modes)
+    dim_Zb = len(Zb_modes)
+
+    AR = np.zeros((dim_Rb, dimx))
+    AZ = np.zeros((dim_Zb, dimx))
+    bR = cRb.reshape((-1, 1))
+    bZ = cZb.reshape((-1, 1))
+
+    for i, (l, m, n) in enumerate(Rb_modes):
+        if m == 0:
+            j = np.argwhere(R0_modes == n)
+            AR[i, j] = 1
+            j = np.argwhere(np.logical_and(
+                r_modes[:, 1] == 1, r_modes[:, 2] == n)) + dim_R0 + dim_Z0
+            AR[i, j] = 1/2
+        elif abs(m) == 1:
+            j = np.argwhere(np.logical_and(
+                r_modes[:, 1] == 0, r_modes[:, 2] == n)) + dim_R0 + dim_Z0
+            AR[i, j] = 1
+            j = np.argwhere(np.logical_and(
+                r_modes[:, 1] == 1, r_modes[:, 2] == n)) + dim_R0 + dim_Z0
+            AR[i, j] = 1/2
+        else:
+            j = np.argwhere(np.logical_and(
+                r_modes[:, 1] == m+1, r_modes[:, 2] == n)) + dim_R0 + dim_Z0
+            AR[i, j] = 1/2
+            j = np.argwhere(np.logical_and(
+                r_modes[:, 1] == m-1, r_modes[:, 2] == n)) + dim_R0 + dim_Z0
+            AR[i, j] = -1/2
+
+    for i, (l, m, n) in enumerate(Zb_modes):
+        if m == 0:
+            j = np.argwhere(Z0_modes == n) + dim_R0
+            AZ[i, j] = 1
+            j = np.argwhere(np.logical_and(
+                r_modes[:, 1] == -1, r_modes[:, 2] == n)) + dim_R0 + dim_Z0
+            AZ[i, j] = 1/2
+        elif abs(m) == 1:
+            j = np.argwhere(np.logical_and(
+                r_modes[:, 1] == 0, r_modes[:, 2] == n)) + dim_R0 + dim_Z0
+            AZ[i, j] = 1
+            j = np.argwhere(np.logical_and(
+                r_modes[:, 1] == 1, r_modes[:, 2] == n)) + dim_R0 + dim_Z0
+            AZ[i, j] = -1/2
+        else:
+            j = np.argwhere(np.logical_and(
+                r_modes[:, 1] == -m-1, r_modes[:, 2] == n)) + dim_R0 + dim_Z0
+            AZ[i, j] = 1/2
+            j = np.argwhere(np.logical_and(
+                r_modes[:, 1] == -m+1, r_modes[:, 2] == n)) + dim_R0 + dim_Z0
+            AZ[i, j] = -1/2
+
+    A = np.vstack([AR, AZ])
+    b = np.vstack([bR, bZ])
+    return A, b
+
+
+def get_axis_bc_matrices(R0_basis, Z0_basis, r_basis, l_basis):
+    """Compute constraint matrices for the magnetic axis
+
+    enforces r(0,theta,zeta) == 0 and lambda(0,theta,zeta) == 0
+
+    Parameters
+    ----------
+    R0_basis : Basis
+        Fourier basis for R0
+    Z0_basis : Basis
+        Fourier basis for Z0
+    r_basis : Basis
+        Fourier-Zernike basis for r
+    l_basis : Basis
+        Fourier-Zernike basis for lambda
+
+    Returns
+    -------
+    A, b : ndarray
+        Constraint matrix and vector such that the constraint is satisfied
+        if and only if Ax=b
+    """
+
+    dim_R0 = len(R0_basis.modes)
+    dim_Z0 = len(Z0_basis.modes)
+    dim_r = len(r_basis.modes)
+    dim_l = len(l_basis.modes)
+    dimx = dim_R0 + dim_Z0 + dim_r + dim_l
+
+    N = max(r_basis.N, l_basis.N)
+    ns = np.arange(-N, N+1)
+    A = np.zeros((len(ns)*2, dimx))
+    b = np.zeros((len(ns)*2, 1))
+
+    # r(0,t,z) = 0
+    lmn = r_basis.modes
+    for i, (l, m, n) in enumerate(lmn):
+        if m != 0:
+            continue
+        if (l//2) % 2 == 0:
+            j = np.argwhere(n == ns)
+            A[j, i + dim_R0 + dim_Z0] = 1
+        else:
+            j = np.argwhere(n == ns)
+            A[j, i + dim_R0 + dim_Z0] = -1
+
+    # l(0,t,z) = 0
+    lmn = l_basis.modes
+    for i, (l, m, n) in enumerate(lmn):
+        if m != 0:
+            continue
+        if (l//2) % 2 == 0:
+            j = np.argwhere(n == ns) + len(ns)
+            A[j, i + dim_R0 + dim_Z0 + dim_r] = 1
+        else:
+            j = np.argwhere(n == ns) + len(ns)
+            A[j, i + dim_R0 + dim_Z0 + dim_r] = -1
+
+    return A, b
 
 
 # XXX: Note that this method cannot be improved with FFT due to non-uniform grid
@@ -58,11 +318,11 @@ def compute_bdry_err(cR, cZ, cL, cRb, cZb, R1_transform, Z1_transform, L_transfo
         ratio_Zb = ratio_Rb
     else:
         Rb_basis = DoubleFourierSeries(
-                                M=L_transform.basis.M, N=L_transform.basis.N,
-                                NFP=L_transform.basis.NFP, sym=Tristate(True))
+            M=L_transform.basis.M, N=L_transform.basis.N,
+            NFP=L_transform.basis.NFP, sym=Tristate(True))
         Zb_basis = DoubleFourierSeries(
-                                M=L_transform.basis.M, N=L_transform.basis.N,
-                                NFP=L_transform.basis.NFP, sym=Tristate(False))
+            M=L_transform.basis.M, N=L_transform.basis.N,
+            NFP=L_transform.basis.NFP, sym=Tristate(False))
         AR = Rb_basis.evaluate(nodes)
         AZ = Zb_basis.evaluate(nodes)
         pinv_R = jnp.linalg.pinv(AR, rcond=1e-6)
@@ -150,7 +410,7 @@ def compute_bdry_err_sfl(cR, cZ, cL, cRb, cZb, RZ_transform, L_transform, bdry_t
 
 
 # XXX: this function is used in callback()
-def compute_lambda_err(cL, L_basis:DoubleFourierSeries):
+def compute_lambda_err(cL, L_basis: DoubleFourierSeries):
     """Computes the error in the constraint lambda(t=0, p=0) = 0
 
     Parameters

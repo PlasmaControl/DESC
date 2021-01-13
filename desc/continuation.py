54c4b4d952891ab3f9051b073ac58dd9f54d7f69
Rory Conlin <roryconlin@gmail.com>
2021-01-13 01:22:31 -0500

    Update continuation
    -Most of the stuff with initializing equilibria and changing resolution
    has been moved to Equilibrium class.
    -continuation now basically just calls some methods of Equilibria in
    a loop, and soon will be moved to within EquilibriaFamily
    -perturbations currently don't work, will need to update them for the
    new structure as well as the new coordinates/BC

--- desc/continuation.py
+++ desc/continuation.py
@@ -7,7 +7,7 @@ from desc.backend import use_jax, jit
 from desc.utils import Timer
 from desc.grid import ConcentricGrid
 from desc.transform import Transform
-from desc.configuration import EquilibriaFamily
+from desc.equilibrium import EquilibriaFamily, Equilibrium
 from desc.objective_funs import ObjectiveFunctionFactory
 from desc.perturbations import perturb_continuation_params
 from desc.vmec import VMECIO
@@ -124,53 +124,15 @@ def solve_eq_continuation(inputs, file_name=None, vmec_path=None, device=None):
 
             equil_fam = EquilibriaFamily(inputs=inputs_ii)
             if vmec_path is not None:
-                equil_fam[ii] = VMECIO.load(
-                    vmec_path, L=L[ii], M=M[ii], N=N[ii], index=zern_mode
+                equil_fam[ii] = Equilibrium.from_configuration(
+                    VMECIO.load(vmec_path, L=L[ii], M=M[ii], N=N[ii], index=zern_mode)
                 )
             equil = equil_fam[ii]
 
-            timer.start("Transform precomputation")
-            if verbose > 0:
-                print("Precomputing Transforms")
-
-            # bases (extracted from Equilibrium)
-            R0_basis = equil.R0_basis
-            Z0_basis = equil.Z0_basis
-            r_basis = equil.r_basis
-            l_basis = equil.l_basis
-            p_basis = equil.p_basis
-            i_basis = equil.i_basis
-            R1_basis = equil.R1_basis
-            Z1_basis = equil.Z1_basis
-
-            # grid
-            RZ_grid = ConcentricGrid(
-                M_grid[ii],
-                N_grid[ii],
-                NFP=NFP,
-                sym=sym,
-                axis=False,
-                index=zern_mode,
-                surfs=node_mode,
-            )
-
-            # transforms
-            R0_transform = Transform(RZ_grid, R0_basis, derivs=2)
-            Z0_transform = Transform(RZ_grid, Z0_basis, derivs=2)
-            R1_transform = Transform(RZ_grid, R1_basis, derivs="force")
-            Z1_transform = Transform(RZ_grid, Z1_basis, derivs="force")
-            r_transform = Transform(RZ_grid, r_basis, derivs="force")
-            l_transform = Transform(RZ_grid, l_basis, derivs="force")
-            p_transform = Transform(RZ_grid, p_basis, derivs=1)
-            i_transform = Transform(RZ_grid, i_basis, derivs=1)
-
-            timer.stop("Transform precomputation")
-            if verbose > 1:
-                timer.disp("Transform precomputation")
-
+            equil.precompute_transforms(verbose)
         # continuing from previous solution
         else:
-            equil_fam.append(copy.deepcopy(equil))
+            equil_fam.append(equil.copy())
             equil = equil_fam[ii]
             equil.x0 = equil.x  # new initial guess is previous solution
             equil.solved = False
@@ -178,15 +140,6 @@ def solve_eq_continuation(inputs, file_name=None, vmec_path=None, device=None):
             timer.start("Iteration {} changing resolution".format(ii + 1))
             # change grids
             if M_grid[ii] != M_grid[ii - 1] or N_grid[ii] != N_grid[ii - 1]:
-                RZ_grid = ConcentricGrid(
-                    M_grid[ii],
-                    N_grid[ii],
-                    NFP=NFP,
-                    sym=sym,
-                    axis=False,
-                    index=zern_mode,
-                    surfs=node_mode,
-                )
                 if verbose:
                     print(
                         "Changing node resolution from (M_grid,N_grid) = ({},{}) to ({},{})".format(
@@ -204,25 +157,11 @@ def solve_eq_continuation(inputs, file_name=None, vmec_path=None, device=None):
                         )
                     )
 
-                equil.change_resolution(L=L[ii], M=M[ii], N=N[ii])
-                R0_basis = equil.R0_basis
-                Z0_basis = equil.Z0_basis
-                r_basis = equil.r_basis
-                l_basis = equil.l_basis
-                p_basis = equil.p_basis
-                i_basis = equil.i_basis
-                R1_basis = equil.R1_basis
-                Z1_basis = equil.Z1_basis
-            # change transform matrices
+            equil.change_resolution(
+                L=L[ii], M=M[ii], N=N[ii], M_grid=M_grid[ii], N_grid=N_grid[ii]
+            )
+            equil.precompute_transforms(verbose)
 
-            R0_transform.change_resolution(grid=RZ_grid, basis=R0_basis)
-            Z0_transform.change_resolution(grid=RZ_grid, basis=Z0_basis)
-            r_transform.change_resolution(grid=RZ_grid, basis=r_basis)
-            l_transform.change_resolution(grid=RZ_grid, basis=l_basis)
-            p_transform.change_resolution(grid=RZ_grid)
-            i_transform.change_resolution(grid=RZ_grid)
-            R1_transform.change_resolution(grid=RZ_grid, basis=R1_basis)
-            Z1_transform.change_resolution(grid=RZ_grid, basis=Z1_basis)
             timer.stop("Iteration {} changing resolution".format(ii + 1))
             if verbose > 1:
                 timer.disp("Iteration {} changing resolution".format(ii + 1))
@@ -233,32 +172,34 @@ def solve_eq_continuation(inputs, file_name=None, vmec_path=None, device=None):
             delta_zeta = zeta_ratio[ii] - zeta_ratio[ii - 1]
             deltas = np.array([delta_bdry, delta_pres, delta_zeta])
 
-            # need a non-scalar objective function to do the perturbations
-            obj_fun = ObjectiveFunctionFactory.get_equil_obj_fun(
-                errr_mode,
-                R0_transform=R0_transform,
-                Z0_transform=Z0_transform,
-                r_transform=r_transform,
-                l_transform=l_transform,
-                R1_transform=R1_transform,
-                Z1_transform=Z1_transform,
-                p_transform=p_transform,
-                i_transform=i_transform,
-            )
-            equil_obj = obj_fun.compute
-            callback = obj_fun.callback
-            args = (
-                equil.R1_mn,
-                equil.Z1_mn,
-                equil.p_l,
-                equil.i_l,
-                equil.Psi,
-                zeta_ratio[ii - 1],
-            )
-
             # TODO: should probably perturb before expanding resolution
             # perturbations
             if np.any(deltas):
+                raise NotImplementedError(
+                    "perturbations don't work with new stuff, yell at someone about that."
+                )
+                # need a non-scalar objective function to do the perturbations
+                obj_fun = ObjectiveFunctionFactory.get_equil_obj_fun(
+                    errr_mode,
+                    R0_transform=R0_transform,
+                    Z0_transform=Z0_transform,
+                    r_transform=r_transform,
+                    l_transform=l_transform,
+                    R1_transform=R1_transform,
+                    Z1_transform=Z1_transform,
+                    p_transform=p_transform,
+                    i_transform=i_transform,
+                )
+                equil_obj = obj_fun.compute
+                callback = obj_fun.callback
+                args = (
+                    equil.R1_mn,
+                    equil.Z1_mn,
+                    equil.p_l,
+                    equil.i_l,
+                    equil.Psi,
+                    zeta_ratio[ii - 1],
+                )
                 if verbose > 1:
                     print("Perturbing equilibrium")
                 x, timer = perturb_continuation_params(
@@ -266,26 +207,9 @@ def solve_eq_continuation(inputs, file_name=None, vmec_path=None, device=None):
                 )
                 equil.x = x
 
-        # equilibrium objective function
-        if optim_method in ["bfgs"]:
-            scalar = True
-        else:
-            scalar = False
+        equil.objective = errr_mode
+        equil.optimizer = optim_method
 
-        obj_fun = ObjectiveFunctionFactory.get_equil_obj_fun(
-            errr_mode,
-            R0_transform=R0_transform,
-            Z0_transform=Z0_transform,
-            r_transform=r_transform,
-            l_transform=l_transform,
-            R1_transform=R1_transform,
-            Z1_transform=Z1_transform,
-            p_transform=p_transform,
-            i_transform=i_transform,
-        )
-
-        equil.objective = obj_fun
-        equil.optimizer = "scipy-trf"
         equil.solve(
             ftol=ftol[ii],
             xtol=xtol[ii],

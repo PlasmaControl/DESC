b0dc976a5573707362d3ecc62ddc1d34b758a24a
Daniel Dudt <ddudt@pppl.gov>
2021-04-05 17:08:07 -0400

    initial framework for optimize function
    - `optimize` finds the input parameter perturbation that minimizes a given objective.
    - This function is incomplete.

--- desc/perturbations.py
+++ desc/perturbations.py
@@ -1,5 +1,4 @@
 import numpy as np
-import time
 import warnings
 from termcolor import colored
 
@@ -8,7 +7,7 @@ from desc.backend import use_jax
 from desc.boundary_conditions import get_boundary_condition
 from desc.optimize.tr_subproblems import trust_region_step_exact
 
-__all__ = ["perturb"]
+__all__ = ["perturb", "optimize"]
 
 
 def perturb(
@@ -34,8 +33,8 @@ def perturb(
         equilibrium to perturb
     dRb, dZb, dp, di, dPsi, dzeta_ratio : ndarray or float
         deltas for perturbations of R_boundary, Z_boundary, pressure, iota,
-        toroidal flux, and zeta ratio.. Setting to None or zero ignores that term
-        in the expansion.
+        toroidal flux, and zeta ratio.
+        Setting to None or zero ignores that term in the expansion.
     order : {0,1,2,3}
         order of perturbation (0=none, 1=linear, 2=quadratic, etc)
     tr_ratio : float or array of float
@@ -62,17 +61,17 @@ def perturb(
         warnings.warn(
             colored(
                 "Computing perturbations with finite differences can be "
-                + "highly innacurate. Consider using JAX for exact derivatives",
+                + "highly innacurate. Consider using JAX for exact derivatives.",
                 "yellow",
             )
         )
     if not eq.objective:
         raise AttributeError(
-            "Equilibrium must have objective defined before perturbing"
+            "Equilibrium must have objective defined before perturbing."
         )
     if eq.objective.scalar:
         raise AttributeError(
-            "Cannot perturb with a scalar objective {}".format(eq.objective)
+            "Cannot perturb with a scalar objective: {}".format(eq.objective)
         )
 
     deltas = {}
@@ -116,7 +115,7 @@ def perturb(
     # 1st order
     if order > 0:
 
-        # partial derivatives wrt state vector (x)
+        # 1st partial derivatives wrt state vector (x)
         if Jx is None:
             if verbose > 0:
                 print("Computing df")
@@ -125,9 +124,10 @@ def perturb(
             timer.stop("df computation")
             if verbose > 1:
                 timer.disp("df computation")
+
         u, s, vt = np.linalg.svd(Jx, full_matrices=False)
         m, n = Jx.shape
-        #  cutoff = np.finfo(s.dtype).eps * m * np.amax(s, axis=-1, keepdims=True)
+        # cutoff = np.finfo(s.dtype).eps * m * np.amax(s, axis=-1, keepdims=True)
         cutoff = cutoff * s[0]
         large = s > cutoff
         s_inv = np.divide(1, s, where=large)
@@ -135,7 +135,6 @@ def perturb(
         RHS1 = eq.objective.compute(*args)
 
         # partial derivatives wrt input parameters (c)
-
         inds = tuple([arg_idx[key] for key in deltas])
         dc = tuple([val for val in deltas.values()])
         timer.start("df/dc computation ({})".format(keys))
@@ -143,6 +142,7 @@ def perturb(
         timer.stop("df/dc computation ({})".format(keys))
         if verbose > 1:
             timer.disp("df/dc computation ({})".format(keys))
+
         dx1, hit, alpha = trust_region_step_exact(
             n,
             m,
@@ -191,7 +191,7 @@ def perturb(
     # 3rd order
     if order > 2:
 
-        # 3nd partial derivatives wrt state vector (x)
+        # 3rd partial derivatives wrt state vector (x)
         if verbose > 0:
             print("Computing d^3f")
         timer.start("d^3f computation")
@@ -253,3 +253,160 @@ def perturb(
         timer.disp("Total perturbation")
 
     return eq_new
+
+
+def optimize(
+    eq,
+    objective,
+    dRb=False,
+    dZb=False,
+    dp=False,
+    di=False,
+    dPsi=False,
+    dzeta_ratio=False,
+    order=2,
+    tr_ratio=0.1,
+    cutoff=1e-6,
+    Jx=None,
+    verbose=1,
+    copy=True,
+):
+    """Optimize an Equilibrium with respect to an objective.
+
+    Parameters
+    ----------
+    eq : Equilibrium
+        equilibrium to optimize
+    objective : ObjectiveFunction
+        objective to optimize
+    dRb, dZb, dp, di, dPsi, dzeta_ratio : ndarray or bool
+        modes to include in the perturbations of R_boundary, Z_boundary, pressure, iota,
+        toroidal flux, and zeta ratio.
+        Setting to None or False ignores that mode in the expansion.
+    order : {0,1,2,3}
+        order of perturbation (0=none, 1=linear, 2=quadratic, etc)
+    tr_ratio : float or array of float
+        radius of the trust region, as a fraction of ||x||.
+        enforces ||dx1|| <= tr_ratio*||x|| and ||dx2|| <= tr_ratio*||dx1||
+        if a scalar uses same ratio for all steps, if an array uses the first element
+        for the first step and so on
+    cutoff : float
+        relative cutoff for small singular values in pseudoinverse
+    Jx : ndarray, optional
+        jacobian matrix df/dx
+    verbose : int
+        level of output to display
+    copy : bool
+        whether to perturb the input equilibrium or make a copy. Defaults to True
+
+    Returns
+    -------
+    eq_new : Equilibrium
+        optimized equilibrium
+
+    """
+    if not use_jax:
+        warnings.warn(
+            colored(
+                "Computing perturbations with finite differences can be "
+                + "highly innacurate. Consider using JAX for exact derivatives.",
+                "yellow",
+            )
+        )
+    if not eq.objective:
+        raise AttributeError(
+            "Equilibrium must have objective defined before perturbing."
+        )
+    if eq.objective.scalar:
+        raise AttributeError(
+            "Cannot perturb with a scalar objective: {}".format(eq.objective)
+        )
+
+    deltas = {}
+    if dRb is True and not np.all(dRb == 0):
+        deltas["Rb_lmn"] = np.array(dRb, dtype=bool)
+    if dZb is True and not np.all(dZb == 0):
+        deltas["Zb_lmn"] = np.array(dZb, dtype=bool)
+    if dp is True and not np.all(dp == 0):
+        deltas["p_l"] = np.array(dp, dtype=bool)
+    if di is True and not np.all(di == 0):
+        deltas["i_l"] = np.array(di, dtype=bool)
+    if dPsi is True and not np.all(dPsi == 0):
+        deltas["Psi"] = np.array(dPsi, dtype=bool)
+    if dzeta_ratio is True and not np.all(dzeta_ratio == 0):
+        deltas["zeta_ratio"] = np.array(dzeta_ratio, dtype=bool)
+
+    c_idx = np.array([])
+    for key in deltas:
+        c_idx = np.concatentate((c_idx, deltas[key]))
+
+    timer = Timer()
+    timer.start("Total perturbation")
+
+    arg_idx = {"Rb_lmn": 1, "Zb_lmn": 2, "p_l": 3, "i_l": 4, "Psi": 5, "zeta_ratio": 6}
+    if not eq.built:
+        eq.build(verbose)
+    y = eq.objective.BC_constraint.project(eq.x)
+    args = (y, eq.Rb_lmn, eq.Zb_lmn, eq.p_l, eq.i_l, eq.Psi, eq.zeta_ratio)
+    dx1 = 0
+    dx2 = 0
+    dx3 = 0
+
+    if np.isscalar(tr_ratio):
+        tr_ratio = tr_ratio * np.ones(order)
+    elif len(tr_ratio) < order:
+        raise ValueError(
+            "Got only {} tr_ratios for order {} perturbations".format(
+                len(tr_ratio), order
+            )
+        )
+    keys = ", ".join(deltas.keys())
+    if verbose > 0:
+        print("Perturbing {}".format(keys))
+
+    # 1st order
+    if order > 0:
+
+        # 1st partial derivatives wrt state vector (x)
+        if Jx is None:
+            if verbose > 0:
+                print("Computing df")
+            timer.start("df computation")
+            Jx = eq.objective.jac_x(*args)
+            timer.stop("df computation")
+            if verbose > 1:
+                timer.disp("df computation")
+        if verbose > 0:
+            print("Computing dg")
+        timer.start("dg computation")
+        Gx = objective.jac_x(*args)
+        timer.stop("dg computation")
+        if verbose > 1:
+            timer.disp("dg computation")
+
+        # partial derivatives wrt input parameters (c)
+        inds = tuple([arg_idx[key] for key in deltas])
+        timer.start("df/dc computation ({})".format(keys))
+        Jc = eq.objective.derivative(inds, *args)[:, c_idx]
+        timer.stop("df/dc computation ({})".format(keys))
+        if verbose > 1:
+            timer.disp("df/dc computation ({})".format(keys))
+        inds = tuple([arg_idx[key] for key in deltas])
+        timer.start("dg/dc computation ({})".format(keys))
+        Gc = objective.derivative(inds, *args)[:, c_idx]
+        timer.stop("dg/dc computation ({})".format(keys))
+        if verbose > 1:
+            timer.disp("dg/dc computation ({})".format(keys))
+
+        f = eq.objective.compute(*args)
+        g = objective.compute(*args)
+        Jx_inv = np.linalg.pinv(Jx, rcond=cutoff)
+        LHS = Gx @ Jx_inv @ Jc - Gc
+        RHS = g - Gx @ Jx_inv @ f
+
+    if order > 1:
+        raise ValueError(
+            "Higher-order optimizations not yet implemented: {}".format(order)
+        )
+
+    return LHS, RHS

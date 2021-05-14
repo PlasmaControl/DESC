b2d52a54958dd424c6fdca7c9d166d49e742826d
Daniel Dudt <ddudt@pppl.gov>
2021-05-14 17:27:40 -0400

    QS test script updates from STELLOPT benchmark
    - Note: this file will get deleted eventually.

--- qs_test.py
+++ qs_test.py
@@ -1,14 +1,68 @@
 """Testing QS perturbations."""
 
 import numpy as np
+import matplotlib.pyplot as plt
 from desc.equilibrium import EquilibriaFamily
-from desc.objective_funs import QuasisymmetryTripleProduct
+from desc.objective_funs import QuasisymmetryTripleProduct, QuasisymmetryFluxFunction
 from desc.transform import Transform
 from desc.grid import LinearGrid
-from desc.plotting import plot_surfaces, plot_2d
-import matplotlib.pyplot as plt
+from desc.plotting import plot_surfaces, plot_2d, plot_section
 
 
+# validating functions
+
+fam = EquilibriaFamily.load("examples/DESC/outputs/HSXT_ansi.h5")
+eq = fam[-1]
+args = (eq.x, eq.Rb_lmn, eq.Zb_lmn, eq.p_l, eq.i_l, eq.Psi)
+
+res = 30
+rho = np.logspace(-1, 0, 11)
+TP_err = np.zeros_like(rho)
+FF_err = np.zeros_like(rho)
+
+for i in range(rho.size):
+    print("rho = {}".format(rho[i]))
+    grid = LinearGrid(M=res, N=res, NFP=eq.NFP, rho=rho[i])
+    R_transform = Transform(grid, eq.R_basis)
+    Z_transform = Transform(grid, eq.Z_basis)
+    L_transform = Transform(grid, eq.L_basis)
+    Rb_transform = Transform(grid, eq.Rb_basis)
+    Zb_transform = Transform(grid, eq.Zb_basis)
+    p_transform = Transform(grid, eq.p_basis)
+    i_transform = Transform(grid, eq.i_basis)
+    TP_fun = QuasisymmetryTripleProduct(
+        R_transform,
+        Z_transform,
+        L_transform,
+        Rb_transform,
+        Zb_transform,
+        p_transform,
+        i_transform,
+        eq.constraint,
+    )
+    FF_fun = QuasisymmetryFluxFunction(
+        R_transform,
+        Z_transform,
+        L_transform,
+        Rb_transform,
+        Zb_transform,
+        p_transform,
+        i_transform,
+        eq.constraint,
+    )
+    TP_err[i] = TP_fun.compute(*args)
+    FF_err[i] = FF_fun.compute(*args)
+
+plt.rcParams.update({"font.size": 20})
+fig, ax = plt.subplots(1, 1, figsize=(6, 6))
+ax.loglog(rho, TP_err, "bo-", label="$f_T$")
+ax.loglog(rho, FF_err, "ro-", label="$f_P$")
+ax.legend()
+ax.set_xlabel("$\\rho = \\sqrt{\\psi_N}$")
+ax.set_title("HSX")
+fig.set_tight_layout(True)
+
+"""
 rho = 0.9  # surface to optimize
 order = 2  # optimization order
 iters = 5  # optimization iterations
@@ -115,3 +169,4 @@ ax.set_xlabel("Iteration")
 ax.set_ylabel("$\\langle g(x,c) \\rangle$")
 ax.set_title("Quasi-symmetry error at $\\rho={}$".format(rho))
 fig.set_tight_layout(True)
+"""

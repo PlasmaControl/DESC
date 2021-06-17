7b58013c281770b166320b78bd07719eeaa54b4d
ddudt <ddudt@pppl.gov>
2021-06-17 11:47:26 -0400

    QS objective funs compute scalar improvements

--- qs_test.py
+++ qs_test.py
@@ -8,18 +8,15 @@ from desc.objective_funs import QuasisymmetryTripleProduct, QuasisymmetryFluxFun
 from desc.transform import Transform
 from desc.grid import LinearGrid
 
-fname = "QHS_TP_r70_aR0_m2_i2"
+fname = "QHS_TP_r70_aR0_m4_i10"
 
 rho = 0.7  # surface to optimize
 order = 2  # optimization order
-iters = 2  # number of iterations
-mn_lim = np.array([1, 2])  # boundary mode optimization limit
+iters = 10  # number of iterations
+mn_lim = np.array([1, 2, 3, 4])  # boundary mode optimization limit
 
 fam = EquilibriaFamily.load("examples/DESC/qs/QHS_init.h5")
 eq = fam[-1]
-
-err = 1e6
-err0 = 0
 
 timer = Timer()
 timer.start("total")
@@ -45,7 +42,7 @@ p_tform_err = Transform(grid_err, eq.p_basis)
 i_tform_err = Transform(grid_err, eq.i_basis)
 
 for k, mn in enumerate(mn_lim):
-    print("Optimization step {}. Optimizing boundary modes m,n<={}".format(k, mn))
+    print("\nOptimization step {}. Optimizing boundary modes m,n <= {}\n".format(k, mn))
     timer.start("opt step {}".format(k))
 
     # optimization variables: boundary modes <= MN
@@ -82,10 +79,14 @@ for k, mn in enumerate(mn_lim):
             i_tform_err,
             eq.constraint,
         )
+        args = (eq.x, eq.Rb_lmn, eq.Zb_lmn, eq.p_l, eq.i_l, eq.Psi)
+        err0 = fun_err.compute_scalar(*args)
+        err = err0 + 1
+        print("error = {}".format(err))
 
         tr_ratio = 0.1
         while err > err0:
-            print("trust-region ratio = {}".format(tr_ratio))
+            print("trust-region ratio = {}\n".format(tr_ratio))
             eq_p = eq.perturb(
                 objective=fun_obj,
                 dRb=dRb,
@@ -96,16 +97,16 @@ for k, mn in enumerate(mn_lim):
                 copy=True,
             )
             args = (eq_p.x, eq_p.Rb_lmn, eq_p.Zb_lmn, eq_p.p_l, eq_p.i_l, eq_p.Psi)
-            err = fun_err.compute(*args)
+            err = fun_err.compute_scalar(*args)
             tr_ratio /= 2
 
-        err0 = err
+        print("error = {}".format(err))
         eq = eq_p
         fam.insert(len(fam), eq)
         eq.solve(ftol=1e-2, xtol=1e-6, gtol=1e-6, maxiter=50, verbose=3)
+        fam.save("examples/DESC/qs/" + fname + ".h5")
         timer.stop("opt step {}".format(k))
         timer.disp("opt step {}".format(k))
 
-fam.save("examples/DESC/qs/" + fname + ".h5")
 timer.stop("total")
 timer.disp("total")

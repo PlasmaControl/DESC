68cec4366598d25f84d83383718d083fbd950775
Rory Conlin <roryconlin@gmail.com>
2021-09-01 14:11:43 -0400

    Remove old plotting test functions
    TeX labels for computed quantities are now stored in data_index
    rather than being dynamically built by _format_name and _name_label,
    so tests for those functions were removed.

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -23,104 +23,7 @@ from desc.basis import (
     FourierZernikeBasis,
 )
 from desc.equilibrium import EquilibriaFamily
-from desc import plotting as dplt
-
-
-class TestPlot(unittest.TestCase):
-    def setUp(self):
-        self.names = [
-            "B",
-            "|B|",
-            "B^zeta",
-            "B_zeta",
-            "B_r",
-            "B^zeta_r",
-            "B_zeta_r",
-            "B**2",
-            "B_r**2",
-            "B^zeta**2",
-            "B_zeta**2",
-            "B^zeta_r**2",
-            "B_zeta_r**2",
-        ]
-        self.bases = ["B", "|B|", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B"]
-        self.sups = ["", "", "zeta", "", "", "zeta", "", "", "", "zeta", "", "zeta", ""]
-        self.subs = ["", "", "", "zeta", "", "", "zeta", "", "", "", "zeta", "", "zeta"]
-        self.ds = ["", "", "", "", "r", "r", "r", "", "r", "", "", "r", "r"]
-        self.pows = ["", "", "", "", "", "", "", "2", "2", "2", "2", "2", "2"]
-        self.name_dicts = []
-        for name in self.names:
-            self.name_dicts.append(dplt._format_name(name))
-
-    def test_name_dict(self):
-        self.assertTrue(
-            all(
-                [
-                    self.name_dicts[i]["base"] == self.bases[i]
-                    for i in range(len(self.names))
-                ]
-            )
-        )
-        self.assertTrue(
-            all(
-                [
-                    self.name_dicts[i]["sups"] == self.sups[i]
-                    for i in range(len(self.names))
-                ]
-            )
-        )
-        self.assertTrue(
-            all(
-                [
-                    self.name_dicts[i]["subs"] == self.subs[i]
-                    for i in range(len(self.names))
-                ]
-            )
-        )
-        self.assertTrue(
-            all([self.name_dicts[i]["d"] == self.ds[i] for i in range(len(self.names))])
-        )
-        self.assertTrue(
-            all(
-                [
-                    self.name_dicts[i]["power"] == self.pows[i]
-                    for i in range(len(self.names))
-                ]
-            )
-        )
-
-    def test_name_label(self):
-        labels = [dplt._name_label(nd) for nd in self.name_dicts]
-        print(labels)
-        self.assertTrue(all([label[0] == "$" and label[-1] == "$" for label in labels]))
-        self.assertTrue(
-            all(
-                [
-                    "/dr" in labels[i]
-                    for i in range(len(labels))
-                    if self.name_dicts[i]["d"] != ""
-                ]
-            )
-        )
-        self.assertTrue(
-            all(
-                [
-                    "^{" not in labels[i]
-                    for i in range(len(labels))
-                    if self.name_dicts[i]["sups"] == ""
-                    and self.name_dicts[i]["power"] == ""
-                ]
-            )
-        )
-        self.assertTrue(
-            all(
-                [
-                    "_{" not in labels[i]
-                    for i in range(len(labels))
-                    if self.name_dicts[i]["subs"] == ""
-                ]
-            )
-        )
+from desc.compute import data_index
 
 
 @pytest.mark.mpl_image_compare(tolerance=50)
@@ -310,7 +213,7 @@ def test_plot_magnetic_pressure(plot_eq):
 
 @pytest.mark.mpl_image_compare(tolerance=50)
 def test_plot_gradpsi(plot_eq):
-    fig, ax = plot_2d(plot_eq, "|grad(psi)|")
+    fig, ax = plot_2d(plot_eq, "|grad(rho)|")
     return fig
 
 

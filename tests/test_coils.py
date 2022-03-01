f61e606290053fc653f26b989165043310a743ef
Rory Conlin <roryconlin@gmail.com>
2022-02-28 22:38:39 -0500

    Add tests for coils

--- tests/test_coils.py
+++ tests/test_coils.py
@@ -1,9 +1,9 @@
 import numpy as np
 import unittest
 import pytest
-
-from desc.coils import Coil, CoilSet
-from desc.geometry import FourierRZCurve, FourierXYZCurve, FourierPlanarCurve
+from desc.grid import LinearGrid, Grid
+from desc.coils import CoilSet, FourierRZCoil, FourierXYZCoil, FourierPlanarCoil
+from desc.geometry import FourierRZCurve
 
 
 class TestCoil(unittest.TestCase):
@@ -13,18 +13,16 @@ class TestCoil(unittest.TestCase):
         I = 1
         By_true = 1e-7 * 2 * np.pi * R ** 2 * I / (y ** 2 + R ** 2) ** (3 / 2)
         B_true = np.array([0, By_true, 0])
-        coil = Coil(FourierXYZCurve(), I)
-        coil.grid = 100
+        coil = FourierXYZCoil(I)
+        coil.grid = LinearGrid(N=100, endpoint=True)
         assert coil.grid.num_nodes == 100
-        B_approx = coil.compute_magnetic_field([10, y, 0], basis="xyz")[0]
+        B_approx = coil.compute_magnetic_field(Grid([[10, y, 0]]), basis="xyz")[0]
         np.testing.assert_allclose(B_true, B_approx, rtol=1e-3, atol=1e-10)
 
     def test_properties(self):
 
-        curve = FourierPlanarCurve()
         current = 4.34
-        coil = Coil(curve, current)
-        assert coil.curve is curve
+        coil = FourierPlanarCoil(current)
         assert coil.current == current
         new_current = 3.5
         coil.current = new_current
@@ -39,8 +37,12 @@ class TestCoilSet(unittest.TestCase):
         I = 1
         Bz_true = np.sum(1e-7 * 2 * np.pi * R ** 2 * I / (z ** 2 + R ** 2) ** (3 / 2))
         B_true = np.array([0, 0, Bz_true])
-        curve = FourierRZCurve()
-        coils = CoilSet.linspaced_linear(curve, I, [0, 0, 10], 10, True)
+        coil = FourierRZCoil(0.1)
+        coils = CoilSet.linspaced_linear(
+            coil, displacement=[0, 0, 10], n=10, endpoint=True
+        )
+        coils.current = I
+        np.testing.assert_allclose(coils.current, I)
         coils.grid = 100
         assert coils.grid.num_nodes == 100
         B_approx = coils.compute_magnetic_field([0, 0, z[-1]], basis="xyz")[0]
@@ -53,8 +55,9 @@ class TestCoilSet(unittest.TestCase):
         I = 1
         Bp_true = np.sum(1e-7 * 4 * np.pi * N * I / 2 / np.pi / R)
         B_true = np.array([0, Bp_true, 0])
-        curve = FourierPlanarCurve()
-        coils = CoilSet.linspaced_angular(curve, I, n=N)
+        coil = FourierPlanarCoil()
+        coil.current = I
+        coils = CoilSet.linspaced_angular(coil, n=N)
         coils.grid = 100
         assert all([coil.grid.num_nodes == 100 for coil in coils])
         B_approx = coils.compute_magnetic_field([10, 0, 0], basis="rpz")[0]
@@ -67,8 +70,8 @@ class TestCoilSet(unittest.TestCase):
         I = 1
         Bp_true = np.sum(1e-7 * 4 * np.pi * N * I / 2 / np.pi / R)
         B_true = np.array([0, Bp_true, 0])
-        curve = FourierPlanarCurve()
-        coils = CoilSet.linspaced_angular(curve, I, angle=np.pi / 2, n=N // 4)
+        coil = FourierPlanarCoil()
+        coils = CoilSet.linspaced_angular(coil, angle=np.pi / 2, n=N // 4)
         coils = CoilSet.from_symmetry(coils, NFP=4)
         coils.grid = 100
         assert all([coil.grid.num_nodes == 100 for coil in coils])
@@ -77,13 +80,165 @@ class TestCoilSet(unittest.TestCase):
 
         # with stellarator symmetry
         NFP = 4
-        curve = FourierXYZCurve()
-        curve.rotate(angle=np.pi / N)
+        coil = FourierXYZCoil()
+        coil.rotate(angle=np.pi / N)
         coils = CoilSet.linspaced_angular(
-            curve, I, [0, 0, 1], np.pi / NFP, N // NFP // 2
+            coil, I, [0, 0, 1], np.pi / NFP, N // NFP // 2
         )
         coils.grid = 100
         assert coils.grid.num_nodes == 100
         coils2 = CoilSet.from_symmetry(coils, NFP, True)
         B_approx = coils2.compute_magnetic_field([10, 0, 0], basis="rpz")[0]
         np.testing.assert_allclose(B_true, B_approx, rtol=1e-3, atol=1e-10)
+
+    def test_properties(self):
+        coil = FourierPlanarCoil()
+        coils = CoilSet.linspaced_linear(coil, n=4)
+        coils.grid = np.array([[0.0, 0.0, 0.0]])
+        np.testing.assert_allclose(
+            coils.compute_coordinates(),
+            np.array(
+                [
+                    [12, 0, 0],
+                    [12.5, 0, 0],
+                    [13, 0, 0],
+                    [13.5, 0, 0],
+                ]
+            ).reshape((4, 1, 3)),
+        )
+        np.testing.assert_allclose(coils.compute_curvature(), 1 / 2)
+        np.testing.assert_allclose(coils.compute_torsion(), 0)
+        TNB = coils.compute_frenet_frame(grid=np.array([[0.0, 0.0, 0.0]]), basis="xyz")
+        T = [foo[0] for foo in TNB]
+        N = [foo[1] for foo in TNB]
+        B = [foo[2] for foo in TNB]
+        np.testing.assert_allclose(
+            T,
+            np.array(
+                [
+                    [0, 0, -1],
+                    [0, 0, -1],
+                    [0, 0, -1],
+                    [0, 0, -1],
+                ]
+            ).reshape((4, 1, 3)),
+            atol=1e-12,
+        )
+        np.testing.assert_allclose(
+            N,
+            np.array(
+                [
+                    [-1, 0, 0],
+                    [-1, 0, 0],
+                    [-1, 0, 0],
+                    [-1, 0, 0],
+                ]
+            ).reshape((4, 1, 3)),
+            atol=1e-12,
+        )
+        np.testing.assert_allclose(
+            B,
+            np.array(
+                [
+                    [0, 1, 0],
+                    [0, 1, 0],
+                    [0, 1, 0],
+                    [0, 1, 0],
+                ]
+            ).reshape((4, 1, 3)),
+            atol=1e-12,
+        )
+        coils.grid = 100
+        np.testing.assert_allclose(coils.compute_length(), 2 * 2 * np.pi)
+        coils.translate([1, 1, 1])
+        np.testing.assert_allclose(coils.compute_length(), 2 * 2 * np.pi)
+        coils.flip([1, 0, 0])
+        coils.grid = np.array([[0.0, 0.0, 0.0]])
+        TNB = coils.compute_frenet_frame(grid=np.array([[0.0, 0.0, 0.0]]), basis="xyz")
+        T = [foo[0] for foo in TNB]
+        N = [foo[1] for foo in TNB]
+        B = [foo[2] for foo in TNB]
+        np.testing.assert_allclose(
+            T,
+            np.array(
+                [
+                    [0, 0, -1],
+                    [0, 0, -1],
+                    [0, 0, -1],
+                    [0, 0, -1],
+                ]
+            ).reshape((4, 1, 3)),
+            atol=1e-12,
+        )
+        np.testing.assert_allclose(
+            N,
+            np.array(
+                [
+                    [1, 0, 0],
+                    [1, 0, 0],
+                    [1, 0, 0],
+                    [1, 0, 0],
+                ]
+            ).reshape((4, 1, 3)),
+            atol=1e-12,
+        )
+        np.testing.assert_allclose(
+            B,
+            np.array(
+                [
+                    [0, 1, 0],
+                    [0, 1, 0],
+                    [0, 1, 0],
+                    [0, 1, 0],
+                ]
+            ).reshape((4, 1, 3)),
+            atol=1e-12,
+        )
+
+    def test_dunder_methods(self):
+        coil1 = FourierXYZCoil()
+        coils1 = CoilSet.from_symmetry(coil1, NFP=4)
+        coil2 = FourierPlanarCoil()
+        coils2 = coils1 + [coil2]
+        assert coils2[-1] is coil2
+        coils2 = coils1 + CoilSet(coil2)
+        assert coils2[-1] is coil2
+
+        with pytest.raises(TypeError):
+            coils3 = coils1 + FourierRZCurve()
+
+        with pytest.raises(TypeError):
+            coils1[-1] = FourierRZCurve()
+
+        coils1[-1] = coil2
+        assert coils1[-1] is coil2
+
+        coils1.insert(-1, coil2)
+        with pytest.raises(TypeError):
+            coils1.insert(-1, FourierRZCurve())
+
+        assert len(coils1) == 5
+
+        assert coils1[-1] is coil2
+        assert coils1[-2] is coil2
+
+        s = coils1[-2:]
+        assert s[-1] is coil2
+
+        del coils1[-2]
+        assert len(coils1) == 4
+        assert coils1[-1] is coil2
+        assert coils1[-2][0].__class__ is coil1.__class__
+
+
+def test_repr():
+    coil = FourierRZCoil()
+    assert "FourierRZCoil" in str(coil)
+    assert "current=1" in str(coil)
+
+    coils = CoilSet.linspaced_angular(coil, n=4)
+    assert "CoilSet" in str(coils)
+    assert "4 submembers" in str(coils)
+
+    coils.name = "MyCoils"
+    assert "MyCoils" in str(coils)

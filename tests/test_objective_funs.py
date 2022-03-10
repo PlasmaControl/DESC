b7c59a581768b5e0c3131ed6f4c4741865d7d94b
Rory Conlin <roryconlin@gmail.com>
2022-03-09 21:16:14 -0500

    Add basic tests for objective functions

--- tests/test_objective_funs.py
+++ tests/test_objective_funs.py
@@ -1,25 +1,73 @@
 import unittest
 import numpy as np
-import pytest
-from desc.grid import LinearGrid, ConcentricGrid
 from desc.equilibrium import Equilibrium
 from desc.objectives import (
-    ObjectiveFunction,
-    FixedBoundaryR,
-    FixedBoundaryZ,
-    FixedPressure,
-    FixedIota,
-    FixedPsi,
-    LCFSBoundary,
-    RadialForceBalance,
-    HelicalForceBalance,
     Energy,
+    Volume,
+    AspectRatio,
+    ToroidalCurrent,
+    ToroidalCurrentDensity,
+    QuasisymmetryBoozer,
+    QuasisymmetryTwoTerm,
+    QuasisymmetryTripleProduct,
 )
 
 
 class TestObjectiveFunction(unittest.TestCase):
     """Test ObjectiveFunction class."""
 
-    def test_something(self):
-        """Test something."""
-        pass
+    def test_volume(self):
+        eq = Equilibrium()
+        obj = Volume(target=10 * np.pi ** 2, weight=1 / np.pi ** 2)
+        obj.build(eq)
+        V = obj.compute(eq.R_lmn, eq.Z_lmn)
+        np.testing.assert_allclose(V, 10)
+
+    def test_aspect_ratio(self):
+        eq = Equilibrium()
+        obj = AspectRatio(target=5, weight=2)
+        obj.build(eq)
+        AR = obj.compute(eq.R_lmn, eq.Z_lmn)
+        np.testing.assert_allclose(AR, 10)
+
+    def test_energy(self):
+        eq = Equilibrium()
+        obj = Energy(target=0, weight=(4 * np.pi * 1e-7))
+        obj.build(eq)
+        W = obj.compute(eq.R_lmn, eq.Z_lmn, eq.L_lmn, eq.i_l, eq.p_l, eq.Psi)
+        np.testing.assert_allclose(W, 10)
+
+    def test_toroidal_current(self):
+        eq = Equilibrium()
+        obj = ToroidalCurrent(target=1, weight=2)
+        obj.build(eq)
+        I = obj.compute(eq.R_lmn, eq.Z_lmn, eq.L_lmn, eq.i_l, eq.Psi)
+        np.testing.assert_allclose(I, -2)
+
+    def test_toroidal_current_density(self):
+        eq = Equilibrium()
+        obj = ToroidalCurrentDensity(target=1, weight=2)
+        obj.build(eq)
+        Jt = obj.compute(eq.R_lmn, eq.Z_lmn, eq.L_lmn, eq.i_l, eq.Psi)
+        np.testing.assert_allclose(Jt, -2)
+
+    def test_qs_boozer(self):
+        eq = Equilibrium()
+        obj = QuasisymmetryBoozer()
+        obj.build(eq)
+        fb = obj.compute(eq.R_lmn, eq.Z_lmn, eq.L_lmn, eq.i_l, eq.Psi)
+        np.testing.assert_allclose(fb, 0)
+
+    def test_qs_twoterm(self):
+        eq = Equilibrium()
+        obj = QuasisymmetryTwoTerm()
+        obj.build(eq)
+        fb = obj.compute(eq.R_lmn, eq.Z_lmn, eq.L_lmn, eq.i_l, eq.Psi)
+        np.testing.assert_allclose(fb, 0)
+
+    def test_qs_tp(self):
+        eq = Equilibrium()
+        obj = QuasisymmetryTripleProduct()
+        obj.build(eq)
+        fb = obj.compute(eq.R_lmn, eq.Z_lmn, eq.L_lmn, eq.i_l, eq.Psi)
+        np.testing.assert_allclose(fb, 0)

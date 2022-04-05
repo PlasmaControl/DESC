40f4ab93bf73cc351b7f206b1b8826e4fe06f59b
Timothy LeViness <timothy@leviness.email>
2022-04-05 13:19:24 -0400

    Fixed flake8 config reference

--- scripts/run_flake8_linting.sh
+++ scripts/run_flake8_linting.sh
@@ -1,8 +1,7 @@
-rm -f flake8_errors.output flake8_summary.output
-echo "Generating Flake8 Error Summary"
-flake8 --config flake8.ini -qqq --output-file flake8_summary.output --statistics ../desc/ ../tests/
-echo "Generating Flake8 Full Report"
-flake8 --config flake8.ini --output-file flake8_errors.output ../desc/ ../tests/
-echo "Number of formatting errors found:"
-flake8 --config flake8.ini -qqq --count ../desc/ ../tests/ || flake_return_code=$?
-echo "FLAKE_RETURN_CODE =$flake_return_code"
+rm -f flake8.output
+
+echo "Running Flake8 using settings defined in flake8.ini..."
+flake8 --config flake8rc --output-file flake8_errors.output ../desc/ ../tests/
+
+echo "Generating Flake8 report..."
+flake8 --config flake8.rc -qqq --statistics --output-file flake8_summary.output ../desc/ ../tests/

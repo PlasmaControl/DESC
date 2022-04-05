6c4ee9f318d18ee2045ea693192eb29006dc637f
Timothy LeViness <timothy@leviness.email>
2022-04-05 13:44:31 -0400

    Removing old files

--- scripts/run_flake8_linting.sh
+++
@@ -1,7 +0,0 @@
-rm -f flake8.output
-
-echo "Running Flake8 using settings defined in flake8.ini..."
-flake8 --config flake8rc --output-file flake8_errors.output ../desc/ ../tests/
-
-echo "Generating Flake8 report..."
-flake8 --config flake8.rc -qqq --statistics --output-file flake8_summary.output ../desc/ ../tests/
--- scripts/run_pylint.sh
+++
@@ -1,6 +0,0 @@
-echo "Removing old output files..."
-rm -i -f pylint.output
-
-echo "Running Pylint on settings defined in pylintrc..."
-pylint ../desc  ../tests > "pylint.output"
-echo "Done!"

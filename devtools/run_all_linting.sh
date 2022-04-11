73ebe9baadd9f19bf77810ff94691fb24424bcc8
Timothy LeViness <timothy@leviness.email>
2022-04-11 18:29:54 -0400

    Reverting change that broke testing config files, will fix tomorrow

--- devtools/run_all_linting.sh
+++ devtools/run_all_linting.sh
@@ -1,10 +1,10 @@
 rm -f flake8.output
 
-echo "Running Flake8 using settings defined in setup.cfg..."
-flake8 --config ../setup.cfg --output-file flake8_errors.output ../desc/ ../tests/
+echo "Running Flake8 using settings defined in flake8.ini..."
+flake8 --config flake8rc --output-file flake8_errors.output ../desc/ ../tests/
 
 echo "Generating Flake8 report..."
-flake8 --config ../setup.cfg -qqq --statistics --output-file flake8_summary.output ../desc/ ../tests/
+flake8 --config flake8rc -qqq --statistics --output-file flake8_summary.output ../desc/ ../tests/
 
 echo "Running Pylint on settings defined in pylintrc..."
 pylint ../desc  ../tests > "pylint.output"

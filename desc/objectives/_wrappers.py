3a1e9824013930992848c5532e7804106b8a123e
ddudt <ddudt@pppl.gov>
2022-05-06 19:32:23 -0400

    Edits to WrappedEquilibriumObjective. This is still buggy.

--- desc/objectives/_wrappers.py
+++ desc/objectives/_wrappers.py
@@ -1,184 +1,186 @@
 import numpy as np
+
 from desc.backend import jnp
+from desc.compute import arg_order
+from .objective_funs import ObjectiveFunction
 from .utils import (
     get_equilibrium_objective,
     get_fixed_boundary_constraints,
     factorize_linear_constraints,
 )
-from desc.compute import arg_order
 
 
-class WrappedEquilibriumObjective:
-    """Evaluate an objective subject to equilibrium constraint
-
-    Parameters
-    ----------
-    objetive : ObjectiveFunction
-        objective to evaluate
-    eq : Equilibrium
-        starting equilibrium for optimization
-    perturb_options : dict
-        keyword options passed to eq.perturb after each update
-    solve_options : dict
-        keyword arguments passed to eq.solve after each update
-    """
+class WrappedEquilibriumObjective(ObjectiveFunction):
+    """Evaluate an objective subject to equilibrium constraint."""
 
     def __init__(
         self,
         objective,
-        equilibrium_objective=None,
+        eq_objective=None,
         eq=None,
+        use_jit=True,
         verbose=1,
         perturb_options={},
         solve_options={},
     ):
+        """Initialize a WrappedEquilibriumObjective.
 
+        Parameters
+        ----------
+        objective : ObjectiveFunction
+            Objective function to optimize.
+        eq_objective : ObjectiveFunction
+            Equilibrium objective to enforce.
+        eq : Equilibrium, optional
+            Equilibrium that will be optimized to satisfy the objectives.
+        use_jit : bool, optional
+            Whether to just-in-time compile the objectives and derivatives.
+        verbose : int, optional
+            Level of output.
+
+        """
         self._objective = objective
+        self._eq_objective = eq_objective
         self._perturb_options = perturb_options
         self._solve_options = solve_options
-        self._verbose = verbose
+        self._use_jit = use_jit
+        self._built = False
+        self._compiled = True
 
         if eq is not None:
-            self.build(eq)
+            self.build(eq, use_jit=self._use_jit, verbose=verbose)
 
-    def build(self, eq):
+    # TODO: add timing and verbose statements
+    def build(self, eq, use_jit=True, verbose=1):
+        """Build the objective.
 
+        Parameters
+        ----------
+        eq : Equilibrium, optional
+            Equilibrium that will be optimized to satisfy the Objective.
+        use_jit : bool, optional
+            Whether to just-in-time compile the objective and derivatives.
+        verbose : int, optional
+            Level of output.
+
+        """
         self._eq = eq.copy()
-        if self._equilibrium_objective is None:
-            self._equilibrium_objective = get_equilibrium_objective()
-        self._constraints = get_fixed_boundary_constraints(self._eq.bdry_mode)
+        if self._eq_objective is None:
+            self._eq_objective = get_equilibrium_objective()
+        self._constraints = get_fixed_boundary_constraints()
 
-        self._objective.build(self._eq)
-        self._equilibrium_objective.build(self._eq)
+        self._objective.build(self._eq, use_jit=self.use_jit, verbose=verbose)
+        self._eq_objective.build(self._eq, use_jit=self.use_jit, verbose=verbose)
         for constraint in self._constraints:
-            constraint.build(self._eq)
+            constraint.build(self._eq, use_jit=self.use_jit, verbose=verbose)
+        self._objectives = self._objective.objectives
 
-        xp, A, Ainv, b, Z, unfixed_idx, project, recover = factorize_linear_constraints(
-            self._constraints,
-            self._equilibrium_objective.dim_x,
-            self._equilibrium_objective.x_idx,
-        )
-        self._dimensions = {}
-        self._dimensions["R_lmn"] = self._eq.R_basis.num_modes
-        self._dimensions["Z_lmn"] = self._eq.Z_basis.num_modes
-        self._dimensions["L_lmn"] = self._eq.L_basis.num_modes
-        self._dimensions["Rb_lmn"] = self._eq.surface.R_basis.num_modes
-        self._dimensions["Zb_lmn"] = self._eq.surface.Z_basis.num_modes
-        self._dimensions["p_l"] = self._eq.pressure.params.size
-        self._dimensions["i_l"] = self._eq.iota.params.size
-        self._dimensions["Psi"] = 1
+        self._dim_f = self._objective.dim_f
+        if self._dim_f == 1:
+            self._scalar = True
+        else:
+            self._scalar = False
+
+        # set_state_vector
         self._args = ["Rb_lmn", "Zb_lmn", "p_l", "i_l", "Psi"]
-        self._x_idx = {}
+        self._dimensions = self._objective.dimensions
         idx = 0
-        for arg in self._args:
-            self._x_idx[arg] = np.arange(idx, idx + self._dimensions[arg])
-            idx += self._dimensions[arg]
+        self._x_idx = {}
+        for arg in self.args:
+            self.x_idx[arg] = np.arange(idx, idx + self.dimensions[arg])
+            idx += self.dimensions[arg]
+        self._dim_x = idx
 
-        self._unfixed_idx = unfixed_idx
-        self._fixed_idx = np.setdiff1d(
-            np.arange(self._equilibrium_objective.dim_x), unfixed_idx
+        (
+            xp,
+            A,
+            self._Ainv,
+            b,
+            self._Z,
+            self._unfixed_idx,
+            project,
+            recover,
+        ) = factorize_linear_constraints(
+            self._constraints,
+            self._eq_objective.dim_x,
+            self._eq_objective.x_idx,  # FIXME: this has to contain all the arguments
         )
-        self._A = A
-        self._Ainv = Ainv
-        self._Z = Z
-        # full x is everything FB takes, which should be everything?
-        self._xfull = self._equilibrium_objective.x(eq)
-        # optimization x is all the stuff thats fixed during a normal eq solve
-        self._xopt_old = jnp.concatenate(
-            [jnp.atleast_1d(getattr(self._eq, arg)) for arg in self._args]
-        )
 
-    def x(self, eq):
-        return jnp.concatenate([jnp.atleast_1d(getattr(eq, arg)) for arg in self._args])
+        self._x_old = np.zeros((self._dim_x,))
+        for arg in self.args:
+            self._x_old[self.x_idx[arg]] = getattr(eq, arg)
 
-    def _unpack_xopt(self, xopt):
-        kwargs = {}
-        for arg in self._args:
-            kwargs[arg] = xopt[self._x_idx[arg]]
-        return kwargs
+        self._built = True
 
-    def _update(self, xopt):
-        if jnp.all(xopt == self._xopt_old):
+    def _update_equilibrium(self, x):
+        """Description."""
+        if jnp.all(x == self._x_old):
             pass
         else:
-            if self._verbose:
-                print("Updating equilibrium")
-            xoptd = self._unpack_xopt(xopt)
-            xoptd_old = self._unpack_xopt(self._xopt_old)
-            deltas = {"d" + str(key): xoptd[key] - xoptd_old[key] for key in xoptd}
+            x_dict = self._unpack_state(x)
+            x_dict_old = self._unpack_state(self._x_old)
+            deltas = {
+                "d" + str(key).split("_")[0]: x_dict[key] - x_dict_old[key]
+                for key in x_dict
+            }
             self._eq.perturb(
-                self._equilibrium_objective,
-                self._constraints,
-                **deltas,
-                **self._perturb_options
+                self._eq_objective, self._constraints, **deltas, **self._perturb_options
             )
-            self._eq.solve(objective=self._equilibrium_objective, **self._solve_options)
-            self._xopt_old = xopt
+            self._eq.solve(objective=self._eq_objective, **self._solve_options)
+            self._x_old = x
 
-    def compute(self, xopt):
+    def compute(self, x):
+        """Compute the objective function.
 
-        self._update(xopt)
-        f = self._objective.compute(self._objective.x(self._eq))
-        return f
+        Parameters
+        ----------
+        x : ndarray
+            State vector.
 
-    def grad(self, xopt):
+        Returns
+        -------
+        f : ndarray
+            Objective function value(s).
 
-        f = self.compute(xopt)
-        J = self.jac(xopt)
+        """
+        self._update_equilibrium(x)
+        return super().compute(self._objective.x(self._eq))
+
+    def grad(self, x):
+
+        f = self.compute(x)
+        J = self.jac(x)
         return f.T @ J
 
-    def jac(self, xopt):
+    def jac(self, x):
 
-        self._update(xopt)
+        self._update_equilibrium(x)
 
-        dxdc = np.zeros((self._equilibrium_objective.dim_x, 0))
-        x_idx = np.concatenate(
-            [self._equilibrium_objective.x_idx[arg] for arg in self._args[2:]]
-        )
-        x_idx.sort(kind="mergesort")
-        dxdc = np.eye(self._equilibrium_objective.dim_x)[:, x_idx]
+        dxdc = np.eye(self._eq_objective.dim_x)[:, self._x_idx]
         dxdRb = (
-            np.eye(self._equilibrium_objective.dim_x)[
-                :, self._equilibrium_objective.x_idx["R_lmn"]
-            ]
+            np.eye(self._eq_objective.dim_x)[:, self._eq_objective.x_idx["R_lmn"]]
             @ self._Ainv["R_lmn"]
         )
         dxdc = np.hstack((dxdc, dxdRb))
         dxdZb = (
-            np.eye(self._equilibrium_objective.dim_x)[
-                :, self._equilibrium_objective.x_idx["Z_lmn"]
-            ]
+            np.eye(self._eq_objective.dim_x)[:, self._eq_objective.x_idx["Z_lmn"]]
             @ self._Ainv["Z_lmn"]
         )
         dxdc = np.hstack((dxdc, dxdZb))
 
-        xf = self._equilibrium_objective.x(self._eq)
-        Fx = self._equilibrium_objective.jac(xf)
-
-        # 1st partial derivatives of g objective wrt both x and c
-        xg = self._objective.x(self._eq)
-        Gx = self._objective.jac(xg)
-
+        # Jacobian matrices wrt full state vectors
+        xf = self._eq_objective.x(self._eq)
+        Fx = self._eq_objective.jac(xf)
+        Gx = self._objective.jac(x)
         Fx = {
-            arg: Fx[:, self._equilibrium_objective.x_idx[arg]]
-            for arg in self._equilibrium_objective.args
+            arg: Fx[:, self._eq_objective.x_idx[arg]] for arg in self._eq_objective.args
         }
         Gx = {arg: Gx[:, self._objective.x_idx[arg]] for arg in self._objective.args}
-
-        for arg in arg_order:
-            if (arg in self._objective.args) and not (
-                arg in self._equilibrium_objective.args
-            ):
-                Fx[arg] = jnp.zeros(
-                    (self._equilibrium_objective.dim_f, self._objective.dimensions[arg])
-                )
-            if (arg in self._equilibrium_objective.args) and not (
-                arg in self._objective.args
-            ):
-                Gx[arg] = jnp.zeros(
-                    (self._objective.dim_f, self._equilibrium_objective.dimensions[arg])
-                )
-
+        for arg in self.args:
+            if arg not in Fx.keys():
+                Fx[arg] = jnp.zeros((self._eq_objective.dim_f, self.dimensions[arg]))
+            if arg not in Gx.keys():
+                Gx[arg] = jnp.zeros((self._objective.dim_f, self.dimensions[arg]))
         Fx = jnp.hstack([Fx[arg] for arg in arg_order if arg in Fx])
         Gx = jnp.hstack([Gx[arg] for arg in arg_order if arg in Gx])
 
@@ -189,9 +191,9 @@ class WrappedEquilibriumObjective:
 
         GxFx = Gx @ Fx_inv
         LHS = GxFx @ Fc - Gc
-        return LHS
+        return LHS  # XXX check dimensions, shape[1] should be size of boundary, profiles, Psi
 
-    def hess(self, xopt):
+    def hess(self, x):
 
-        J = self.jac(xopt)
+        J = self.jac(x)
         return J.T @ J

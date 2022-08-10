6fc8f5a648b391f9705a365b480a004200c3ad7f
Dario Panici <dpanici@princeton.edu>
2022-08-10 13:24:56 -0400

    change 1d opt test to not use lsq-exact but instead use a scalar minimization algorithm

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -14,6 +14,7 @@ from desc.objectives import (
     FixPsi,
 )
 from desc.vmec_utils import vmec_boundary_subspace
+from desc.optimize import Optimizer
 from .utils import area_difference_vmec
 
 
@@ -87,7 +88,7 @@ def test_force_balance_grids():
 
 def test_1d_optimization(SOLOVEV):
     """Tests 1D optimization for target aspect ratio."""
-
+    optimizer = Optimizer("scipy-trust-exact")
     eq = EquilibriaFamily.load(load_from=str(SOLOVEV["desc_h5_path"]))[-1]
     objective = ObjectiveFunction(AspectRatio(target=2.5))
     constraints = (
@@ -99,7 +100,7 @@ def test_1d_optimization(SOLOVEV):
         FixPsi(),
     )
     options = {"perturb_options": {"order": 1}}
-    eq.optimize(objective, constraints, options=options)
+    eq.optimize(objective, constraints, options=options, optimizer=optimizer)
 
     np.testing.assert_allclose(eq.compute("V")["R0/a"], 2.5)
 

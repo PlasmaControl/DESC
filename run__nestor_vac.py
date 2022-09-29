1085b059b0e51ad534d0cdb347ba237d1b16935b
Rory Conlin <roryconlin@gmail.com>
2022-09-28 20:08:19 -0400

    Bug fixes for nestor objective

--- run__nestor_vac.py
+++ run__nestor_vac.py
@@ -55,112 +55,221 @@ from desc.compute import (
     compute_contravariant_magnetic_field,
     compute_pressure,
     compute_magnetic_field_magnitude,
-    compute_covariant_magnetic_field    
+    compute_covariant_magnetic_field,
+    compute_jacobian,
 )
 from desc.geometry.utils import rpz2xyz, rpz2xyz_vec, xyz2rpz_vec, rotation_matrix
 from desc.nestor import Nestor
 
 
-
-def compute_I(R_lmn, Z_lmn, L_lmn, i_l, Psi, R_transform, Z_transform, L_transform, i_profile, orientation):
-    data = compute_covariant_magnetic_field(R_lmn, Z_lmn, L_lmn, i_l, Psi, R_transform, Z_transform, L_transform, i_profile, orientation)
+def compute_I(
+    R_lmn,
+    Z_lmn,
+    L_lmn,
+    i_l,
+    c_l,
+    Psi,
+    R_transform,
+    Z_transform,
+    L_transform,
+    i_profile,
+    c_profile,
+    orientation,
+):
+    data = compute_covariant_magnetic_field(
+        R_lmn,
+        Z_lmn,
+        L_lmn,
+        i_l,
+        c_l,
+        Psi,
+        R_transform,
+        Z_transform,
+        L_transform,
+        i_profile,
+        c_profile,
+        orientation,
+    )
     w = R_transform.grid.weights
-    return jnp.sum(data['B_theta']*w)/(mu_0*2*np.pi)
+    return jnp.sum(data["B_theta"] * w) / (mu_0 * 2 * np.pi)
 
-def bsq_vac(R_lmn, Z_lmn, L_lmn, i_l, Psi, R_transform, Z_transform, L_transform, i_profile, orientation, nest):
-    
-    ctor = compute_I(R_lmn, Z_lmn, L_lmn, i_l, Psi, R_transform, Z_transform, L_transform, i_profile, orientation)
+
+def bsq_vac(
+    R_lmn,
+    Z_lmn,
+    L_lmn,
+    i_l,
+    c_l,
+    Psi,
+    R_transform,
+    Z_transform,
+    L_transform,
+    i_profile,
+    c_profile,
+    orientation,
+    nest,
+):
+
+    ctor = compute_I(
+        R_lmn,
+        Z_lmn,
+        L_lmn,
+        i_l,
+        c_l,
+        Psi,
+        R_transform,
+        Z_transform,
+        L_transform,
+        i_profile,
+        c_profile,
+        orientation,
+    )
     out = nest.compute(R_lmn, Z_lmn, ctor)
-    bsq = out[1]['|B|^2']
-    w = nest._Rb_transform.grid.weights
-    return (bsq*w/mu_0).flatten()
+    grid = nest._Rb_transform.grid
+    bsq = out[1]["|B|^2"].reshape((grid.num_zeta, grid.num_theta)).T.flatten()
+    return bsq / (2 * mu_0)
 
-def bsq_plasma(R_lmn, Z_lmn, L_lmn, i_l, Psi, R_transform, Z_transform, L_transform, i_profile, orientation):
-    
-    data = compute_magnetic_field_magnitude(R_lmn, Z_lmn, L_lmn, i_l, Psi, R_transform, Z_transform, L_transform, i_profile, orientation)
-    bsq = data['|B|']**2
-    g = data['|e_theta x e_zeta|']
-    w = R_transform.grid.weights
-    return (bsq*g*w/2/mu_0)
 
+def bsq_plasma(
+    R_lmn,
+    Z_lmn,
+    L_lmn,
+    i_l,
+    c_l,
+    Psi,
+    R_transform,
+    Z_transform,
+    L_transform,
+    i_profile,
+    c_profile,
+    orientation,
+):
+
+    data = compute_magnetic_field_magnitude(
+        R_lmn,
+        Z_lmn,
+        L_lmn,
+        i_l,
+        c_l,
+        Psi,
+        R_transform,
+        Z_transform,
+        L_transform,
+        i_profile,
+        c_profile,
+        orientation,
+    )
+    bsq = data["|B|^2"]
+    return bsq / (2 * mu_0)
 
 
 class BoundaryErrorNESTOR(_Objective):
     _scalar = False
     _linear = False
 
-    def __init__(self,         
-                 ext_field,
-                 eq=None,
-                 target=0,
-                 weight=1,
-                 mf=None,
-                 nf=None,
-                 ntheta=None,
-                 nzeta=None,
-                 name="NESTOR Boundary",
-): 
-        
+    def __init__(
+        self,
+        ext_field,
+        eq=None,
+        target=0,
+        weight=1,
+        mf=None,
+        nf=None,
+        ntheta=None,
+        nzeta=None,
+        name="NESTOR Boundary",
+    ):
+
         self.mf = mf
         self.nf = nf
         self.ntheta = ntheta
         self.nzeta = nzeta
         self.ext_field = ext_field
-        self._callback_fmt = "Boundary Pressure Imbalance: {:10.3e} " + "Pa"
+        self._print_value_fmt = "Boundary Pressure Imbalance: {:10.3e} " + "Pa"
         super().__init__(eq, target, weight, name)
 
-
     def build(self, eq, use_jit=True, verbose=1):
-        
-        self.mf = eq.M+1 if self.mf is None else self.mf
+
+        self.mf = eq.M + 1 if self.mf is None else self.mf
         self.nf = eq.N if self.nf is None else self.nf
-        self.ntheta = 4*eq.M+1 if self.ntheta is None else self.ntheta
-        self.nzeta = 4*eq.N+1 if self.nzeta is None else self.nzeta
-        
+        self.ntheta = 4 * eq.M + 1 if self.ntheta is None else self.ntheta
+        self.nzeta = 4 * eq.N + 1 if self.nzeta is None else self.nzeta
+
         eq._sym = False
         self.nest = Nestor(eq, ext_field, self.mf, self.nf, self.ntheta, self.nzeta)
         eq._sym = True
-        Bgrid = LinearGrid(rho=np.array([1]), theta=np.linspace(0,2*np.pi,self.ntheta, endpoint=False), zeta=np.linspace(0,2*np.pi/eq.NFP,self.nzeta, endpoint=False), NFP=eq.NFP) 
+        Bgrid = LinearGrid(rho=1, theta=self.ntheta, zeta=self.nzeta, NFP=eq.NFP)
         self.BR_transform = Transform(Bgrid, eq.R_basis, derivs=1)
         self.BZ_transform = Transform(Bgrid, eq.Z_basis, derivs=1)
         self.BL_transform = Transform(Bgrid, eq.L_basis, derivs=1)
 
-        Igrid = LinearGrid(M=self.ntheta,N=0, rho=1, NFP=eq.NFP) 
+        Igrid = LinearGrid(theta=self.ntheta, N=0, rho=1, NFP=eq.NFP)
         self.IR_transform = Transform(Igrid, eq.R_basis, derivs=1)
         self.IZ_transform = Transform(Igrid, eq.Z_basis, derivs=1)
         self.IL_transform = Transform(Igrid, eq.L_basis, derivs=1)
 
         self._dim_f = Bgrid.num_nodes
         self.orientation = eq.orientation
-        
+
         self.Bp_profile = eq.pressure.copy()
-        self.Bi_profile = eq.iota.copy()
         self.Ip_profile = eq.pressure.copy()
-        self.Ii_profile = eq.iota.copy()
         self.Bp_profile.grid = Bgrid
-        self.Bi_profile.grid = Bgrid
         self.Ip_profile.grid = Igrid
-        self.Ii_profile.grid = Igrid
-        
-                
+        if eq.iota is not None:
+            self.Bi_profile = eq.iota.copy()
+            self.Bi_profile.grid = Bgrid
+            self.Ii_profile = eq.iota.copy()
+            self.Ii_profile.grid = Igrid
+            self.Bc_profile = None
+            self.Ic_profile = None
+        else:
+            self.Bi_profile = None
+            self.Ii_profile = None
+            self.Bc_profile = eq.current.copy()
+            self.Ic_profile = eq.current.copy()
+            self.Bc_profile.grid = Bgrid
+            self.Ic_profile.grid = Igrid
+
         self._check_dimensions()
         self._set_dimensions(eq)
         self._set_derivatives(use_jit=use_jit)
         self._built = True
-        
-        
 
+    def compute(self, R_lmn, Z_lmn, L_lmn, p_l, i_l, c_l, Psi):
 
-    def compute(self, R_lmn, Z_lmn, L_lmn, p_l, i_l, Psi):
-    
-        bv = bsq_vac(R_lmn, Z_lmn, L_lmn, i_l, Psi, self.IR_transform, self.IZ_transform, self.IL_transform, self.Ii_profile, self.orientation, self.nest)
-        bp = bsq_plasma(R_lmn, Z_lmn, L_lmn, i_l, Psi, self.BR_transform, self.BZ_transform, self.BL_transform, self.Bi_profile, self.orientation)
-        bp = bp.reshape((self.nzeta, self.ntheta)).T.flatten()
-        return mu_0*(bv - bp - self.Bp_profile([1.0]))
-                 
-                 
-
-
+        bv = bsq_vac(
+            R_lmn,
+            Z_lmn,
+            L_lmn,
+            i_l,
+            c_l,
+            Psi,
+            self.IR_transform,
+            self.IZ_transform,
+            self.IL_transform,
+            self.Ii_profile,
+            self.Ic_profile,
+            self.orientation,
+            self.nest,
+        )
+        bp = bsq_plasma(
+            R_lmn,
+            Z_lmn,
+            L_lmn,
+            i_l,
+            c_l,
+            Psi,
+            self.BR_transform,
+            self.BZ_transform,
+            self.BL_transform,
+            self.Bi_profile,
+            self.Bc_profile,
+            self.orientation,
+        )
+        data = compute_jacobian(R_lmn, Z_lmn, self.BR_transform, self.BZ_transform)
+        w = self.BR_transform.grid.weights
+        g = data["|e_theta x e_zeta|"]
+        return mu_0 * (bv - bp - self.Bp_profile([1.0])) * w * g
 
 
 f = Dataset("wout_test_iota.vmec.nc")
@@ -255,7 +364,7 @@ out = eq1._optimize(
     ObjectiveFunction(fb_objective),
     maxiter=60,
     verbose=3,
-    perturb_options={"order":2, "dZb":True, "dRb":True, "tr_ratio":[0.01, 0.01]}
+    perturb_options={"order": 2, "dZb": True, "dRb": True, "tr_ratio": [0.01, 0.01]},
 )
 
 eq1.save("run__nestor_vac_out1.h5")
@@ -289,7 +398,7 @@ out = eq2._optimize(
     ObjectiveFunction(fb_objective),
     maxiter=60,
     verbose=3,
-    perturb_options={"order":2, "dZb":True, "dRb":True, "tr_ratio":[0.01, 0.01]}
+    perturb_options={"order": 2, "dZb": True, "dRb": True, "tr_ratio": [0.01, 0.01]},
 )
 
 
@@ -303,7 +412,7 @@ out = veq._optimize(
     ObjectiveFunction(fb_objective),
     maxiter=60,
     verbose=3,
-    perturb_options={"order":2, "dZb":True, "dRb":True, "tr_ratio":[0.01, 0.01]}
+    perturb_options={"order": 2, "dZb": True, "dRb": True, "tr_ratio": [0.01, 0.01]},
 )
 
 veq.save("run__nestor_vac_outv.h5")

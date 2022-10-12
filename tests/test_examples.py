2980c5d7dd0479e049a745c3a1812d3c4c0c9747
Rory Conlin <roryconlin@gmail.com>
2022-10-12 16:18:48 -0400

    Add back manual setting of initial trust radius for test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -248,6 +248,7 @@ def run_qh_step(n, eq):
         verbose=3,
         copy=True,
         options={
+            "initial_trust_radius": 0.5,
             "perturb_options": {"verbose": 0},
             "solve_options": {"verbose": 0},
         },

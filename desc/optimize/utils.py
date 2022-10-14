0ad452ebc8848548cd55ea02ecbf99cba437dd7a
Rory Conlin <roryconlin@gmail.com>
2022-10-14 15:58:20 -0400

    Fix comparison in find_matching_inds

--- desc/optimize/utils.py
+++ desc/optimize/utils.py
@@ -317,7 +317,7 @@ def find_matching_inds(arr1, arr2):
     arr1, arr2 = map(np.atleast_2d, (arr1, arr2))
     inds = []
     for i, a in enumerate(arr2):
-        x = np.isclose(arr1, a, rtol=1e-14, atol=1e-14)
+        x = arr1 == a
         j = np.where(x.all(axis=1))[0]
         if len(j):
             inds.append(i)

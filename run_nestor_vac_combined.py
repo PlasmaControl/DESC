iota sign fix

run_nestor_vac_combined.py
@@ -44,12 +44,14 @@ one = np.ones_like(surf.Z_lmn)
 one[surf.Z_basis.modes[:,1] < 0] *= -1
 surf.Z_lmn *= one
 surf.change_resolution(M=1, N=0)
+iota = veq.iota.copy()
+iota.params *= -1
 
 eq = Equilibrium(
     Psi=veq.Psi,
     surface=surf,
     pressure=veq.pressure,
-    iota=-veq.iota,
+    iota=iota,
     spectral_indexing=veq.spectral_indexing,
     sym=veq.sym,
     NFP=veq.NFP,
fba41faa4bc480a1dd2348be566d3ea990059bf6
Rory Conlin <roryconlin@gmail.com>
2022-11-16 20:22:45 -0500

    Ensure we don't keep bad iterates from wrapped objective

--- desc/objectives/_wrappers.py
+++ desc/objectives/_wrappers.py
@@ -123,8 +123,21 @@ class WrappedEquilibriumObjective(ObjectiveFunction):
 
         self._built = True
 
-    def _update_equilibrium(self, x):
-        """Update the internal equilibrium with new boundary, profile etc."""
+    def _update_equilibrium(self, x, store=False):
+        """Update the internal equilibrium with new boundary, profile etc.
+
+        Parameters
+        ----------
+        x : ndarray
+            New values of optimization variables.
+        store : bool
+            Whether the new x should be stored in self.history
+
+        Notes
+        -----
+        After updating, if store=False, self._eq will revert back to the previous
+        solution when store was True
+        """
         if jnp.allclose(x, self._x_old, rtol=1e-14, atol=1e-14):
             pass
         else:
@@ -145,11 +158,20 @@ class WrappedEquilibriumObjective(ObjectiveFunction):
                 constraints=self._constraints,
                 **self._solve_options
             )
+
+        xopt = self._objective.x(self._eq)
+        xeq = self._eq_objective.x(self._eq)
+        if store:
             self._x_old = x
             self._allx.append(x)
-
             for arg in self._full_args:
                 self.history[arg] += [np.asarray(getattr(self._eq, arg)).copy()]
+        else:
+            for arg in self._full_args:
+                val = self.history[arg][-1].copy()
+                if val.size:
+                    setattr(self._eq, arg, val)
+        return xopt, xeq
 
     def compute(self, x):
         """Compute the objective function.
@@ -165,9 +187,8 @@ class WrappedEquilibriumObjective(ObjectiveFunction):
             Objective function value(s).
 
         """
-        self._update_equilibrium(x)
-        x_obj = self._objective.x(self._eq)
-        return self._objective.compute(x_obj)
+        xopt, _ = self._update_equilibrium(x, store=False)
+        return self._objective.compute(xopt)
 
     def grad(self, x):
         """Compute gradient of the sum of squares of residuals.
@@ -199,7 +220,7 @@ class WrappedEquilibriumObjective(ObjectiveFunction):
         J : ndarray
             Jacobian matrix.
         """
-        self._update_equilibrium(x)
+        xg, xf = self._update_equilibrium(x, store=True)
 
         # dx/dc
         x_idx = np.concatenate(
@@ -221,10 +242,6 @@ class WrappedEquilibriumObjective(ObjectiveFunction):
             @ self._Ainv["Z_lmn"]
         )
         dxdc = np.hstack((dxdc, dxdZb))
-
-        # state vectors
-        xf = self._eq_objective.x(self._eq)
-        xg = self._objective.x(self._eq)
 
         # Jacobian matrices wrt combined state vectors
         Fx = self._eq_objective.jac(xf)

Fix weighting for profile objectives

tests/test_objective_funs.py
@@ -107,7 +107,7 @@ class TestObjectiveFunction:
         def test(eq):
             obj = TargetIota(target=1, weight=2, eq=eq)
             iota = obj.compute(eq.R_lmn, eq.Z_lmn, eq.L_lmn, eq.i_l, eq.c_l, eq.Psi)
-            np.testing.assert_allclose(iota, -2)
+            np.testing.assert_allclose(iota, -2 / 3)
 
         test(Equilibrium(iota=PowerSeriesProfile(0)))
         test(Equilibrium(current=PowerSeriesProfile(0)))
@@ -119,7 +119,7 @@ class TestObjectiveFunction:
         def test(eq):
             obj = TargetCurrent(target=1, weight=2, eq=eq, normalize=False)
             I = obj.compute(eq.R_lmn, eq.Z_lmn, eq.L_lmn, eq.i_l, eq.c_l, eq.Psi)
-            np.testing.assert_allclose(I, -2)
+            np.testing.assert_allclose(I, -2 / 3)
 
         test(Equilibrium(iota=PowerSeriesProfile(0)))
         test(Equilibrium(current=PowerSeriesProfile(0)))
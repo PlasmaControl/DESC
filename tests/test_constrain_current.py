a239698ebd333d9d1932881244de9854e21a8c3e
Daniel Dudt <ddudt@pppl.gov>
2023-01-03 16:13:38 -0500

    include iota_rr in tests

--- tests/test_constrain_current.py
+++ tests/test_constrain_current.py
@@ -22,6 +22,8 @@ class _ExactValueProfile:
             return self.eq.compute("current", grid=self.grid)["current"]
         if dr == 1:
             return self.eq.compute("current_r", grid=self.grid)["current_r"]
+        if dr == 2:
+            return self.eq.compute("current_rr", grid=self.grid)["current_rr"]
 
 
 class TestConstrainCurrent:
@@ -30,12 +32,13 @@ class TestConstrainCurrent:
     @pytest.mark.unit
     @pytest.mark.solve
     def test_compute_rotational_transform(self, DSHAPE_current, HELIOTRON_vac):
-        """Test that compute_rotational_transform recovers iota and iota_r.
+        """Test that compute functions recover iota and radial derivatives.
 
         When the current is fixed to the current computed on an equilibrium
         solved with iota fixed.
 
-        This tests that compute_rotational_transform is correct, among other things.
+        This tests that rotational transform computations from fixed-current profiles
+        are correct, among other things.
         """
 
         def test(stellarator, grid_type):
@@ -46,8 +49,6 @@ class TestConstrainCurrent:
                 f = ConcentricGrid if grid_type == "concentric" else LinearGrid
                 grid = f(L=eq.L_grid, M=eq.M_grid, N=eq.N_grid, NFP=eq.NFP, sym=eq.sym)
 
-            transforms = get_transforms("iota_r", eq=eq, grid=grid)
-            profiles = {"iota": None, "current": _ExactValueProfile(eq, grid)}
             params = {
                 "R_lmn": eq.R_lmn,
                 "Z_lmn": eq.Z_lmn,
@@ -56,13 +57,15 @@ class TestConstrainCurrent:
                 "c_l": None,
                 "Psi": eq.Psi,
             }
+            transforms = get_transforms("iota_rr", eq=eq, grid=grid)
+            profiles = {"iota": None, "current": _ExactValueProfile(eq, grid)}
             data = compute_fun(
-                ["iota", "iota_r"],
+                ["iota", "iota_r", "iota_rr"],
                 params=params,
                 transforms=transforms,
                 profiles=profiles,
             )
-            benchmark_data = eq.compute("iota_r", grid=grid)
+            benchmark_data = eq.compute("iota_rr", grid=grid)
 
             if grid_type == "linear":
                 # ignore axis
@@ -74,9 +77,14 @@ class TestConstrainCurrent:
                     compress(grid, data["iota_r"])[1:],
                     compress(grid, benchmark_data["iota_r"])[1:],
                 )
+                np.testing.assert_allclose(
+                    compress(grid, data["iota_rr"])[1:],
+                    compress(grid, benchmark_data["iota_rr"])[1:],
+                )
             else:
                 np.testing.assert_allclose(data["iota"], benchmark_data["iota"])
                 np.testing.assert_allclose(data["iota_r"], benchmark_data["iota_r"])
+                np.testing.assert_allclose(data["iota_rr"], benchmark_data["iota_rr"])
 
         for e in ("quadrature", "concentric", "linear"):
             # works with any stellarators in desc/examples

6614824ea3d095b3c76ef2642f144d29734fbf9e
Daniel Dudt <ddudt@pppl.gov>
2023-02-01 14:00:14 -0500

    add NFP to data index

--- test.py
+++ test.py
@@ -1,23 +1,10 @@
-import numpy as np
+from desc import set_device
+
+set_device("gpu")
+
 from desc.equilibrium import Equilibrium
+from desc.objectives import ObjectiveFunction, QuasiIsodynamic
 
-eq_sym1 = Equilibrium(M=3, N=2, sym=True)
-eq_asym1 = Equilibrium(M=3, N=2, sym=False)
-
-eq_sym2 = eq_asym1.copy()
-eq_asym2 = eq_sym1.copy()
-
-eq_sym2.change_resolution(sym=True)
-eq_asym2.change_resolution(sym=False)
-
-np.testing.assert_allclose(eq_sym1.R_lmn, eq_sym2.R_lmn)
-np.testing.assert_allclose(eq_sym1.Z_lmn, eq_sym2.Z_lmn)
-np.testing.assert_allclose(eq_sym1.L_lmn, eq_sym2.L_lmn)
-np.testing.assert_allclose(eq_sym1.Rb_lmn, eq_sym2.Rb_lmn)
-np.testing.assert_allclose(eq_sym1.Zb_lmn, eq_sym2.Zb_lmn)
-
-np.testing.assert_allclose(eq_asym1.R_lmn, eq_asym2.R_lmn)
-np.testing.assert_allclose(eq_asym1.Z_lmn, eq_asym2.Z_lmn)
-np.testing.assert_allclose(eq_asym1.L_lmn, eq_asym2.L_lmn)
-np.testing.assert_allclose(eq_asym1.Rb_lmn, eq_asym2.Rb_lmn)
-np.testing.assert_allclose(eq_asym1.Zb_lmn, eq_asym2.Zb_lmn)
+eq = Equilibrium()
+obj = ObjectiveFunction(QuasiIsodynamic(M_booz=eq.M, N_booz=eq.N), eq)
+f = obj.compute(obj.x(eq))

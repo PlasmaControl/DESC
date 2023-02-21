d65ac2ffa5ced926aab255250ca79922a28f2566
Daniel Dudt <ddudt@pppl.gov>
2023-02-21 16:21:01 -0500

    make QI objective dimensionless

--- desc/objectives/_qs.py
+++ desc/objectives/_qs.py
@@ -556,7 +556,7 @@ class QuasiIsodynamic(_Objective):
 
     _scalar = False
     _linear = False
-    _units = "(T)"
+    _units = "(dimensionless)"  # _units = "(T)"
     _print_value_fmt = "Quasi-isodynamic error: {:10.3e} "
 
     def __init__(
@@ -565,8 +565,8 @@ class QuasiIsodynamic(_Objective):
         target=0,
         bounds=None,
         weight=1,
-        normalize=True,
-        normalize_target=True,
+        normalize=False,
+        normalize_target=False,
         grid=None,
         L_QI=3,
         M_QI=1,
@@ -663,9 +663,11 @@ class QuasiIsodynamic(_Objective):
         if verbose > 1:
             timer.disp("Precomputing transforms")
 
+        """
         if self._normalize:
             scales = compute_scaling_factors(eq)
             self._normalization = scales["B"]
+        """
 
         super().build(eq=eq, use_jit=use_jit, verbose=verbose)
 
@@ -706,7 +708,7 @@ class QuasiIsodynamic(_Objective):
             transforms=self._transforms,
             profiles=self._profiles,
         )
-        return data["f_QI"] * self.grid.weights
+        return data["f_QI"] * self.grid.weights / jnp.mean(params["QI_l"])
 
     def _set_dimensions(self, eq):
         """Set state vector component dimensions."""

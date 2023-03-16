Use correct jacobian for constraints and perturbations

desc/objectives/utils.py
@@ -258,7 +258,7 @@ def factorize_linear_constraints(constraints, objective_args):  # noqa: C901
         if obj.bounds is not None:
             raise ValueError("Linear constraints must use target instead of bounds.")
         A_ = {
-            arg: obj.derivatives["jac_unscaled"][arg](
+            arg: obj.derivatives["jac_scaled"][arg](
                 *[jnp.zeros(obj.dimensions[arg]) for arg in obj.args]
             )
             for arg in args

desc/optimize/_constraint_wrappers.py
@@ -440,19 +440,17 @@ class ProximalProjection(ObjectiveFunction):
         self._dxdc = np.eye(self._dim_x_full)[:, x_idx]
         if "Rb_lmn" in self._args:
             c = get_instance(self._linear_constraints, BoundaryRSelfConsistency)
-            A = c.derivatives["jac"]["R_lmn"](
+            A = c.derivatives["jac_unscaled"]["R_lmn"](
                 *[jnp.zeros(c.dimensions[arg]) for arg in c.args]
             )
-            A = (c.normalization * (A.T / c.weight)).T
             Ainv = np.linalg.pinv(A)
             dxdRb = np.eye(self._dim_x_full)[:, self._x_idx_full["R_lmn"]] @ Ainv
             self._dxdc = np.hstack((self._dxdc, dxdRb))
         if "Zb_lmn" in self._args:
             c = get_instance(self._linear_constraints, BoundaryZSelfConsistency)
-            A = c.derivatives["jac"]["Z_lmn"](
+            A = c.derivatives["jac_unscaled"]["Z_lmn"](
                 *[jnp.zeros(c.dimensions[arg]) for arg in c.args]
             )
-            A = (c.normalization * (A.T / c.weight)).T
             Ainv = np.linalg.pinv(A)
             dxdZb = np.eye(self._dim_x_full)[:, self._x_idx_full["Z_lmn"]] @ Ainv
             self._dxdc = np.hstack((self._dxdc, dxdZb))

desc/perturbations.py
@@ -199,19 +199,17 @@ def perturb(  # noqa: C901 - FIXME: break this up into simpler pieces
     tangents = np.zeros((objective.dim_x,))
     if "Rb_lmn" in deltas.keys():
         con = get_instance(constraints, BoundaryRSelfConsistency)
-        A = con.derivatives["jac"]["R_lmn"](
+        A = con.derivatives["jac_unscaled"]["R_lmn"](
             *[np.zeros(con.dimensions[arg]) for arg in con.args]
         )
-        A = (con.normalization * (A.T / con.weight)).T
         Ainv = np.linalg.pinv(A)
         dc = deltas["Rb_lmn"]
         tangents += np.eye(objective.dim_x)[:, objective.x_idx["R_lmn"]] @ Ainv @ dc
     if "Zb_lmn" in deltas.keys():
         con = get_instance(constraints, BoundaryZSelfConsistency)
-        A = con.derivatives["jac"]["Z_lmn"](
+        A = con.derivatives["jac_unscaled"]["Z_lmn"](
             *[np.zeros(con.dimensions[arg]) for arg in con.args]
         )
-        A = (con.normalization * (A.T / con.weight)).T
         Ainv = np.linalg.pinv(A)
         dc = deltas["Zb_lmn"]
         tangents += np.eye(objective.dim_x)[:, objective.x_idx["Z_lmn"]] @ Ainv @ dc
@@ -595,19 +593,17 @@ def optimal_perturb(  # noqa: C901 - FIXME: break this up into simpler pieces
         dxdc = np.eye(objective_f.dim_x)[:, x_idx]
     if "Rb_lmn" in deltas.keys():
         con = get_instance(constraints, BoundaryRSelfConsistency)
-        A = con.derivatives["jac"]["R_lmn"](
+        A = con.derivatives["jac_unscaled"]["R_lmn"](
             *[np.zeros(con.dimensions[arg]) for arg in con.args]
         )
-        A = (con.normalization * (A.T / con.weight)).T
         Ainv = np.linalg.pinv(A)
         dxdRb = np.eye(objective_f.dim_x)[:, objective_f.x_idx["R_lmn"]] @ Ainv
         dxdc = np.hstack((dxdc, dxdRb))
     if "Zb_lmn" in deltas.keys():
         con = get_instance(constraints, BoundaryZSelfConsistency)
-        A = con.derivatives["jac"]["Z_lmn"](
+        A = con.derivatives["jac_unscaled"]["Z_lmn"](
             *[np.zeros(con.dimensions[arg]) for arg in con.args]
         )
-        A = (con.normalization * (A.T / con.weight)).T
         Ainv = np.linalg.pinv(A)
         dxdZb = np.eye(objective_f.dim_x)[:, objective_f.x_idx["Z_lmn"]] @ Ainv
         dxdc = np.hstack((dxdc, dxdZb))
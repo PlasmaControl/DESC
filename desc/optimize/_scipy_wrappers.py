Fix scaling, use auto scaling with scipy methods

desc/optimize/_scipy_wrappers.py
@@ -9,6 +9,8 @@ from desc.backend import jnp
 from .optimizer import register_optimizer
 from .utils import (
     check_termination,
+    compute_hess_scale,
+    compute_jac_scale,
     evaluate_quadratic_form_hess,
     evaluate_quadratic_form_jac,
     f_where_x,
@@ -97,17 +99,18 @@ def _optimize_scipy_minimize(  # noqa: C901 - FIXME: simplify this
 
     """
     assert constraint is None, f"method {method} doesn't support constraints"
-    x_scale = 1 if x_scale == "auto" else x_scale
     options = {} if options is None else options
     options.setdefault("maxiter", np.iinfo(np.int32).max)
     options.setdefault("disp", False)
-    x_scale = 1 if x_scale == "auto" else x_scale
-    if isinstance(x_scale, str):
-        raise ValueError(f"Method {method} does not support x_scale type {x_scale}")
-    if method in ["scipy-trust-exact", "scipy-trust-ncg"]:
-        options.setdefault("initial_trust_radius", 1e-2 * np.linalg.norm(x0 * x_scale))
     fun, grad, hess = objective.compute_scalar, objective.grad, objective.hess
-
+    if isinstance(x_scale, str) and x_scale == "auto":
+        H = hess(x0)
+        scale, _ = compute_hess_scale(H)
+    else:
+        scale = x_scale
+    if method in ["scipy-trust-exact", "scipy-trust-ncg"]:
+        options.setdefault("initial_trust_radius", 1e-2 * np.linalg.norm(x0 / scale))
+        options.setdefault("max_trust_radius", np.inf)
     # need to use some "global" variables here
     allx = []
     func_allx = []
@@ -121,30 +124,42 @@ def _optimize_scipy_minimize(  # noqa: C901 - FIXME: simplify this
 
     def fun_wrapped(xs):
         # record all the x and fs we see
-        x = xs / x_scale
-        func_allx.append(x)
-        f = fun(x)
-        func_allf.append(f)
+        x = xs * scale
+        if len(func_allx):
+            f = f_where_x(x, func_allx, func_allf, dim=0)
+        else:
+            f = np.array([])
+        if not f.size:
+            func_allx.append(x)
+            f = fun(x)
+            func_allf.append(f)
         return f
 
     def grad_wrapped(xs):
         # record all the x and grad we see
-        x = xs / x_scale
-        grad_allx.append(x)
-        g = grad(x)
-        grad_allf.append(g)
-        # need to use callback here since otherwise callback is called even
-        # after unsuccessful steps which we don't want.
-        callback(x)
-        return g / x_scale
+        x = xs * scale
+        if len(grad_allx):
+            g = f_where_x(x, grad_allx, grad_allf, dim=1)
+        else:
+            g = np.array([])
+        if not g.size:
+            grad_allx.append(x)
+            g = grad(x)
+            grad_allf.append(g)
+        return g * scale
 
     def hess_wrapped(xs):
         # record all the x and hess we see
-        x = xs / x_scale
-        hess_allx.append(x)
-        H = hess(x)
-        hess_allf.append(H)
-        return H / (np.atleast_2d(x_scale).T * np.atleast_2d(x_scale))
+        x = xs * scale
+        if len(hess_allx):
+            H = f_where_x(x, hess_allx, hess_allf, dim=2)
+        else:
+            H = np.array([[]])
+        if not H.size:
+            hess_allx.append(x)
+            H = hess(x)
+            hess_allf.append(H)
+        return H * (np.atleast_2d(scale).T * np.atleast_2d(scale))
 
     hess_wrapped = None if method in ["scipy-bfgs", "scipy-CG"] else hess_wrapped
 
@@ -214,7 +229,7 @@ def _optimize_scipy_minimize(  # noqa: C901 - FIXME: simplify this
     try:
         result = scipy.optimize.minimize(
             fun_wrapped,
-            x0=x0,
+            x0=x0 / scale,
             args=(),
             method=method.replace("scipy-", ""),
             jac=grad_wrapped,
@@ -531,10 +546,13 @@ def _optimize_scipy_constrained(  # noqa: C901 - FIXME: simplify this
     options = {} if options is None else options
     options.setdefault("maxiter", np.iinfo(np.int32).max)
     options.setdefault("disp", False)
-    x_scale = 1 if x_scale == "auto" else x_scale
-    if isinstance(x_scale, str):
-        raise ValueError(f"Method {method} does not support x_scale type {x_scale}")
     fun, grad, hess = objective.compute_scalar, objective.grad, objective.hess
+
+    if isinstance(x_scale, str) and x_scale == "auto":
+        H = hess(x0)
+        scale, _ = compute_hess_scale(H)
+    else:
+        scale = x_scale
 
     if constraint is not None:
         num_equality = np.count_nonzero(constraint.bounds[0] == constraint.bounds[1])
@@ -544,14 +562,54 @@ def _optimize_scipy_constrained(  # noqa: C901 - FIXME: simplify this
                 + "equality constraints than free variables. Suggest reducing the grid "
                 + "resolution of constraints"
             )
-        constraint_wrapped = NonlinearConstraint(
-            constraint.compute_unscaled,
-            constraint.bounds[0],
-            constraint.bounds[1],
-            constraint.jac_unscaled,
-        )
+        if isinstance(x_scale, str) and x_scale == "auto":
+            J = constraint.jac_scaled(x0)
+            Jscale, _ = compute_jac_scale(J)
+            scale = jnp.sqrt(scale * Jscale)
+
+        def cfun_wrapped(xs):
+            # record all the x and fs we see
+            x = xs * scale
+            if len(cfun_allx):
+                f = f_where_x(x, cfun_allx, cfun_allf, dim=1)
+            else:
+                f = np.array([])
+            if not f.size:
+                cfun_allx.append(x)
+                f = constraint.compute_scaled(x)
+                cfun_allf.append(f)
+            return f
+
+        def cjac_wrapped(xs):
+            x = xs * scale
+            if len(cjac_allx):
+                J = f_where_x(x, cjac_allx, cjac_allf, dim=2)
+            else:
+                J = np.array([[]])
+            if not J.size:
+                cjac_allx.append(x)
+                J = constraint.jac_scaled(x)
+                cjac_allf.append(J)
+            return J * scale
+
+        lb, ub = constraint.bounds
     else:
-        constraint_wrapped = None
+
+        def cfun_wrapped(xs):
+            return 0.0
+
+        def cjac_wrapped(xs):
+            return jnp.zeros_like(xs)
+
+        lb, ub = 0, 0
+
+    constraint_wrapped = NonlinearConstraint(
+        cfun_wrapped,
+        lb,
+        ub,
+        cjac_wrapped,
+    )
+
     # need to use some "global" variables here
     allx = []
     func_allx = []
@@ -560,35 +618,51 @@ def _optimize_scipy_constrained(  # noqa: C901 - FIXME: simplify this
     grad_allf = []
     hess_allx = []
     hess_allf = []
+    cfun_allx = []
+    cfun_allf = []
+    cjac_allx = []
+    cjac_allf = []
     message = [""]
     success = [None]
 
     def fun_wrapped(xs):
         # record all the x and fs we see
-        x = xs / x_scale
-        func_allx.append(x)
-        f = fun(x)
-        func_allf.append(f)
+        x = xs * scale
+        if len(func_allx):
+            f = f_where_x(x, func_allx, func_allf, dim=0)
+        else:
+            f = np.array([])
+        if not f.size:
+            func_allx.append(x)
+            f = fun(x)
+            func_allf.append(f)
         return f
 
     def grad_wrapped(xs):
         # record all the x and grad we see
-        x = xs / x_scale
-        grad_allx.append(x)
-        g = grad(x)
-        grad_allf.append(g)
-        # need to use callback here since otherwise callback is called even
-        # after unsuccessful steps which we don't want.
-        callback(x)
-        return g / x_scale
+        x = xs * scale
+        if len(grad_allx):
+            g = f_where_x(x, grad_allx, grad_allf, dim=1)
+        else:
+            g = np.array([])
+        if not g.size:
+            grad_allx.append(x)
+            g = grad(x)
+            grad_allf.append(g)
+        return g * scale
 
     def hess_wrapped(xs):
         # record all the x and hess we see
-        x = xs / x_scale
-        hess_allx.append(x)
-        H = hess(x)
-        hess_allf.append(H)
-        return H / (np.atleast_2d(x_scale).T * np.atleast_2d(x_scale))
+        x = xs * scale
+        if len(hess_allx):
+            H = f_where_x(x, hess_allx, hess_allf, dim=2)
+        else:
+            H = np.array([[]])
+        if not H.size:
+            hess_allx.append(x)
+            H = hess(x)
+            hess_allf.append(H)
+        return H * (np.atleast_2d(scale).T * np.atleast_2d(scale))
 
     hess_wrapped = None if method in ["scipy-SLSQP"] else hess_wrapped
 
@@ -623,10 +697,7 @@ def _optimize_scipy_constrained(  # noqa: C901 - FIXME: simplify this
             else:
                 reduction_ratio = 0
 
-        constr_violation = (
-            0 if constraint is None else np.max(np.abs(constraint.compute_scaled(x1)))
-        )
-
+        constr_violation = np.max(np.abs(jnp.clip(cfun_wrapped(x1 / scale), lb, ub)))
         if verbose > 1:
             print_iteration_nonlinear(
                 len(allx),
@@ -670,12 +741,12 @@ def _optimize_scipy_constrained(  # noqa: C901 - FIXME: simplify this
     try:
         result = scipy.optimize.minimize(
             fun_wrapped,
-            x0=x0,
+            x0=x0 / scale,
             args=(),
             method=method.replace("scipy-", ""),
             jac=grad_wrapped,
             hess=hess_wrapped,
-            constraints=constraint_wrapped,
+            constraints=(constraint_wrapped if constraint else None),
             tol=EPS,
             options=options,
         )
@@ -685,7 +756,7 @@ def _optimize_scipy_constrained(  # noqa: C901 - FIXME: simplify this
         result["nhev"] = len(hess_allx)
         result["nit"] = len(allx)
         result["constr_violation"] = np.max(
-            np.abs(constraint.compute_scaled(result["x"]))
+            np.abs(jnp.clip(cfun_wrapped(result["x"] / scale), lb, ub))
         )
     except StopIteration:
         x = grad_allx[-1]
@@ -702,9 +773,7 @@ def _optimize_scipy_constrained(  # noqa: C901 - FIXME: simplify this
             grad=g,
             hess=H,
             optimality=np.linalg.norm(g, ord=np.inf),
-            constr_violation=0
-            if constraint is None
-            else np.max(np.abs(constraint.compute_scaled(x))),
+            constr_violation=np.max(np.abs(jnp.clip(cfun_wrapped(x / scale), lb, ub))),
             nfev=len(func_allx),
             ngev=len(grad_allx),
             nhev=len(hess_allx),
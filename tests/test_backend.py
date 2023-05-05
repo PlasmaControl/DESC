dd97566d357d9fe8f1618231b686a60d4ca96d95
Kaya Unalmis <kayaunalmis@proton.me>
2023-05-05 12:46:37 -0500

    add backend test

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pytest
 
-from desc.backend import put, sign
+from desc.backend import put, sign, vmap
 
 
 @pytest.mark.unit
@@ -23,3 +23,17 @@ def test_sign():
     assert sign(4) == 1
     assert sign(0) == 1
     assert sign(-10.3) == -1
+
+
+@pytest.mark.unit
+def test_vmap():
+    """Test lax numpy implementation of Python's map function."""
+    a = np.arange(6)
+    inputs = np.stack([a, a[::-1], -a])
+
+    def f(x):
+        return x[: x.size // 2] ** 3
+
+    outputs = np.array([[0, 1, 8], [125, 64, 27], [0, -1, -8]])
+    np.testing.assert_allclose(vmap(f, out_axes=0)(inputs), outputs)
+    np.testing.assert_allclose(vmap(f, out_axes=1)(inputs), outputs.T)

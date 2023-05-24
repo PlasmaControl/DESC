5368f3bf8b6e3f0eff66e2928d045b26415ebb8e
Dario Panici <dpanici@princeton.edu>
2023-05-24 00:53:05 -0400

    add test script to make csv files from DESC equilibria

--- database_converter.py
+++ database_converter.py
@@ -1,5 +1,6 @@
 """Functions to convert DESC or VMEC output files into .csv files for the Datbase."""
-
+import csv
+import os
 from datetime import date
 
 import numpy as np
@@ -8,55 +9,33 @@ from desc.equilibrium import EquilibriaFamily
 from desc.grid import LinearGrid
 from desc.io import load
 from desc.io.hdf5_io import hdf5Reader
-from desc.vmec_utils import (
-    fourier_to_zernike,
-    ptolemy_identity_fwd,
-    ptolemy_identity_rev,
-    zernike_to_fourier,
-)
+from desc.vmec_utils import ptolemy_identity_rev, zernike_to_fourier
 
 
 def get_DESC_runid(eq):  # or take in the data from a DESC eq?
-    """Take in a DESC equilibrium and return a unique hash for the run.
-
-    Args:
-    ----
-        eq (_type_): _description_
-    """
+    """Take in a DESC equilibrium and return a unique hash for the run."""
     return None
 
 
 def get_config_hash(eq):  # or take in the data from a DESC eq?
-    """Take in a DESC equilibrium and return a unique hash for the configuration.
-
-    Args:
-    ----
-        eq (_type_): _description_
-    """
-
+    """Take in a DESC equilibrium and return a unique hash for the configuration."""
     # what to do? first 10 bdry modes and their strengths?
-
     return None
 
 
-def desc_to_csv(
+def desc_to_csv(  # noqa
     eq,
-    filename=None,
-    inputfilename=None,
-    publications=None,
-    version=None,
+    current=True,
     **kwargs,
 ):
     """Save DESC as a csv with relevant information.
 
     Args
     ----
-        eq (Equilibrium or str): DESC equilibrium to save or path to .h5 of DESC equilibrium to save
-        filename: name of csv file to save to
-        inputfilename (str): Path to DESC input file or script used to create the DESC equilibrium
-
-        filename: name of csv file to save to
-
+        eq (Equilibrium or str): DESC equilibrium to save or path to .h5 of
+         DESC equilibrium to save
+        current: bool, if the equilibrium was solved with fixed current or not
+            if False, was solved with fixed iota
 
     Returns
     -------
@@ -66,17 +45,23 @@ def desc_to_csv(
     data_desc_runs = {}
     data_configurations = {}
 
+    desc_runs_csv_name = "desc_runs.csv"
+    configurations_csv_name = "configurations.csv"
+
     if isinstance(eq, str):
-        data_desc_runs["outputfile"] = eq
-        eq = load(eq)
+        data_desc_runs["outputfile"] = os.path.basename(eq)
         reader = hdf5Reader(eq)
         version = reader.read_dict()["__version__"]
+        eq = load(eq)
+
     elif isinstance(eq, EquilibriaFamily):
         eq = eq[-1]  # just use last equilibrium
-        runid = None
+        runid = None  # TODO: is this needed, the equilibriaFamily being an input?
         savename = kwargs.pop("savename", f"DESC_eq_runid_{runid}.h5")
         data_desc_runs["outputfile"] = savename
         from desc import __version__ as version
+    if isinstance(eq, EquilibriaFamily):
+        eq = eq[-1]
 
     ############ DESC_runs Data Table ############
     # FIXME: what to do for these?
@@ -97,14 +82,11 @@ def desc_to_csv(
     data_desc_runs[
         "git_commit"
     ] = version  # this is basically redundant with git commit I think
-    if inputfilename:
-        data_desc_runs["inputfilename"] = inputfilename
-    else:
-        pass  # make an input file? though this would not necessarily correlate to the same exact equilibrium
+    data_desc_runs["inputfilename"] = kwargs.pop("inputfilename", None)
 
     data_desc_runs["initialization_method"] = kwargs.pop(
-        "initialization_method", None
-    )  # we don't usually store this information in the DESC equilibrium
+        "initialization_method", "surface"
+    )
 
     data_desc_runs["l_rad"] = eq.L
     data_desc_runs["l_grid"] = eq.L_grid
@@ -118,15 +100,44 @@ def desc_to_csv(
     ] = 1.0  # this is not a equilibrium property, so should not be saved
 
     # save profiles
-    # TODO: note which ones were fixed for the solve? or only include what was fixed in the solve?
 
-    rho = np.linspace(0, 1.0, 10, endpoint=True)
-    if eq.iota:
-        data_desc_runs["iota_profile"] = eq.iota(rho)
-    else:
+    rho = np.linspace(0, 1.0, 11, endpoint=True)
+    rho_grid = LinearGrid(rho=rho, M=0, N=0, NFP=eq.NFP)
+    data_desc_runs["profile_rho"] = rho
+    rho_dense = np.linspace(0, 1.0, 101, endpoint=True)
+    rho_grid_dense = LinearGrid(rho=rho_dense, M=0, N=0, NFP=eq.NFP)
+
+    rho_grid.nodes[0, 0] = 1e-12  # bc we dont have axis limit right now
+    rho_grid_dense.nodes[0, 0] = 1e-12  # bc we dont have axis limit right now
+
+    if eq.iota and not current:
+        data_desc_runs["iota_profile"] = eq.iota(rho)  # sohuld name differently
+        data_desc_runs["iota_max"] = np.max(eq.iota(rho_dense))
+
+        data_desc_runs["iota_min"] = np.min(eq.iota(rho_dense))
+
+        data_desc_runs["current_profile"] = round(
+            eq.compute("current", grid=rho_grid)["current"], ndigits=14
+        )  # round to make sure any 0s are actually zero
+        data_configurations["current_specification"] = "iota"
+        data_desc_runs["current_specification"] = "iota"
+    elif eq.current and current:
         data_desc_runs["current_profile"] = eq.current(rho)
+        data_desc_runs["iota_profile"] = round(
+            eq.compute("iota", grid=rho_grid)["iota"], ndigits=14
+        )
+        data_desc_runs["iota_max"] = np.max(
+            eq.compute("iota", grid=rho_grid_dense)["iota"]
+        )
+        data_desc_runs["iota_min"] = np.min(
+            eq.compute("iota", grid=rho_grid_dense)["iota"]
+        )
+        data_configurations["current_specification"] = "net enclosed current"
+        data_desc_runs["current_specification"] = "net enclosed current"
 
     data_desc_runs["pressure_profile"] = eq.pressure(rho)
+    data_desc_runs["pressure_max"] = np.max(eq.pressure(rho_dense))
+    data_desc_runs["pressure_min"] = np.min(eq.pressure(rho_dense))
 
     today = date.today()
     data_desc_runs["date_created"] = kwargs.get("date_created", today)
@@ -137,16 +148,17 @@ def desc_to_csv(
     data_desc_runs["user_updated"] = kwargs.get(
         "user_updated", None
     )  # FIXME: what is this?
-    data_desc_runs["publicationid"] = kwargs.get(
-        "publicationid", None
-    )  # FIXME: what is this?
+    data_desc_runs["publicationid"] = kwargs.get("publicationid", None)
 
     ############ configuration Data Table ############
     data_configurations["configid"] = kwargs.get(
         "configid", None
     )  # FIXME what should this be? how to hash?
     data_configurations["name"] = kwargs.get("name", None)
-    data_configurations["deviceid"] = kwargs.get("name", None)
+    data_configurations["NFP"] = eq.NFP
+    data_configurations["stell_sym"] = bool(eq.sym)
+
+    data_configurations["deviceid"] = kwargs.get("deviceid", None)
     data_configurations["hashkey"] = get_config_hash(eq)  # FIXME: what is this?
 
     # FIXME: Defaults for these?
@@ -165,21 +177,19 @@ def desc_to_csv(
     # surface geometry
     # currently saving as VMEC format but I'd prefer if we could do DESC format...
 
-    xm = np.tile(np.linspace(0, M, M + 1), (2 * N + 1, 1)).T.flatten()[
-        -file.dimensions["mn_mode"].size :
-    ]
-
-    data_configurations["RBC"] = eq.surface.R_lmn
-
     r1 = np.ones_like(eq.R_lmn)
     r1[eq.R_basis.modes[:, 1] < 0] *= -1
     m, n, x_mn = zernike_to_fourier(
         r1 * eq.R_lmn, basis=eq.R_basis, rho=np.array([1.0])
     )
     xm, xn, s, c = ptolemy_identity_rev(m, n, x_mn)
-    data_configurations["RBC"] = c
+
+    data_configurations["m"] = xm
+    data_configurations["n"] = xn
+
+    data_configurations["RBC"] = c[0, :]
     if not eq.sym:
-        data_configurations["RBS"] = s
+        data_configurations["RBS"] = s[0, :]
     else:
         data_configurations["RBS"] = np.zeros_like(c)
     # Z
@@ -195,51 +205,71 @@ def desc_to_csv(
     else:
         data_configurations["ZBC"] = np.zeros_like(s)
 
+    # profiles
+    data_configurations["pressure_profile_type"] = "power_series"
+    data_configurations["pressure_profile_data1"] = eq.pressure.basis.modes[
+        :, 0
+    ]  # these are the mode numbers
+    data_configurations[
+        "pressure_profile_data2"
+    ] = eq.pressure.params  # these are the coefficients
+
+    if eq.current:
+        data_configurations["current_profile_type"] = "power_series"
+        data_configurations["current_profile_data1"] = eq.current.basis.modes[
+            :, 0
+        ]  # these are the mode numbers
+        data_configurations[
+            "current_profile_data2"
+        ] = eq.current.params  # these are the coefficients
+        data_configurations["iota_profile_type"] = None
+        data_configurations["iota_profile_data1"] = None
+        data_configurations["iota_profile_data2"] = None
+
+    elif eq.iota:
+        data_configurations["iota_profile_type"] = "power_series"
+        data_configurations["iota_profile_data1"] = eq.iota.basis.modes[
+            :, 0
+        ]  # these are the mode numbers
+        data_configurations[
+            "iota_profile_data2"
+        ] = eq.iota.params  # these are the coefficients
+        data_configurations["current_profile_type"] = None
+        data_configurations["current_profile_data1"] = None
+        data_configurations["current_profile_data2"] = None
+
+    data_configurations["date_created"] = kwargs.get("date_created", today)
+    data_configurations["user_created"] = kwargs.get(
+        "user_created", None
+    )  # FIXME: what is this?
+    data_configurations["date_updated"] = kwargs.get("date_updated", today)
+    data_configurations["user_updated"] = kwargs.get(
+        "user_updated", None
+    )  # FIXME: what is this?
+
+    csv_columns_desc_runs = list(data_desc_runs.keys())
+    csv_columns_desc_runs.sort()
+    desc_runs_csv_exists = os.path.isfile(desc_runs_csv_name)
+
+    try:
+        with open(desc_runs_csv_name, "a+") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns_desc_runs)
+            if not desc_runs_csv_exists:
+                writer.writeheader()  # only need header if file did not exist already
+            writer.writerow(data_desc_runs)
+    except OSError:
+        print("I/O error")
+    csv_columns_configurations = list(data_configurations.keys())
+    csv_columns_configurations.sort()
+
+    configurations_csv_exists = os.path.isfile(configurations_csv_name)
+    try:
+        with open(configurations_csv_name, "a+") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns_configurations)
+            if not configurations_csv_exists:
+                writer.writeheader()  # only need header if file did not exist already
+            writer.writerow(data_configurations)
+    except OSError:
+        print("I/O error")
+
     return None
-
-
-# TODO: other fields?
-# provenance could be used to include if the file was based off a
-# NAE equilibrium, but maybe if this is supposed to be more
-# common we could also include the NAE parameters...
-
-# Aza would ideally like one CSV per input
-# can take multiple inputs at a time
-# then just output to a few csvs
-
-# one CVS per table
-
-
-# CSV
-# headers of columsn are the different IDs etc
-# each row is a different input (so this fxn will append to these CSVs)
-# one CSV for desc_runs
-# one CSV for configurations
-
-# if there are attributes I think should be added, I can add these here
-
-# use this for saving https://www.tutorialspoint.com/How-to-save-a-Python-Dictionary-to-CSV-file
-
-# or maybe this one https://www.geeksforgeeks.org/how-to-append-a-new-row-to-an-existing-csv-file/
-# check that the file exists, if not do first way, else open it and append a row with this second way
-"""from csv import writer
-
-# List that we want to add as a new row
-List = [6, 'William', 5532, 1, 'UAE']
-
-# Open our existing CSV file in append mode
-# Create a file object for this file
-with open('event.csv', 'a') as f_object:
-
-    # Pass this file object to csv.writer()
-    # and get a writer object
-    writer_object = writer(f_object)
-
-    # Pass the list as an argument into
-    # the writerow()
-    writer_object.writerow(List)
-
-    # Close the file object
-    f_object.close()
-"""
-# so make fieldnames from list(dict.keys()) probably to make it easiest on me
---
+++ try_converter.py
@@ -0,0 +1,41 @@
+"""Test the CSV database input creator."""
+import os
+
+from database_converter import desc_to_csv
+
+names = []
+
+inputs = {}
+outputs = {}
+is_current = {
+    "precise_QA": True,
+    "precise_QH": True,
+    "QAS": True,
+    "DSHAPE_CURRENT": True,
+    "ESTELL": True,
+}
+for root, dirs, files in os.walk("desc/examples"):
+    for file in files:
+        if file.find(".h5") != -1:
+            name = file.split("_output.h5")[0]
+            names.append(name)
+            outputs[name] = file
+for root, dirs, files in os.walk("desc/examples"):
+    for file in files:
+        if file.find(".h5") == -1:
+            for name in names:
+                if file.find(name) != -1:
+                    inputs[name] = file
+for name in names:
+    try:
+        used_current = is_current[name]
+    except KeyError:
+        used_current = False
+    print(f"Saving example {name}")
+    desc_to_csv(
+        f"desc/examples/{name}_output.h5",
+        name=name,
+        provenance="Example Equilibrium From DESC Repository desc/examples folder",
+        inputfilename=inputs[name],
+        current=used_current,
+    )

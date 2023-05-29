ade804069e18f20279e3028010bd2ecd87da3bdc
Daniel Dudt <ddudt@pppl.gov>
2023-05-29 11:49:05 -0400

    update helical and toroidal results

--- publications/dudt2023/helical_qs.py
+++ publications/dudt2023/helical_qs.py
@@ -7,8 +7,8 @@ set_device("gpu")
 import numpy as np
 from qsc import Qsc
 
-from desc.equilibrium import Equilibrium
-from desc.grid import LinearGrid
+from desc.equilibrium import EquilibriaFamily, Equilibrium
+from desc.grid import LinearGrid, QuadratureGrid
 from desc.objectives import (
     CurrentDensity,
     FixOmni,
@@ -16,30 +16,38 @@ from desc.objectives import (
     Omnigenity,
     StraightBmaxContour,
 )
-from desc.objectives.utils import get_NAE_constraints
+from desc.objectives.utils import get_fixed_boundary_constraints, get_NAE_constraints
 from desc.vmec import VMECIO
 
 
+fname = "helical_qs"
 sym = True
 NFP = 5
 helicity = (1, NFP)
-L = 8
-M = 8
-N = 8
+LM = [8, 10, 12]
+N = 12
 L_well = 4
 M_well = 8
 L_omni = 0
 M_omni = 0
 N_omni = 0
 well_weight = 2
+eq_weights = [1e0, 2e0, 4e0]
 aspect_ratio = 20
 surfaces = [0.2, 0.4, 0.6, 0.8, 1.0]
 
-M_booz = int(np.ceil(1.5 * M))
-N_booz = int(np.ceil(1.5 * N))
-M_grid = int(np.ceil(1.5 * M_booz))
-N_grid = int(np.ceil(1.5 * N_booz))
+assert len(LM) == len(eq_weights)
 
+
+def eq_error(eq):
+    grid = QuadratureGrid(L=32, M=32, N=32, NFP=NFP)
+    data = eq.compute(["<|F|>_vol", "<|grad(|B|^2)|/2mu0>_vol"], grid=grid)
+    return data["<|F|>_vol"] / data["<|grad(|B|^2)|/2mu0>_vol"]
+
+
+fam = EquilibriaFamily()
+
+# initial NAE solution
 qsc = Qsc(
     nfp=NFP,
     rc=[
@@ -76,8 +84,8 @@ qsc = Qsc(
 eq = Equilibrium.from_near_axis(
     qsc,
     r=1 / aspect_ratio,
-    L=L,
-    M=M,
+    L=LM[0],
+    M=LM[0],
     N=N,
     L_well=L_well,
     M_well=M_well,
@@ -85,11 +93,13 @@ eq = Equilibrium.from_near_axis(
     M_omni=M_omni,
     N_omni=N_omni,
 )
-constraints = get_NAE_constraints(eq, qsc, order=1) + (
-    FixOmni(),
-    StraightBmaxContour(),
-)
-eq.solve(
+fam.append(eq)
+fam.save(fname + ".h5")
+print("equlibrium error: {:.2e}".format(eq_error(eq)))
+
+# re-solve with NAE constraints
+constraints = get_NAE_constraints(eq, qsc, order=1)
+eq, result = eq.solve(
     objective="vacuum",
     constraints=constraints,
     ftol=1e-2,
@@ -97,36 +107,69 @@ eq.solve(
     gtol=1e-6,
     maxiter=100,
     verbose=3,
+    copy=True,
 )
-eq.save("helical_qs.h5")
+fam.append(eq)
+fam.save(fname + ".h5")
+print("equlibrium error: {:.2e}".format(eq_error(eq)))
 
-grids = {}
-objs = {}
-for rho in surfaces:
-    grids[rho] = LinearGrid(M=M_grid, N=N_grid, NFP=eq.NFP, sym=False, rho=rho)
-    objs[rho] = Omnigenity(
-        bounds=(-1e-3, 1e-3),
-        grid=grids[rho],
-        helicity=helicity,
-        M_booz=M_booz,
-        N_booz=N_booz,
-        well_weight=well_weight,
+# optimize with increasing resolution
+for i in range(len(LM)):
+    eq.change_resolution(L=LM[i], M=LM[i], L_grid=2 * LM[i], M_grid=2 * LM[i])
+    M_booz = min(int(np.ceil(1.5 * LM[i])), 16)
+    N_booz = min(int(np.ceil(1.5 * N)), 16)
+    M_grid = int(np.ceil(1.5 * M_booz))
+    N_grid = int(np.ceil(1.5 * N_booz))
+
+    grids = {}
+    objs = {}
+    for rho in surfaces:
+        grids[rho] = LinearGrid(M=M_grid, N=N_grid, NFP=eq.NFP, sym=False, rho=rho)
+        objs[rho] = Omnigenity(
+            grid=grids[rho],
+            helicity=helicity,
+            M_booz=M_booz,
+            N_booz=N_booz,
+            well_weight=well_weight,
+        )
+
+    objective = ObjectiveFunction(
+        (CurrentDensity(weight=eq_weights[i]),) + tuple(objs.values())
     )
+    constraints = get_NAE_constraints(eq, qsc, order=1) + (
+        FixOmni(),
+        StraightBmaxContour(),
+    )
+    eq, result = eq.solve(
+        objective=objective,
+        constraints=constraints,
+        optimizer="lsq-exact",
+        ftol=1e-3,
+        xtol=1e-6,
+        gtol=1e-6,
+        maxiter=200,
+        verbose=3,
+        copy=True,
+    )
+    fam.append(eq)
+    fam.save(fname + ".h5")
+    print("equlibrium error: {:.2e}".format(eq_error(eq)))
 
-objective = ObjectiveFunction(
-    (CurrentDensity(bounds=(-1e-6, 1e-6), weight=1e0),) + tuple(objs.values())
-)
+# re-solve with fixed boundary constraints
+constraints = get_fixed_boundary_constraints(iota=False)
 eq, result = eq.solve(
-    objective=objective,
+    objective="vacuum",
     constraints=constraints,
-    optimizer="lsq-exact",
-    ftol=1e-3,
+    ftol=1e-2,
     xtol=1e-6,
     gtol=1e-6,
     maxiter=200,
     verbose=3,
     copy=True,
 )
-eq.save("helical_qs.h5")
+fam.append(eq)
+fam.save(fname + ".h5")
+print("equlibrium error: {:.2e}".format(eq_error(eq)))
 
-VMECIO.save(eq, "wout_helical_qs.nc", surfs=256)
+# save wout file
+VMECIO.save(eq, "wout_" + fname + ".nc", surfs=256)
--- publications/dudt2023/toroidal.py
+++ publications/dudt2023/toroidal.py
@@ -5,11 +5,10 @@ from desc import set_device
 set_device("gpu")
 
 import numpy as np
-import os
 from qsc import Qsc
 
-from desc.equilibrium import Equilibrium
-from desc.grid import LinearGrid
+from desc.equilibrium import EquilibriaFamily, Equilibrium
+from desc.grid import LinearGrid, QuadratureGrid
 from desc.objectives import (
     FixOmni,
     ForceBalance,
@@ -17,40 +16,48 @@ from desc.objectives import (
     Omnigenity,
     StraightBmaxContour,
 )
-from desc.objectives.utils import get_NAE_constraints
+from desc.objectives.utils import get_fixed_boundary_constraints, get_NAE_constraints
 from desc.vmec import VMECIO
 
 
+fname = "toroidal"
 sym = True
 NFP = 1
 helicity = (1, 0)
-L = 8
-M = 8
-N = 8
+LM = [8, 10, 12]
+N = 12
 L_well = 4
 M_well = 8
 L_omni = 0
 M_omni = 1
 N_omni = 1
 well_weight = 2
+eq_weights = [5e2, 1e3, 2e3]
 aspect_ratio = 20
+surfaces = [0.2, 0.4, 0.6, 0.8, 1.0]
 target_mode = [0, 1, -1]
 target_amplitude = np.pi / 6
-surfaces = [0.2, 0.4, 0.6, 0.8, 1.0]
 
-M_booz = int(np.ceil(1.5 * M))
-N_booz = int(np.ceil(1.5 * N))
-M_grid = int(np.ceil(1.5 * M_booz))
-N_grid = int(np.ceil(1.5 * N_booz))
+assert len(LM) == len(eq_weights)
 
+
+def eq_error(eq):
+    grid = QuadratureGrid(L=32, M=32, N=32, NFP=NFP)
+    data = eq.compute(["<|F|>_vol", "<|grad(p)|>_vol"], grid=grid)
+    return data["<|F|>_vol"] / data["<|grad(p)|>_vol"]
+
+
+fam = EquilibriaFamily()
+
+# initial NAE solution
 qsc = Qsc(
     nfp=NFP, rc=[1, 0.3], zs=[0, -0.3], B0=1.0, etabar=1.0, I2=1.0, p2=-4e6, order="r1"
 )
 eq = Equilibrium.from_near_axis(
     qsc,
     r=1 / aspect_ratio,
-    L=L,
-    M=M,
+    L=LM[0],
+    M=LM[0],
     N=N,
     L_well=L_well,
     M_well=M_well,
@@ -62,48 +69,83 @@ idx = np.nonzero((eq.omni_basis.modes == target_mode).all(axis=1))[0]
 omni_lmn = np.zeros(eq.omni_basis.num_modes)
 omni_lmn[idx] = target_amplitude
 eq._omni_lmn = omni_lmn
-constraints = get_NAE_constraints(eq, qsc, order=1) + (
-    StraightBmaxContour(),
-    FixOmni(),
-)
-eq.solve(
+fam.append(eq)
+fam.save(fname + ".h5")
+print("equlibrium error: {:.2e}".format(eq_error(eq)))
+
+# re-solve with NAE constraints
+constraints = get_NAE_constraints(eq, qsc, order=1)
+eq, result = eq.solve(
     objective="force",
     constraints=constraints,
     ftol=1e-2,
-    xtol=1e-6,
-    gtol=1e-6,
-    maxiter=100,
-    verbose=3,
-)
-eq.save("toroidal.h5")
-
-grids = {}
-objs = {}
-for rho in surfaces:
-    grids[rho] = LinearGrid(M=M_grid, N=N_grid, NFP=eq.NFP, sym=False, rho=rho)
-    objs[rho] = Omnigenity(
-        bounds=(-1e-3, 1e-3),
-        grid=grids[rho],
-        helicity=helicity,
-        M_booz=M_booz,
-        N_booz=N_booz,
-        well_weight=well_weight,
-    )
-
-objective = ObjectiveFunction(
-    (ForceBalance(bounds=(-1e-6, 1e-6), weight=4e2),) + tuple(objs.values())
-)
-eq, result = eq.solve(
-    objective=objective,
-    constraints=constraints,
-    optimizer="lsq-exact",
-    ftol=1e-3,
     xtol=1e-6,
     gtol=1e-6,
     maxiter=200,
     verbose=3,
     copy=True,
 )
-eq.save("toroidal.h5")
+fam.append(eq)
+fam.save(fname + ".h5")
+print("equlibrium error: {:.2e}".format(eq_error(eq)))
 
-VMECIO.save(eq, "wout_toroidal.nc", surfs=256)
+# optimize with increasing resolution
+for i in range(len(LM)):
+    eq.change_resolution(L=LM[i], M=LM[i], L_grid=2 * LM[i], M_grid=2 * LM[i])
+    M_booz = min(int(np.ceil(1.5 * LM[i])), 16)
+    N_booz = min(int(np.ceil(1.5 * N)), 16)
+    M_grid = int(np.ceil(1.5 * M_booz))
+    N_grid = int(np.ceil(1.5 * N_booz))
+
+    grids = {}
+    objs = {}
+    for rho in surfaces:
+        grids[rho] = LinearGrid(M=M_grid, N=N_grid, NFP=eq.NFP, sym=False, rho=rho)
+        objs[rho] = Omnigenity(
+            grid=grids[rho],
+            helicity=helicity,
+            M_booz=M_booz,
+            N_booz=N_booz,
+            well_weight=well_weight,
+        )
+
+    objective = ObjectiveFunction(
+        (ForceBalance(weight=eq_weights[i]),) + tuple(objs.values())
+    )
+    constraints = get_NAE_constraints(eq, qsc, order=1) + (
+        FixOmni(),
+        StraightBmaxContour(),
+    )
+    eq, result = eq.solve(
+        objective=objective,
+        constraints=constraints,
+        optimizer="lsq-exact",
+        ftol=1e-3,
+        xtol=1e-6,
+        gtol=1e-6,
+        maxiter=200,
+        verbose=3,
+        copy=True,
+    )
+    fam.append(eq)
+    fam.save(fname + ".h5")
+    print("equlibrium error: {:.2e}".format(eq_error(eq)))
+
+# re-solve with fixed boundary constraints
+constraints = get_fixed_boundary_constraints(iota=False)
+eq, result = eq.solve(
+    objective="force",
+    constraints=constraints,
+    ftol=1e-2,
+    xtol=1e-6,
+    gtol=1e-6,
+    maxiter=200,
+    verbose=3,
+    copy=True,
+)
+fam.append(eq)
+fam.save(fname + ".h5")
+print("equlibrium error: {:.2e}".format(eq_error(eq)))
+
+# save wout file
+VMECIO.save(eq, "wout_" + fname + ".nc", surfs=256)

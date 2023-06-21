a6bea173a4543683a34ef49a78a58005fe77d2f6
Rory Conlin <roryconlin@gmail.com>
2023-06-21 18:09:30 -0400

    Add fft interpolation for future use in free boundary

--- desc/interpolate.py
+++ desc/interpolate.py
@@ -8,6 +8,96 @@ import numpy as np
 from desc.backend import jnp
 
 
+def fft_interp1d(f, n, sx=0, dx=1):
+    """Interpolation of a 1d periodic function via FFT.
+
+    Parameters
+    ----------
+    f : ndarray, shape(nx, ...)
+        Source data. Assumed to cover 1 full period, excluding the endpoint.
+    n : int
+        Number of desired interpolation points.
+    sx : float
+        Shift in x to evaluate at. If original data is f(x), interpolates to f(x + sx)
+    dx : float
+        Spacing of source points
+
+    Returns
+    -------
+    fi : ndarray, shape(n, ...)
+        Interpolated (and possibly shifted) data points
+    """
+    c = jnp.fft.ifft(f, axis=0)
+    nx = c.shape[0]
+    sx = jnp.exp(-1j * 2 * jnp.pi * jnp.fft.fftfreq(nx) * sx / dx)
+    c = (c.T * sx).T
+    pad = ((n - nx) // 2, n - nx - (n - nx) // 2)
+    if nx % 2 != 0:
+        pad = pad[::-1]
+    c = jnp.fft.ifftshift(_pad_along_axis(jnp.fft.fftshift(c), pad, axis=0))
+    return jnp.fft.fft(c, axis=0).real
+
+
+def fft_interp2d(f, n1, n2, sx=0, sy=0, dx=1, dy=1):
+    """Interpolation of a 2d periodic function via FFT.
+
+    Parameters
+    ----------
+    f : ndarray, shape(nx, ny, ...)
+        Source data. Assumed to cover 1 full period, excluding the endpoint.
+    n1, n2 : int
+        Number of desired interpolation points in x and y directions
+    sx, sy : float
+        Shift in x and y to evaluate at. If original data is f(x,y), interpolates to
+        f(x + sx, y + sy)
+    dx, dy : float
+        Spacing of source points in x and y
+
+    Returns
+    -------
+    fi : ndarray, shape(n1, n2, ...)
+        Interpolated (and possibly shifted) data points
+    """
+    c = jnp.fft.ifft2(f, axes=(0, 1))
+    nx, ny = c.shape[:2]
+    sx = jnp.exp(-1j * 2 * jnp.pi * jnp.fft.fftfreq(nx) * sx / dx)
+    sy = jnp.exp(-1j * 2 * jnp.pi * jnp.fft.fftfreq(ny) * sy / dy)
+    c = (c.T * sx * sy[:, None]).T
+    padx = ((n1 - nx) // 2, n1 - nx - (n1 - nx) // 2)
+    pady = ((n2 - ny) // 2, n2 - ny - (n2 - ny) // 2)
+    if nx % 2 != 0:
+        padx = padx[::-1]
+    if ny % 2 != 0:
+        pady = pady[::-1]
+
+    c = jnp.fft.ifftshift(
+        _pad_along_axis(jnp.fft.fftshift(c, axes=0), padx, axis=0), axes=0
+    )
+    c = jnp.fft.ifftshift(
+        _pad_along_axis(jnp.fft.fftshift(c, axes=1), pady, axis=1), axes=1
+    )
+
+    return jnp.fft.fft2(c, axes=(0, 1)).real
+
+
+def _pad_along_axis(array, pad=(0, 0), axis=0):
+    """Pad with zeros or truncate a given dimension."""
+    array = jnp.moveaxis(array, axis, 0)
+
+    if pad[0] <= 0:
+        array = array[abs(pad[0]) :]
+        pad = (0, pad[1])
+    if pad[1] <= 0:
+        array = array[: -abs(pad[1])]
+        pad = (pad[0], 0)
+
+    npad = [(0, 0)] * array.ndim
+    npad[0] = pad
+
+    array = jnp.pad(array, pad_width=npad, mode="constant", constant_values=0)
+    return jnp.moveaxis(array, 0, axis)
+
+
 def interp1d(
     xq, x, f, method="cubic", derivative=0, extrap=False, period=None, fx=None, **kwargs
 ):

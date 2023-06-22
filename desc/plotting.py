2a0b5bab8a22e130a5151ddd7c5444c44f07b64f
daniel-dudt <daniel.dudt@princetonstellarators.energy>
2023-06-22 17:10:43 -0400

    improve plot_boozer_surface

--- desc/plotting.py
+++ desc/plotting.py
@@ -13,6 +13,7 @@ from scipy.integrate import solve_ivp
 from scipy.interpolate import Rbf
 from termcolor import colored
 
+from desc.backend import sign
 from desc.basis import fourier, zernike_radial_poly
 from desc.compute import data_index, get_transforms
 from desc.compute.utils import compress, surface_averages
@@ -1385,7 +1386,7 @@ def plot_surfaces(eq, rho=8, theta=8, phi=None, ax=None, return_data=False, **kw
         "zeta": phi,
     }
     if plot_theta:
-        # Note: theta* (also known as vartheta) is the poloidal straight field-line
+        # Note: theta* (also known as vartheta) is the poloidal straight field line
         # angle in PEST-like flux coordinates
         t_grid = _get_grid(**grid_kwargs)
         tnr, tnt, tnz = t_grid.num_rho, t_grid.num_theta, t_grid.num_zeta
@@ -2234,8 +2235,10 @@ def plot_boozer_surface(
     eq,
     grid_compute=None,
     grid_plot=None,
+    rho=1,
     fill=False,
-    ncontours=100,
+    ncontours=30,
+    fieldlines=0,
     ax=None,
     return_data=False,
     **kwargs,
@@ -2250,19 +2253,21 @@ def plot_boozer_surface(
         grid to use for computing boozer spectrum
     grid_plot : Grid, optional
         grid to plot on
+    rho : float, optional
+        Radial coordinate of flux surface. Used only if grids are not specified.
     fill : bool, optional
         Whether the contours are filled, i.e. whether to use `contourf` or `contour`.
     ncontours : int, optional
         Number of contours to plot.
+    fieldlines : int, optional
+        Number of (linearly spaced) magnetic fieldlines to plot. Default is 0 (none).
     ax : matplotlib AxesSubplot, optional
         Axis to plot on.
     return_data : bool
         if True, return the data plotted as well as fig,ax
     **kwargs : dict, optional
         Specify properties of the figure, axis, and plot appearance e.g.::
-
             plot_X(figsize=(4,6),cmap="plasma")
-
         Valid keyword arguments are:
 
         * ``figsize``: tuple of length 2, the size of the figure (to be passed to
@@ -2283,23 +2288,28 @@ def plot_boozer_surface(
     Examples
     --------
     .. image:: ../../_static/images/plotting/plot_boozer_surface.png
-
     .. code-block:: python
-
         from desc.plotting import plot_boozer_surface
         fig, ax = plot_boozer_surface(eq)
 
     """
     if grid_compute is None:
         grid_kwargs = {
-            "M": 6 * eq.M + 1,
-            "N": 6 * eq.N + 1,
+            "rho": rho,
+            "M": 4 * eq.M,
+            "N": 4 * eq.N,
             "NFP": eq.NFP,
             "endpoint": False,
         }
         grid_compute = _get_grid(**grid_kwargs)
     if grid_plot is None:
-        grid_kwargs = {"M": 100, "N": 100, "NFP": eq.NFP, "endpoint": True}
+        grid_kwargs = {
+            "rho": rho,
+            "theta": 91,
+            "zeta": 91,
+            "NFP": eq.NFP,
+            "endpoint": True,
+        }
         grid_plot = _get_grid(**grid_kwargs)
 
     M_booz = kwargs.pop("M_booz", 2 * eq.M)
@@ -2315,6 +2325,7 @@ def plot_boozer_surface(
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         data = eq.compute("|B|_mn", grid=grid_compute, transforms=transforms_compute)
+    iota = compress(grid_compute, data["iota"])
     data = transforms_plot["B"].transform(data["|B|_mn"])
     data = data.reshape((grid_plot.num_theta, grid_plot.num_zeta), order="F")
 
@@ -2335,24 +2346,37 @@ def plot_boozer_surface(
 
     cax_kwargs = {"size": "5%", "pad": 0.05}
 
-    xx = (
+    zz = (
         grid_plot.nodes[:, 2]
         .reshape((grid_plot.num_theta, grid_plot.num_zeta), order="F")
         .squeeze()
     )
-    yy = (
+    tt = (
         grid_plot.nodes[:, 1]
         .reshape((grid_plot.num_theta, grid_plot.num_zeta), order="F")
         .squeeze()
     )
 
     if fill:
-        im = ax.contourf(xx, yy, data, **contourf_kwargs)
+        im = ax.contourf(zz, tt, data, **contourf_kwargs)
     else:
-        im = ax.contour(xx, yy, data, **contourf_kwargs)
+        im = ax.contour(zz, tt, data, **contourf_kwargs)
     cax = divider.append_axes("right", **cax_kwargs)
     cbar = fig.colorbar(im, cax=cax)
     cbar.update_ticks()
+
+    if fieldlines:
+        theta0 = np.linspace(0, 2 * np.pi, fieldlines, endpoint=False)
+        zeta = np.linspace(0, 2 * np.pi / grid_plot.NFP, 100)
+        alpha = np.atleast_2d(theta0) + iota * np.atleast_2d(zeta).T
+        alpha1 = np.where(np.logical_and(alpha >= 0, alpha <= 2 * np.pi), alpha, np.nan)
+        alpha2 = np.where(
+            np.logical_or(alpha < 0, alpha > 2 * np.pi),
+            alpha % (sign(iota) * 2 * np.pi) + (sign(iota) < 0) * (2 * np.pi),
+            np.nan,
+        )
+        alphas = np.hstack((alpha1, alpha2))
+        ax.plot(zeta, alphas, color="k", ls="-", lw=2)
 
     ax.set_xlabel(r"$\zeta_{Boozer}$")
     ax.set_ylabel(r"$\theta_{Boozer}$")
@@ -2360,8 +2384,8 @@ def plot_boozer_surface(
 
     fig.set_tight_layout(True)
     plot_data = {}
-    plot_data["zeta_Boozer"] = xx
-    plot_data["theta_Boozer"] = yy
+    plot_data["zeta_Boozer"] = zz
+    plot_data["theta_Boozer"] = tt
     plot_data["|B|"] = data
 
     if return_data:
@@ -3186,7 +3210,7 @@ def plot_field_lines_sfl(
 
     Traces field lines at specified initial vartheta (:math:`\\vartheta`) seed
     locations, then plots them.
-    Field lines traced by first finding the corresponding straight-field-line (SFL)
+    Field lines traced by first finding the corresponding straight field line (SFL)
     coordinates :math:`(\\rho,\\vartheta,\\phi)` for each field line, then
     converting those to the computational :math:`(\\rho,\\theta,\\phi)` coordinates,
     then finally computing from those the toroidal :math:`(R,\\phi,Z)` coordinates of

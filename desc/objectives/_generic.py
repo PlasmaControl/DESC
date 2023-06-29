80d047876413ad561aa13c4c667c96c2cf22013f
Dario Panici <dpanici@princeton.edu>
2023-06-29 15:02:46 -0400

    add missing docstring for generic objectives

--- desc/objectives/_generic.py
+++ desc/objectives/_generic.py
@@ -211,6 +211,10 @@ class GenericObjective(_Objective):
         User-defined loss function to apply to the objective values once computed.
         Must be a JAX transformable function, e.g. `jnp.mean` for taking the average
         or `lambda x: 3*x`, etc
+    loss_function : function, optional
+        User-defined loss function to apply to the objective values once computed.
+        Must be a JAX transformable function, e.g. `jnp.mean` for taking the average
+        or `lambda x: 3*x`, etc
     grid : Grid, ndarray, optional
         Collocation grid containing the nodes to evaluate at.
     name : str
@@ -330,6 +334,10 @@ class ToroidalCurrent(_Objective):
         Whether target and bounds should be normalized before comparing to computed
         values. If `normalize` is `True` and the target is in physical units,
         this should also be set to True.
+    loss_function : function, optional
+        User-defined loss function to apply to the objective values once computed.
+        Must be a JAX transformable function, e.g. `jnp.mean` for taking the average
+        or `lambda x: 3*x`, etc
     loss_function : function, optional
         User-defined loss function to apply to the objective values once computed.
         Must be a JAX transformable function, e.g. `jnp.mean` for taking the average

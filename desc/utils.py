ce453f7e1a2aae8a5b296b53e8fe5832b2967cd6
Rory Conlin <roryconlin@gmail.com>
2023-07-18 18:04:16 -0400

    Add utility functions for checking inputs

--- desc/utils.py
+++ desc/utils.py
@@ -1,5 +1,6 @@
 """Utility functions, independent of the rest of DESC."""
 
+import numbers
 import warnings
 from itertools import combinations_with_replacement, permutations
 
@@ -487,3 +488,39 @@ def parse_argname_change(arg, kwargs, oldname, newname):
         )
         arg = kwargs.pop(oldname)
     return arg
+
+
+def setdefault(val, default, cond=None):
+    """Return default if val is None or cond is True, otherwise val."""
+    if cond is None:
+        cond = val is not None
+    if cond:
+        return val
+    return default
+
+
+def isnonnegint(x):
+    """Determine if x is a non-negative integer."""
+    return isinstance(x, numbers.Real) and (x == int(x)) and (x >= 0)
+
+
+def isposint(x):
+    """Determine if x is a strictly positive integer."""
+    return isinstance(x, numbers.Real) and (x == int(x)) and (x > 0)
+
+
+def errorif(cond, err=ValueError, msg=""):
+    """Raise an error if condition is met.
+
+    Similar to assert but allows wider range of Error types, rather than
+    just AssertionError.
+    """
+    if cond:
+        raise err(msg)
+
+
+def only1(*args):
+    """Return True if 1 and only 1 of args evaluates to True."""
+    # copied from https://stackoverflow.com/questions/16801322/
+    i = iter(args)
+    return any(i) and not any(i)

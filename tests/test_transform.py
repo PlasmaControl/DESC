81d2a68c59b4bf6333bff2de89a20e30b1a5d06a
daniel-dudt <daniel.dudt@princetonstellarators.energy>
2023-07-18 06:42:17 -0600

    test_volume_chebyshev

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -5,6 +5,7 @@ import pytest
 
 import desc.examples
 from desc.basis import (
+    ChebyshevDoubleFourierBasis,
     DoubleFourierSeries,
     FourierSeries,
     FourierZernikeBasis,
@@ -112,7 +113,36 @@ class TestTransform:
         np.testing.assert_allclose(dtz, correct_dtz, atol=1e-8)
 
     @pytest.mark.unit
-    def test_volume(self):
+    def test_volume_chebyshev(self):
+        """Tests transform of Chebyshev-Fourier basis in a toroidal volume."""
+        grid = ConcentricGrid(L=4, M=2, N=2)
+        basis = ChebyshevDoubleFourierBasis(L=1, M=1, N=1, sym="sin")
+        transf = Transform(grid, basis)
+
+        r = grid.nodes[:, 0]  # rho coordinates
+        t = grid.nodes[:, 1]  # theta coordinates
+        z = grid.nodes[:, 2]  # zeta coordinates
+
+        x = 2 * r - 1
+        correct_vals = (
+            2 * x * np.sin(t) * np.cos(z) - 0.5 * x * np.cos(t) * np.sin(z) + np.sin(z)
+        )
+
+        idx_0 = np.where((basis.modes == [1, -1, 1]).all(axis=1))[0]
+        idx_1 = np.where((basis.modes == [1, 1, -1]).all(axis=1))[0]
+        idx_2 = np.where((basis.modes == [0, 0, -1]).all(axis=1))[0]
+
+        c = np.zeros((basis.modes.shape[0],))
+        c[idx_0] = 2
+        c[idx_1] = -0.5
+        c[idx_2] = 1
+
+        values = transf.transform(c, 0, 0, 0)
+
+        np.testing.assert_allclose(values, correct_vals, atol=1e-8)
+
+    @pytest.mark.unit
+    def test_volume_zernike(self):
         """Tests transform of Fourier-Zernike basis in a toroidal volume."""
         grid = ConcentricGrid(L=4, M=2, N=2)
         basis = FourierZernikeBasis(L=-1, M=1, N=1, sym="sin")

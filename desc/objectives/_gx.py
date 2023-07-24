unfortunately, more sign issues. Fixed calculation of gds21 and gbdrift. dealing with alpha != 0 issues.

desc/objectives/_gx.py
@@ -542,6 +542,7 @@ class GXWrapper(_Objective):
             rho = np.sqrt(self.psi)
             iota = fi(rho)
             zeta = np.linspace((-np.pi*self.npol-self.alpha)/np.abs(iota),(np.pi*self.npol-self.alpha)/np.abs(iota),2*self.nzgrid+1)
+
             thetas = self.alpha*np.ones(len(zeta)) + iota*zeta
 
             rhoa = rho*np.ones(len(zeta))
@@ -637,7 +638,7 @@ class GXWrapper(_Objective):
 
         iotas = fi(np.sqrt(self.psi))
         shears = fs(np.sqrt(self.psi))
-
+        
         zeta = np.linspace((-np.pi*self.npol-self.alpha)/np.abs(iotas),(np.pi*self.npol-self.alpha)/np.abs(iotas),2*self.nzgrid+1)
         iota = iotas * np.ones(len(zeta))
         shear = shears * np.ones(len(zeta))
@@ -666,7 +667,7 @@ class GXWrapper(_Objective):
         #normalizations       
         Lref = data_eq['a']
         Bref = 2*np.abs(psib)/Lref**2
-
+        print("Bref is " + str(Bref))
         #calculate bmag
         modB = data['|B|']
         bmag = modB/Bref
@@ -681,7 +682,9 @@ class GXWrapper(_Objective):
         lmbda_r = data['lambda_r']
         lmbda_t = data['lambda_t']
         lmbda_z = data['lambda_z']
-       
+      
+        
+
         grad_alpha_r = (lmbda_r - zeta*shear)
         grad_alpha_t = (1 + lmbda_t)
         grad_alpha_z = (-iota+lmbda_z)
@@ -696,7 +699,8 @@ class GXWrapper(_Objective):
         shat = -x/iotas * shear[0]/Lref
         gds2 = grad_alpha**2 * Lref**2 *self.psi
         #gds21 with negative sign?
-        gds21 = -shat/Bref * grad_psi_dot_grad_alpha
+
+        gds21 = shat/Bref * grad_psi_dot_grad_alpha
         gds22 = (shat/(Lref*Bref))**2 /self.psi * grad_psi**2*data['g^rr']
 
         #calculate gbdrift0 and cvdrift0
@@ -707,7 +711,7 @@ class GXWrapper(_Objective):
         jac = data['sqrt(g)']
         #gbdrift0 = (B_t*dB_z - B_z*dB_t)*2*rho*psib/jac
         #gbdrift0 with negative sign?
-        gbdrift0 = -psib/np.abs(psib)*shat * 2 / modB**3 / rho*(B_t*dB_z + B_z*dB_t)*psib/jac * 2 * rho
+        gbdrift0 = -psib/np.abs(psib)*shat * 2 / modB**3 / rho*(B_t*dB_z - B_z*dB_t)*psib/jac * 2 * rho
         cvdrift0 = gbdrift0
 
         #calculate gbdrift and cvdrift
@@ -780,7 +784,17 @@ class GXWrapper(_Objective):
         #ds = nc.Dataset('/scratch/gpfs/pk2354/DESC/GX/gx.nc')
         
         qflux = ds['Fluxes/qflux']
-        qflux_avg = jnp.mean(qflux[int(len(qflux)/2):]) 
+        qflux = qflux[int(len(qflux)/2):]
+        
+        weighted_birkhoff = np.zeros(len(qflux))
+        N = len(weighted_birkhoff)
+        for i in range(1,N-1):
+            weighted_birkhoff[i] = np.exp(-1/(i/N*(1-i/N)))
+        norm = np.sum(weighted_birkhoff)
+        
+        weighted_birkhoff = weighted_birkhoff.reshape((len(qflux),1))
+        qflux_avg = np.sum(weighted_birkhoff*qflux/norm)
+#        qflux_avg = jnp.mean(qflux[int(len(qflux)/2):]) 
         print(qflux_avg)
         ds.close() 
         #os.rename('/scratch/gpfs/pk2354/DESC/GX/gx_nl.nc','/scratch/gpfs/pk2354/DESC/GX/gx_old.nc')
@@ -897,6 +911,7 @@ class GXWrapper(_Objective):
         
         for i in range(len(uniform_grid)-1):
             if uniform_grid[i] > zgrid[-1]:
+                print("OUT OF BOUNDS")
                 geo_array_gx[i] = geo_array_gx[i-1]
             else:
                 geo_array_gx[i] = f(np.round(uniform_grid[i],5))

desc/optimize/_desc_wrappers.py
@@ -214,27 +214,81 @@ def _optimize_desc_stochastic(
     assert constraint is None, f"method {method} doesn't support constraints"
     options = {} if options is None else options
     
-    def grad_fd(x_reduced,x0):
+    def grad_fd(x_reduced,x0,f):
         x = x_reduced
-        fx = objective.compute(x)
-#        dx = 0.1*np.abs(x0)
-        dx = 0.1*np.power(10,np.floor(np.log10(np.abs(x))))
+#        fx = objective.compute(x)
+        fx = f
+        print("FX IS " + str(fx))
+        dx = 1.0e-1*np.abs(x)
+               
+#        x = jnp.asarray(x).at[x == 0].set(0.001)
 
+#        dx = 0.1*np.power(10,np.floor(np.log10(np.abs(x))))
+        print("dx is " + str(dx))
         tang = np.eye(len(dx))
         jac = np.zeros((len(fx),len(tang)))
         for i in range(len(tang)):
             tang[i][i] = dx[i]
         for i in range(len(tang)):
-            df = (objective.compute(x_reduced+tang[:,i].T)-fx)/np.linalg.norm(objective.recover(tang[:,i].T))
+#            df = (objective.compute(x_reduced+tang[:,i].T)-fx)/np.linalg.norm(objective.recover(tang[:,i].T))
+
+            ob = objective.compute(x_reduced+tang[:,i].T)
+#            obm = objective.compute(x_reduced-tang[:,i].T)
+
+
+            print("OB IS " + str(ob))
+#            print("OBM IS " + str(obm))
+
+#            df = (ob-fx)/np.max(dx)
+#            df = (ob-fx)/np.linalg.norm(tang[:,i].T)
+#            if np.linalg.norm(ob) > np.linalg.norm(fx) and np.linalg.norm(obm) > np.linalg.norm(fx):
+#                print("SET TO 0")
+#                ob = np.zeros(len(ob))
+#                obm = np.zeros(len(obm))
+            df = (ob-fx)/(np.linalg.norm(objective.recover(tang[:,i].T)))
+
+
             jac[:,i] = df
         return fx.T @ jac
+
+    def grad_spsa(x_reduced,x0,f,ck):
+        x = x_reduced
+        #fx = objective.compute(x)
+        fx = f
+#        h = 0.2*np.power(10,np.floor(np.log10(np.abs(x))))
+        h = ck*np.abs(x)
+ 
+        jac = np.zeros((len(fx),len(h)))
+        
+        N = 1
+        for j in range(N):
+            djac = np.zeros((len(fx),len(h)))
+            dx = (np.random.binomial(1,0.5,x.shape)*2-1)*h
+            print("dx is " + str(dx))
+
+            ob = objective.compute(x + dx)
+#            print("OB IS " + str(ob))
+            obm = objective.compute(x - dx)
+#            print("OBM IS " + str(obm))
+            df = ob - obm
+            for i in range(len(x0)):
+                zx = np.zeros(len(x0))
+                zx[i] = dx[i]
+                djac[:,i] = np.abs(dx[i])/dx[i]*df/(2*np.linalg.norm(objective.recover(zx)))
+#                djac[:,i] = df/(2*dx[i])
+            jac = jac + djac
+        jac = jac/N
+
+        return fx.T @ jac
+
 
 
     result = sgd(
         objective,
         objective.compute_scalar,
         x0=x0,
-        grad=grad_fd,
+#        grad=grad_fd,
+        grad=grad_spsa,
         args=(),
         method=method,
         ftol=stoptol["ftol"],

desc/optimize/stochastic.py
@@ -108,13 +108,24 @@ def sgd(
     x = x0.copy()
     print("x is " + str(x))
     v = np.zeros_like(x)
-    f = fun(x, *args)
+    #f = fun(x, *args)
+    fa = obj.compute(x)
+    f = np.linalg.norm(fa)**2/2
     xopt = x
     fopt = f
     print("f is " + str(f))
     nfev += 1
-    g = grad(x,x0)
+#    c = 0.01
+    c = options.pop("c", 0.01)
+    ck = c
+    g = grad(x,x0,fa,ck)
     print("g is " + str(g))
+
+
+    gamma = 0.9
+    G = (1-gamma) * np.outer(g,g)
+    print("diagonal of G is " + str(G.diagonal()))
+
     ngev += 1
     
    
@@ -128,7 +139,10 @@ def sgd(
     alpha = options.pop("alpha", 2e-2 * x_norm / g_norm)
     beta = options.pop("beta", 0.9)
     assert len(options) == 0, "sgd got an unexpected option {}".format(options.keys())
-    
+
+    alpha0 = alpha
+
+
     success = None
     message = None
     step_norm = np.inf
@@ -140,16 +154,23 @@ def sgd(
     if return_all:
         allx = [x]
     v = beta * v + (1 - beta) * g
+
     print("v is " + str(v))
     print("alpha is " + str(alpha))
     x = x - alpha * v
+#    x = x - alpha*g
+#    x = x - alpha *(1/np.sqrt(G.diagonal())) * v
     print("x is " + str(x))
     print("updating eq")
     obj._objective._update_equilibrium(obj.recover(x),store=True)
 
-    fnew = fun(x, *args)
+#    fnew = fun(x, *args)
+    fa = obj.compute(x)
+    fnew = np.linalg.norm(fa)**2/2
     print("new f is " + str(fnew))
-    if fnew < fopt:
+
+#    if fnew < fopt:
+    if True:
         fopt = fnew
         xopt = x
     iteration += 1
@@ -175,19 +196,34 @@ def sgd(
         )
         if success is not None:
             break
+
+        alpha = alpha0/(1+iteration)**0.602
+        ck = c/(iteration+1)**0.101
         
-        g = grad(x,x0)
+        print("alpha is " + str(alpha))
+        print("ck is " + str(ck))
+
+        g = grad(x,x0,fa,ck)
+        G = gamma * G + (1-gamma) * np.outer(g,g)
         print("g is " + str(g))
+        print("diagonal of G is " + str(G.diagonal()))
         v = beta * v + (1 - beta) * g
+
         print("v is " + str(v))
         x = x - alpha * v
+#        x = x - alpha*g
+#        x = x - alpha *(1/np.sqrt(G.diagonal())) * v
+
         print("x is " + str(x))
         
         print("updating eq")
         obj._objective._update_equilibrium(obj.recover(x),store=True)
-        fnew = fun(x, *args)
+#        fnew = fun(x, *args)
+        fa = obj.compute(x)
+        fnew = np.linalg.norm(fa)**2/2
         print("new f is " + str(fnew))
-        if fnew < fopt:
+#        if fnew < fopt:
+        if True:
             fopt = fnew
             xopt = x
 
@@ -210,6 +246,7 @@ def sgd(
                 message = STATUS_MESSAGES["callback"]
 
         iteration += 1
+#        print("alpha is " + str(alpha))
 #    flast = fun(x,*args)
 #    xlast = x
     obj._objective._update_equilibrium(obj.recover(xopt),store=True)

test_gx.py
@@ -1,88 +1,37 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 13 09:39:53 2022
-
-@author: pk123
-"""
-#%%
-import numpy as np
-import matplotlib.pyplot as plt
 from desc import set_device
 set_device('gpu')
-
-plt.rcParams["font.size"] = 12
-
 import desc.io
-from desc.grid import LinearGrid, ConcentricGrid
-from desc.objectives import (
-    ObjectiveFunction,
-    FixBoundaryR,
-    FixBoundaryZ,
-    FixPressure,
-    FixIota,
-    FixPsi,
-    AspectRatio,
-    ForceBalance,
-    QuasisymmetryBoozer,
-    QuasisymmetryTwoTerm,
-    QuasisymmetryTripleProduct,
-    FixLambdaGauge,
-    GXWrapper
-)
-from desc.optimize import Optimizer
-from desc.plotting import plot_grid, plot_boozer_modes, plot_boozer_surface, plot_qs_error
+from desc.objectives import GXWrapper
+from desc.derivatives import nested_zeros_like
+import numpy as np
+import jax
+from desc.vmec import VMECIO
+import dill
+#eq = desc.io.load("/scratch/gpfs/pk2354/DESC/docs/notebooks/tutorials/qs_initial_guess.h5")
+#eq = desc.io.load("/scratch/gpfs/pk2354/DESC/desc/examples/DSHAPE_output.h5")[-1]
+#eq = desc.io.load("/scratch/gpfs/pk2354/DESC/test_equilibria/unconstrained_qs.h5")
+#eq.change_resolution(M=6,L=6,M_grid=12,L_grid=12)
+#eq = desc.io.load("/scratch/gpfs/pk2354/DESC/examples/VMEC/input.set_of_configs_for_gx_output.h5")[-1]
+#eq = VMECIO.load("/scratch/gpfs/pk2354/DESC/examples/VMEC/wout_set_of_configs_for_gx.nc",L=8,M=8,N=8)
+#eq = desc.io.load("/scratch/gpfs/pk2354/DESC/examples/VMEC/OPT_output.h5")[-1]
+#eq = desc.io.load("/scratch/gpfs/pk2354/DESC/examples/DESC/DSHAPE_current_output.h5")[-1]
+#eq = desc.io.load("/scratch/gpfs/pk2354/DESC/examples/VMEC/OPT_scans/OPT_z_m0_n-1_-0.00_output.h5")[-1]
+#eq = desc.io.load("/scratch/gpfs/pk2354/DESC/desc/examples/NCSX_output.h5")[-1]
+#eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/intermediate8_3.h5')
+#eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/input.nfp4_QH_output.h5')[-1]
+#eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/swap_gx_end_case16_n1_all_4.h5')
+#eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/swap_gx_end_case18_n1_all_6.h5')
+#eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/swap_gx_end_case20_n1_all_3.h5')
+#eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/swap_gx_end_case21_n_2_i_4.h5')
+#eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/swap_end4_4.h5')
+#eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/precise_qh2.h5')
+#eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/swap_gx_intermediate_case2_n_3_i_31.h5')
+#eq.change_resolution(M=6,L=6,N=4,M_grid=12,L_grid=12,N_grid=8)
+eq = desc.io.load('/scratch/gpfs/pk2354/DESC/W7X_output.h5')[-1]
+#eq = desc.io.load('/scratch/gpfs/pk2354/DESC/examples/VMEC/input.BEER_output.h5')[-1]
+#eq = desc.io.load('/scratch/gpfs/pk2354/DESC/desc/examples/W7-X_output.h5')[-1]
+print('loaded eq')
+gxw = GXWrapper(eq=eq,psi=0.5,nzgrid=64,t=1,alpha=np.pi/2,npol=2)
 
-#%%
-#eq_init = desc.io.load("/scratch/gpfs/pk2354/DESC/docs/notebooks/tutorials/qs_initial_guess.h5")
-eq_init = desc.io.load('/scratch/gpfs/pk2354/DESC/desc/examples/DSHAPE_output.h5')[-1]
-eq_init.change_resolution(M=6,L=6,M_grid=12,L_grid=12)
-#optimizer = Optimizer("lsq-exact")
-#eq_init = desc.io.load('/scratch/gpfs/pk2354/DESC/examples/VMEC/OPT2_output.h5')
-optimizer = Optimizer("stochastic")
-idx_Rcc = eq_init.surface.R_basis.get_idx(M=1, N=0)
-#idx_Rss = eq_init.surface.R_basis.get_idx(M=-2, N=0)
-idx_Zsc = eq_init.surface.Z_basis.get_idx(M=-1, N=0)
-#idx_Zcs = eq_init.surface.Z_basis.get_idx(M=1, N=0)
-
-# boundary modes to constrain
-R_modes = np.delete(eq_init.surface.R_basis.modes, [idx_Rcc], axis=0)
-Z_modes = np.delete(eq_init.surface.Z_basis.modes, [idx_Zsc], axis=0)
-
-# constraints
-constraints = (
-    ForceBalance(),  # enforce JxB-grad(p)=0 during optimization
-    FixBoundaryR(),  # fix specified R boundary modes
-    FixBoundaryZ(modes=Z_modes),  # fix specified Z boundary modes
-    FixPressure(),  # fix pressure profile
-    FixIota(),  # fix rotational transform profile
-    FixPsi(),  # fix total toroidal magnetic flux
-)
-
-#constraint = ObjectiveFunction((ForceBalance(),FixBoundaryR(modes=R_modes),FixBoundaryZ(modes=Z_modes),FixPressure(),FixIota(),FixPsi()))
-
-grid_vol = ConcentricGrid(L=eq_init.L_grid, M=eq_init.M_grid, N=eq_init.N_grid, NFP=eq_init.NFP, sym=eq_init.sym)
-#plot_grid(grid_vol);
-#objective_fT = ObjectiveFunction(QuasisymmetryTripleProduct(grid=grid_vol), verbose=0,use_jit=False)
-objective_fT = ObjectiveFunction(GXWrapper(target=0.1),verbose=0,use_jit=False)
-
-eq_qs_T_unc, result_T_unc = eq_init.optimize(
-    objective=objective_fT,
-    #constraint=constraint,
-    constraints=constraints,
-    optimizer=optimizer,
-    ftol=1e-2,  # stopping tolerance on the function value
-    xtol=1e-6,  # stopping tolerance on the step size
-    gtol=1e-6,  # stopping tolerance on the gradient
-    maxiter=1,  # maximum number of iterations
-    options={
-        #"initial_trust_radius":0.1,
-        "perturb_options": {"order": 2, "verbose": 0},  # use 2nd-order perturbations
-        "solve_options": {"ftol": 1e-2, "xtol": 1e-6, "gtol": 1e-6, "verbose": 0}, # for equilibrium subproblem
-    },
-    copy=True,  # return a new Equilibrium object (copy=False will overwrite the original)
-    verbose=3,
-)
-
-eq_qs_T_unc.save('/scratch/gpfs/pk2354/DESC/test_equilibria/unconstrained_gx_nl.h5')
+qflux = gxw.compute(R_lmn=eq.R_lmn,Z_lmn=eq.Z_lmn,L_lmn=eq.L_lmn,i_l=eq.i_l,c_l=eq.c_l,p_l=eq.p_l,Psi=eq.Psi,ne_l=eq.ne_l, Ti_l=eq.Ti_l, Zeff_l=eq.Zeff_l, Te_l=eq.Te_l)
 

test_gxwrapper.py
@@ -19,25 +19,38 @@ import dill
 #eq = desc.io.load("/scratch/gpfs/pk2354/DESC/desc/examples/NCSX_output.h5")[-1]
 #eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/intermediate8_3.h5')
 eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/input.nfp4_QH_output.h5')[-1]
+#eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/swap_gx_end_case16_n1_all_4.h5')
+#eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/swap_gx_end_case18_n1_all_6.h5')
+#eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/swap_gx_end_case20_n1_all_3.h5')
 #eq = desc.io.load('/scratch/gpfs/pk2354/DESC/test_equilibria/swap_end4_4.h5')
 #eq.change_resolution(M=6,L=6,N=4,M_grid=12,L_grid=12,N_grid=8)
-gxw = GXWrapper(eq=eq)
+surfaces = [0.2, 0.4, 0.6, 0.8]
+fluxes = []
+count = 0
+for rho in surfaces:
+    print("rho = " + str(rho))
+    gxw = GXWrapper(eq=eq,psi=rho**2,t=count,nzgrid=64,npol=2)
 
-filename = "test_dill"
+    #filename = "test_dill"
 
-filehandler = open(filename, 'wb')
-dill.dump(gxw, filehandler)
-filehandler.close()
+    #filehandler = open(filename, 'wb')
+    #dill.dump(gxw, filehandler)
+    #filehandler.close()
 
-#gxw.compute(R_lmn=eq.R_lmn,Z_lmn=eq.Z_lmn,L_lmn=eq.L_lmn,i_l=eq.i_l,c_l=eq.c_l,p_l=eq.p_l,Psi=eq.Psi)
-#values = (eq.R_lmn,eq.Z_lmn,eq._L_lmn,eq.i_l,eq.p_l,eq.Psi)
-#tangents = nested_zeros_like(values)
-#tangents[0] = tangents[0].at[0].set(1)
-#tangents = (np.random.random(len(x) for x in values[:-1])) + (0.1,)
-#tangents = ()
-#for x in values[:-1]:
-#    print(len(x))
-#    tangents = tangents + (np.random.random(len(x)),)
-#tangents = tangents + (0.1,)
-#gxw.compute_gx_jvp(values,tangents)
-#p,t = jax.jvp(gxw.compute,values,tangents)
+    qflux = gxw.compute(R_lmn=eq.R_lmn,Z_lmn=eq.Z_lmn,L_lmn=eq.L_lmn,i_l=eq.i_l,c_l=eq.c_l,p_l=eq.p_l,Psi=eq.Psi,ne_l=eq.ne_l, Ti_l=eq.Ti_l, Zeff_l=eq.Zeff_l, Te_l=eq.Te_l)
+    fluxes.append(qflux)
+    count = count + 1
+f = open('original_fluxes_npol2_2.txt','w')
+f.write(str(fluxes))
+f.close()
+    #values = (eq.R_lmn,eq.Z_lmn,eq._L_lmn,eq.i_l,eq.p_l,eq.Psi)
+    #tangents = nested_zeros_like(values)
+    #tangents[0] = tangents[0].at[0].set(1)
+    #tangents = (np.random.random(len(x) for x in values[:-1])) + (0.1,)
+    #tangents = ()
+    #for x in values[:-1]:
+    #    print(len(x))
+    #    tangents = tangents + (np.random.random(len(x)),)
+    #tangents = tangents + (0.1,)
+    #gxw.compute_gx_jvp(values,tangents)
+    #p,t = jax.jvp(gxw.compute,values,tangents)
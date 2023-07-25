311ff0a185aea6b316dcf48b5f2a002790d8cdc6
Kaya Unalmis <kayaunalmis@proton.me>
2023-07-25 16:17:36 -0500

    Add script to sort compute functions

---
+++ docs/sort_compute_funs.py
@@ -0,0 +1,38 @@
+"""Sort the compute functions and print to specified output.
+
+First command line argument is module name, e.g. "_basis_vectors".
+Second command line output is file to print to,
+e.g. "sorted_basis_vectors.txt".
+"""
+
+import inspect
+import re
+import sys
+
+import desc.compute
+
+
+def get_matches(fun, pattern):
+    """Return all matches of ``pattern`` in source code of function ``fun``."""
+    src = inspect.getsource(fun)
+    matches = pattern.findall(src)
+    matches = [s.replace("'", "").replace('"', "") for s in matches]
+    return matches
+
+
+# Gather all compute function source code and map to quantity name.
+src_codes = {}
+pattern_name = re.compile(r"(?<=name=)[^,]+")
+for module_name, module in inspect.getmembers(desc.compute, inspect.ismodule):
+    if module_name == sys.argv[1]:
+        for _, fun in inspect.getmembers(module, inspect.isfunction):
+            # quantities that this function computes
+            name = get_matches(fun, pattern_name)
+            if len(name) > 0:  # skip imported functions
+                src_codes[name[0]] = inspect.getsource(fun)
+
+# Write compute functions sorted by name to file.
+with open(sys.argv[2], "w") as output_file:
+    for name in sorted(src_codes):
+        output_file.write(src_codes[name])
+        output_file.write("\n\n")

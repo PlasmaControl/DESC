Fix plot_fsa handling of nan in surface average

desc/plotting.py
@@ -16,7 +16,7 @@ from termcolor import colored
 from desc.backend import sign
 from desc.basis import fourier, zernike_radial_poly
 from desc.compute import data_index, get_transforms
-from desc.compute.utils import surface_averages
+from desc.compute.utils import surface_averages_map
 from desc.grid import Grid, LinearGrid
 from desc.utils import flatten_list, parse_argname_change
 from desc.vmec_utils import ptolemy_linear_transform
@@ -998,7 +998,6 @@ def plot_fsa(
         eq, name, grid, kwargs.pop("component", None), reshape=False
     )
     label = label.split("~")
-    get_value = lambda x: _compute(eq, x, grid, reshape=False)[0]
     if (
         data_index["desc.equilibrium.equilibrium.Equilibrium"][name]["coordinates"]
         == "r"
@@ -1010,10 +1009,15 @@ def plot_fsa(
         # So we avoid surface averaging it and forgo the <> around the label.
         label = r"$ " + label[0][1:] + r" ~" + "~".join(label[1:])
         plot_data_ylabel_key = f"{name}"
+        if (
+            data_index["desc.equilibrium.equilibrium.Equilibrium"][name]["coordinates"]
+            == "r"
+        ):
+            values = grid.compress(values)
     else:
-        is_nan = np.isnan(values)
+        compute_surface_averages = surface_averages_map(grid, expand_out=False)
         if with_sqrt_g:  # flux surface average
-            sqrt_g = get_value("sqrt(g)")
+            sqrt_g = _compute(eq, "sqrt(g)", grid, reshape=False)[0]
             # Attempt to compute the magnetic axis limit.
             # Compute derivative depending on various naming schemes.
             # e.g. B -> B_r, V(r) -> V_r(r), S_r(r) -> S_rr(r)
@@ -1024,7 +1028,7 @@ def plot_fsa(
             )
             values_r = next(
                 (
-                    get_value(x)
+                    _compute(eq, x, grid, reshape=False)[0]
                     for x in schemes
                     if x in data_index["desc.equilibrium.equilibrium.Equilibrium"]
                 ),
@@ -1032,30 +1036,30 @@ def plot_fsa(
             )
             if (np.isfinite(values) & np.isfinite(values_r))[grid.axis].all():
                 # Otherwise cannot compute axis limit in this agnostic manner.
-                sqrt_g = grid.replace_at_axis(sqrt_g, get_value("sqrt(g)_r"), copy=True)
-            values = surface_averages(grid=grid, q=values, sqrt_g=sqrt_g)
+                sqrt_g = grid.replace_at_axis(
+                    sqrt_g, _compute(eq, "sqrt(g)_r", grid, reshape=False)[0], copy=True
+                )
+            averages = compute_surface_averages(values, sqrt_g=sqrt_g)
             label = r"$\langle " + label[0][1:] + r" \rangle~" + "~".join(label[1:])
         else:  # theta average
-            values = surface_averages(grid=grid, q=values)
+            averages = compute_surface_averages(values)
             label = (
                 r"$\langle "
                 + label[0][1:]
                 + r" \rangle_{\theta}~"
                 + "~".join(label[1:])
             )
-        # integration replaced nans with 0, put them back
-        values = np.where(is_nan, np.nan, values)
+        # True if values has nan on a given surface.
+        is_nan = compute_surface_averages(np.isnan(values)).astype(bool)
+        # The integration replaced nan with 0.
+        # Put them back to avoid misleading plot (e.g. cusp near the magnetic axis).
+        values = np.where(is_nan, np.nan, averages)
         plot_data_ylabel_key = f"<{name}>_fsa"
-    if (
-        data_index["desc.equilibrium.equilibrium.Equilibrium"][name]["coordinates"]
-        != ""
-    ):
-        values = grid.compress(values)
 
     if norm_F:
         # normalize force by B pressure gradient
         norm_name = kwargs.pop("norm_name", "<|grad(|B|^2)|/2mu0>_vol")
-        norm_data = get_value(norm_name)
+        norm_data = _compute(eq, norm_name, grid, reshape=False)[0]
         values = values / np.nanmean(np.abs(norm_data))  # normalize
     if log:
         values = np.abs(values)  # ensure data is positive for log plot

tests/test_axis_limits.py
@@ -157,23 +157,27 @@ def assert_is_continuous(
     names = [x for x in names if not ("Boozer" in x or "_mn" in x or x == "B modes")]
 
     num_points = 15
-    rho = np.linspace(start=0, stop=1, num=num_points) * delta
+    rho = np.linspace(start=0, stop=delta, num=num_points)
     grid = LinearGrid(rho=rho, M=5, N=5, NFP=eq.NFP, sym=eq.sym)
     assert grid.axis.size
-    integrate = surface_integrals_map(grid)
+    integrate = surface_integrals_map(grid, expand_out=False)
     data = eq.compute(names, grid=grid)
 
     data_index_eq = data_index["desc.equilibrium.equilibrium.Equilibrium"]
     for name in names:
         if _skip_this(eq, name) or data_index_eq[name]["coordinates"] == "":
+            # can't check continuity of global scaler quantity
             continue
         # make single variable function of rho
         if data_index_eq[name]["coordinates"] == "r":
-            profile = data[name]
+            # already single variable function of rho
+            profile = grid.compress(data[name])
         else:
+            # integrate out theta and zeta dependence
             profile = np.where(
-                np.isnan(data[name]),
-                # integration replaced nans with 0, put them back
+                # True if integrand has nan on a given surface.
+                integrate(np.isnan(data[name])).astype(bool),
+                # The integration below replaces nan with 0; put them back.
                 np.nan,
                 # Norms and integrals are continuous functions, so their composition
                 # cannot disrupt existing continuity. Note that the absolute value
@@ -181,7 +185,6 @@ def assert_is_continuous(
                 # not become continuous once integrated.
                 integrate(np.abs(data[name])),
             )
-        profile = grid.compress(profile)
         fit = kwargs.get(name, {}).get("desired_at_axis", desired_at_axis)
         if fit is None:
             if np.ndim(data_index_eq[name]["dim"]):
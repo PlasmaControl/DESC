904787010c44cbd24a8db156051b389c113942aa
Rahul Gaur <rg6256@della-gpu.princeton.edu>
2023-08-10 15:29:30 -0400

    code coverage vs me: round 2

--- tests/test_input_output.py
+++ tests/test_input_output.py
@@ -142,7 +142,7 @@ def test_descout_to_input(tmpdir_factory):
     arr2mneg = arr2[arr2[:, 1] < 0]
     arr2mpos = arr2[arr2[:, 1] >= 0]
 
-    found = 0
+    found0 = 0
     if (
         np.minimum(
             np.linalg.norm(arr1mneg[:, 3:] - arr2mneg[:, 3:]),
@@ -156,9 +156,47 @@ def test_descout_to_input(tmpdir_factory):
         )
         <= 1e-8
     ):
-        found = 1
+        found0 = 1
 
-    assert found == 1
+    outfile_path = "./tests/inputs/HELIOTRON_vacuum_current.h5"
+    tmpdir = tmpdir_factory.mktemp("desc_inputs")
+    tmp_path = tmpdir.join("input_HELIOTRON_vacuum_current")
+    tmpout_path = tmpdir.join("HELIOTRON_vacuum_current.h5")
+    shutil.copyfile(outfile_path, tmpout_path)
+
+    ir1 = InputReader()
+    ir1.descout_to_input(str(tmp_path), str(tmpout_path))
+    ir1 = InputReader(cl_args=[str(tmp_path)])
+    arr1 = ir1.parse_inputs()[-1]["surface"]
+    arr1 = arr1[arr1[:, 1].argsort()]
+    arr1mneg = arr1[arr1[:, 1] < 0]
+    arr1mpos = arr1[arr1[:, 1] >= 0]
+
+    desc_input_truth = "./tests/inputs/HELIOTRON_vacuum_current"
+    with pytest.warns(UserWarning):
+        ir2 = InputReader(cl_args=[str(desc_input_truth)])
+        arr2 = ir2.parse_inputs()[-1]["surface"]
+    arr2 = arr2[arr2[:, 1].argsort()]
+    arr2mneg = arr2[arr2[:, 1] < 0]
+    arr2mpos = arr2[arr2[:, 1] >= 0]
+
+    found1 = 0
+    if (
+        np.minimum(
+            np.linalg.norm(arr1mneg[:, 3:] - arr2mneg[:, 3:]),
+            np.linalg.norm(arr1mneg[:, 3:] + arr2mneg[:, 3:]),
+        )
+        <= 1e-8
+    ) and (
+        np.minimum(
+            np.linalg.norm(arr1mpos[:, 3:] - arr2mpos[:, 3:]),
+            np.linalg.norm(arr1mpos[:, 3:] + arr2mpos[:, 3:]),
+        )
+        <= 1e-8
+    ):
+        found1 = 1
+
+    assert found0 == 1 and found1 == 1
 
 
 @pytest.mark.unit

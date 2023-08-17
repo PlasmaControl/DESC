da1f930adb627f95e30090fe8492e8990db99a13
daniel-dudt <daniel.dudt@princetonstellarators.energy>
2023-08-17 11:00:40 -0600

    change Redl units

--- desc/compute/_bootstrap.py
+++ desc/compute/_bootstrap.py
@@ -312,7 +312,7 @@ def j_dot_B_Redl(
 @register_compute_fun(
     name="<J*B> Redl",
     label="\\langle\\mathbf{J}\\cdot\\mathbf{B}\\rangle_{Redl}",
-    units="T A m^{-2}",
+    units="N / m^{3}",
     units_long="Tesla Ampere / meter^2",
     description="Bootstrap current profile, Redl model for quasisymmetry",
     dim=1,

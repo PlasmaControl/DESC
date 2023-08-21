73b796e91e204f420b8b9585aea5ff3244efe0b0
Tal Shpigel <talshpigel@Tals-MacBook-Air.local>
2023-08-21 23:09:53 +0300

    added names to compute_fun

--- desc/objectives/_geometry.py
+++ desc/objectives/_geometry.py
@@ -1267,6 +1267,7 @@ class B_dmin(_Objective):
             constants = self.constants
 
         data = compute_fun(
+            "desc.equilibrium.equilibrium.Equilibrium",
             self._data_keys,
             params=params,
             transforms=self._transforms,

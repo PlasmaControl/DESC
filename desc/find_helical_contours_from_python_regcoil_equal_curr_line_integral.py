a0c895621231ae4d76ea29594bf207c1cab87408
Dario Panici <dpanici@princeton.edu>
2023-08-24 10:20:24 -0400

    skip minimum separation until more efficient algorithm is implemented

--- desc/find_helical_contours_from_python_regcoil_equal_curr_line_integral.py
+++ desc/find_helical_contours_from_python_regcoil_equal_curr_line_integral.py
@@ -562,23 +562,23 @@ def find_helical_coils(  # noqa: C901 - FIXME: simplify this
                 else:
                     ax.plot(contour_X[j], contour_Y[j], contour_Z[j], ls)
 
-        # Find the point of minimum separation
-        if find_min_dist:
-            minSeparation2 = 1.0e20
-            for whichCoil1 in range(desirednumcoils):
-                for whichCoil2 in range(whichCoil1):
-                    for whichPoint in range(len(contour_X[whichCoil1])):
-                        dx = contour_X[whichCoil1][whichPoint] - contour_X[whichCoil2]
-                        dy = contour_Y[whichCoil1][whichPoint] - contour_Y[whichCoil2]
-                        dz = contour_Z[whichCoil1][whichPoint] - contour_Z[whichCoil2]
-                        separation2 = dx * dx + dy * dy + dz * dz
-                        this_minSeparation2 = np.min(separation2)
-                        if this_minSeparation2 < minSeparation2:
-                            minSeparation2 = this_minSeparation2
+        # # Find the point of minimum separation
+        # if find_min_dist:
+        #     minSeparation2 = 1.0e20
+        #     for whichCoil1 in range(desirednumcoils):
+        #         for whichCoil2 in range(whichCoil1):
+        #             for whichPoint in range(len(contour_X[whichCoil1])):
+        #                 dx = contour_X[whichCoil1][whichPoint] - contour_X[whichCoil2]
+        #                 dy = contour_Y[whichCoil1][whichPoint] - contour_Y[whichCoil2]
+        #                 dz = contour_Z[whichCoil1][whichPoint] - contour_Z[whichCoil2]
+        #                 separation2 = dx * dx + dy * dy + dz * dz
+        #                 this_minSeparation2 = np.min(separation2)
+        #                 if this_minSeparation2 < minSeparation2:
+        #                     minSeparation2 = this_minSeparation2
 
-            print(
-                f"Minimum coil-coil separation: {np.sqrt(minSeparation2)*1000:3.2f} mm"
-            )
+        #     print(
+        #         f"Minimum coil-coil separation: {np.sqrt(minSeparation2)*1000:3.2f} mm"
+        #     )
         return contour_X, contour_Y, contour_Z, ax
 
     contour_X, contour_Y, contour_Z, ax = find_XYZ_points(

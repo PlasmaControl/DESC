Fix gradient of anisotropic pressure

desc/compute/_profiles.py
@@ -479,8 +479,8 @@ def _p_z(params, transforms, profiles, data, **kwargs):
 def _gradp(params, transforms, profiles, data, **kwargs):
     data["grad(p)"] = (
         data["p_r"] * data["e^rho"].T
-        + data["p_t"] * data["e^theta"].T
-        + data["p_z"] * data["e^zeta"].T
+        + jnp.where(data["p_t"] == 0, 0, data["p_t"] * data["e^theta"].T)
+        + jnp.where(data["p_z"] == 0, 0, data["p_z"] * data["e^zeta"].T)
     ).T
     return data
 
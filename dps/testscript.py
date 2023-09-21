2597ca1621a6b76ccc05188c738e2a6c1259b41e
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2023-09-21 23:40:37 +0100

    ensamble test

--- dps/testscript.py
+++ dps/testscript.py
@@ -5,64 +5,83 @@ import desc.io
 from desc.backend import jnp
 import matplotlib.pyplot as plt
 import numpy as np
+import jax.random
 
 eq = desc.io.load("test_run.h5")
 eq._iota = eq.get_profile("iota").to_powerseries(order=eq.L, sym=True)
 eq._current = None
 # eq.solve()
 
-tmin = 0
-tmax = 0.0007
-nt = 500
-time = jnp.linspace(tmin, tmax, nt)
-psi_i = 0.7
-theta_i = 0.2
-zeta_i = 0.2
+def starting_ensamble(size):
+    key = jax.random.PRNGKey(int(4120))
 
-mass = 1.673e-27
-Energy = 3.52e6*1.6e-19
-ini_vpar = 0.5*jnp.sqrt(2*Energy/mass)
-ini_cond = [psi_i, theta_i, zeta_i, float(ini_vpar)]
+    v_init = jax.random.maxwell(key, (size,))
 
-mass_charge = mass/1.6e-19
+    psi_init = jax.random.uniform(key, (size,), minval=1e-4, maxval=1-1e-4)
+    zeta_init = 0.2
+    theta_init = 0.2
 
-grid = Grid(jnp.array([jnp.sqrt(psi_i), theta_i, zeta_i]).T, jitable=True, sort=False)
-data = eq.compute("|B|", grid=grid)
+    ini_cond = [[float(psi_init[i]), theta_init, zeta_init, float(v_init[i])] for i in range(0, size)]
+    return ini_cond
 
-mu = Energy/(mass*data["|B|"]) - (ini_vpar**2)/(2*data["|B|"])
+init = starting_ensamble(5)
 
-ini_param = [float(mu), mass_charge]
+for initial_conditions in init:
+    tmin = 0
+    tmax = 0.0007
+    nt = 500
+    time = jnp.linspace(tmin, tmax, nt)
+    psi_i = 0.7
+    theta_i = 0.2
+    zeta_i = 0.2
 
-objective = ParticleTracer(eq=eq, output_time=time, initial_conditions=ini_cond, initial_parameters=ini_param, compute_option="tracer")
+    mass = 1.673e-27
+    Energy = 3.52e6*1.6e-19
+    ini_vpar = 0.2*jnp.sqrt(2*Energy/mass)
+    ini_cond = [psi_i, theta_i, zeta_i, float(ini_vpar)]
 
-objective.build()
-solution = objective.compute(*objective.xs(eq))
+    mass_charge = mass/1.6e-19
 
-print("*************** SOLUTION .compute() ***************")
-print(solution)
-print("***************************************************")
+    grid = Grid(jnp.array([jnp.sqrt(psi_i), theta_i, zeta_i]).T, jitable=True, sort=False)
+    data = eq.compute("|B|", grid=grid)
 
-f = open("output_file.txt", "w")
-f.write(f"{solution[:, 0]}, {solution[:, 1]}, {solution[:, 2]}, {solution[:, 3]}, {mu}")
-f.close()
+    mu = Energy/(mass*data["|B|"]) - (ini_vpar**2)/(2*data["|B|"])
 
-plt.plot(np.sqrt(solution[:, 0]) * np.cos(solution[:, 1]), np.sqrt(solution[:, 0]) * np.sin(solution[:, 1]))
-plt.savefig("trajectory.png")
+    ini_param = [float(mu), mass_charge]
 
-fig, axs = plt.subplots(2, 2)
-axs[0, 1].plot(time, solution[:, 0], 'tab:orange')
-axs[0, 1].set_title(r'$\psi$ (t)')
-axs[1, 0].plot(time, solution[:, 1], 'tab:green')
-axs[1, 0].set_title(r'$\theta$ (t)')
-axs[1, 1].plot(time, solution[:, 2], 'tab:red')
-axs[1, 1].set_title(r'$\zeta$ (t)')
-axs[0, 0].plot(time, solution[:, 3], 'tab:blue')
-axs[0, 0].set_title(r"$v_{\parallel}$ (t)")
+    objective = ParticleTracer(eq=eq, output_time=time, initial_conditions=ini_cond, initial_parameters=ini_param, compute_option="tracer")
 
-fig = plt.gcf()
-fig.set_size_inches(10.5, 10.5)
+    objective.build()
+    solution = objective.compute(*objective.xs(eq))
 
-fig.savefig("quantities.png", dpi = 300)
+    print("*************** SOLUTION .compute() ***************")
+    print(solution)
+    print("***************************************************")
+    plt.plot(np.sqrt(solution[:, 0]) * np.cos(solution[:, 1]), np.sqrt(solution[:, 0]) * np.sin(solution[:, 1]))
+
+plt.savefig("all_traj.png")
+
+# f = open("output_file.txt", "w")
+# f.write(f"{solution[:, 0]}, {solution[:, 1]}, {solution[:, 2]}, {solution[:, 3]}, {mu}")
+# f.close()
+
+# plt.plot(np.sqrt(solution[:, 0]) * np.cos(solution[:, 1]), np.sqrt(solution[:, 0]) * np.sin(solution[:, 1]))
+# plt.savefig("trajectory.png")
+
+# fig, axs = plt.subplots(2, 2)
+# axs[0, 1].plot(time, solution[:, 0], 'tab:orange')
+# axs[0, 1].set_title(r'$\psi$ (t)')
+# axs[1, 0].plot(time, solution[:, 1], 'tab:green')
+# axs[1, 0].set_title(r'$\theta$ (t)')
+# axs[1, 1].plot(time, solution[:, 2], 'tab:red')
+# axs[1, 1].set_title(r'$\zeta$ (t)')
+# axs[0, 0].plot(time, solution[:, 3], 'tab:blue')
+# axs[0, 0].set_title(r"$v_{\parallel}$ (t)")
+
+# fig = plt.gcf()
+# fig.set_size_inches(10.5, 10.5)
+
+# fig.savefig("quantities.png", dpi = 300)
 
 
 """

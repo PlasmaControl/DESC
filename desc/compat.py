9a8eff51b444d69d5ba9ea752bf3a9e2de121def
daniel-dudt <daniel.dudt@princetonstellarators.energy>
2023-10-12 15:56:49 -0600

    renamed to 'flip_helicity' and added tests

--- desc/compat.py
+++ desc/compat.py
@@ -5,54 +5,6 @@ import numpy as np
 from desc.grid import Grid
 
 
-def ensure_positive_iota(eq):
-    """Convert an Equilibrium to have a positive rotational transform.
-
-    Parameters
-    ----------
-    eq : Equilibrium or iterable of Equilibrium
-        Equilibria to convert to positive rotational transform.
-
-    Returns
-    -------
-    eq : Equilibrium or iterable of Equilibrium
-        Same as input, but with coefficients adjusted to give positive rotational
-        transform.
-
-    """
-    # maybe it's iterable:
-    if hasattr(eq, "__len__"):
-        for e in eq:
-            ensure_positive_iota(e)
-        return eq
-
-    sign = np.sign(eq.compute("iota", grid=Grid(np.array([[1, 0, 0]])))["iota"])
-    if sign < 0:
-        if eq.iota is not None:
-            eq.i_l *= -1
-        else:
-            eq.c_l *= -1
-
-        rone = np.ones_like(eq.R_lmn)
-        rone[eq.R_basis.modes[:, 2] < 0] *= -1
-        eq.R_lmn *= rone
-
-        zone = np.ones_like(eq.Z_lmn)
-        zone[eq.Z_basis.modes[:, 2] < 0] *= -1
-        eq.Z_lmn *= zone
-
-        lone = np.ones_like(eq.L_lmn)
-        lone[eq.L_basis.modes[:, 2] < 0] *= -1
-        eq.L_lmn *= lone
-
-        eq.axis = eq.get_axis()
-        eq.surface = eq.get_surface_at(rho=1)
-
-    sign = np.sign(eq.compute("iota", grid=Grid(np.array([[1, 0, 0]])))["iota"])
-    assert sign == 1
-    return eq
-
-
 def ensure_positive_jacobian(eq):
     """Convert an Equilibrium to have a positive coordinate Jacobian.
 
@@ -97,4 +49,47 @@ def ensure_positive_jacobian(eq):
 
     sign = np.sign(eq.compute("sqrt(g)", grid=Grid(np.array([[1, 0, 0]])))["sqrt(g)"])
     assert sign == 1
+    return eq
+
+
+def flip_helicity(eq):
+    """Change the sign of the helicity of an Equilibrium.
+
+    Parameters
+    ----------
+    eq : Equilibrium or iterable of Equilibrium
+        Equilibria to flip the helicity of.
+
+    Returns
+    -------
+    eq : Equilibrium or iterable of Equilibrium
+        Same as input, but with the opposite sign helicity.
+
+    """
+    # maybe it's iterable:
+    if hasattr(eq, "__len__"):
+        for e in eq:
+            flip_helicity(e)
+        return eq
+
+    if eq.iota is not None:
+        eq.i_l *= -1
+    else:
+        eq.c_l *= -1
+
+    rone = np.ones_like(eq.R_lmn)
+    rone[eq.R_basis.modes[:, 2] < 0] *= -1
+    eq.R_lmn *= rone
+
+    zone = np.ones_like(eq.Z_lmn)
+    zone[eq.Z_basis.modes[:, 2] < 0] *= -1
+    eq.Z_lmn *= zone
+
+    lone = np.ones_like(eq.L_lmn)
+    lone[eq.L_basis.modes[:, 2] < 0] *= -1
+    eq.L_lmn *= lone
+
+    eq.axis = eq.get_axis()
+    eq.surface = eq.get_surface_at(rho=1)
+
     return eq
--- tests/test_compat.py
+++ tests/test_compat.py
@@ -3,160 +3,140 @@
 import numpy as np
 import pytest
 
-from desc.compat import ensure_positive_iota
-from desc.compute.utils import surface_integrals
-from desc.equilibrium import Equilibrium
-from desc.grid import QuadratureGrid
-from desc.vmec import VMECIO
+from desc.compat import flip_helicity
+from desc.examples import get
+from desc.grid import Grid, QuadratureGrid
 
 
 @pytest.mark.unit
-def test_ensure_positive_iota_axisym():
-    """Test ensure_positive_iota on an axisymmetric Equilibrium."""
-    eq = VMECIO.load("tests/inputs/wout_DSHAPE.nc")
+def test_flip_helicity_axisym():
+    """Test flip_helicity on an axisymmetric Equilibrium."""
+    eq = get("DSHAPE")
+
     grid = QuadratureGrid(L=eq.L_grid, M=eq.M_grid, N=eq.N_grid, NFP=eq.NFP)
-    data_keys = ["iota", "current", "|F|", "D_Mercier"]
+    data_keys = ["current", "|F|", "D_Mercier"]
 
     data_old = eq.compute(data_keys, grid=grid)
-    eq = ensure_positive_iota(eq)
+    eq = flip_helicity(eq)
     data_new = eq.compute(data_keys, grid=grid)
+
+    # check that basis vectors did not change
+    np.testing.assert_allclose(data_old["e_rho"], data_new["e_rho"])
+    np.testing.assert_allclose(data_old["e_theta"], data_new["e_theta"])
+    np.testing.assert_allclose(data_old["e^zeta"], data_new["e^zeta"])
 
     # check that Jacobian is still positive
     np.testing.assert_array_less(0, grid.compress(data_new["sqrt(g)"]))
 
-    # check that iota actually changed sign
+    # check that iota changed sign
     np.testing.assert_array_less(grid.compress(data_old["iota"]), 0)
     np.testing.assert_array_less(0, grid.compress(data_new["iota"]))
 
-    # check that current actually changed sign
+    # check that current changed sign
     np.testing.assert_array_less(grid.compress(data_old["current"]), 0)
     np.testing.assert_array_less(0, grid.compress(data_new["current"]))
+
+    # check that stability did not change
+    np.testing.assert_allclose(
+        grid.compress(data_old["D_Mercier"]), grid.compress(data_new["D_Mercier"])
+    )
 
     # check that force balance did not change
     # (the collocation points are the same because of axisymmetry)
     np.testing.assert_allclose(data_old["|F|"], data_new["|F|"])
 
-    # check that stability did not change
-    np.testing.assert_allclose(
-        grid.compress(data_old["D_Mercier"]), grid.compress(data_new["D_Mercier"])
-    )
-
 
 @pytest.mark.unit
 @pytest.mark.solve
-def test_ensure_positive_iota():
-    """Test ensure_positive_iota on an Equilibrium with an iota profile."""
-    # this is the DummyStellarator, except with a negative iota profile
-    inputs = {
-        "sym": True,
-        "NFP": 3,
-        "Psi": 1.0,
-        "L": 4,
-        "M": 4,
-        "N": 4,
-        "pressure": np.array([[0, 1e4], [2, -2e4], [4, 1e4]]),
-        "iota": np.array([[0, -0.5], [2, -0.5]]),
-        "surface": np.array(
-            [
-                [0, 0, 0, 3, 0],
-                [0, 1, 0, 1, 0],
-                [0, -1, 0, 0, -1],
-                [0, 1, 1, 0.3, 0],
-                [0, -1, -1, 0.3, 0],
-                [0, 1, -1, 0, -0.3],
-                [0, -1, 1, 0, 0.3],
-            ],
-        ),
-    }
-    eq = Equilibrium(**inputs)
-    eq.solve()
+def test_flip_helicity_iota():
+    """Test flip_helicity on an Equilibrium with an iota profile."""
+    eq = get("HELIOTRON")
 
     grid = QuadratureGrid(L=eq.L_grid, M=eq.M_grid, N=eq.N_grid, NFP=eq.NFP)
-    data_keys = ["iota", "current", "|F|", "D_Mercier"]
+    nodes = grid.nodes.copy()
+    nodes[:, -1] *= -1
+    grid_flip = Grid(nodes)  # grid with negative zeta values
+    data_keys = ["current", "|F|", "D_Mercier"]
 
     data_old = eq.compute(data_keys, grid=grid)
-    eq = ensure_positive_iota(eq)
+    eq = flip_helicity(eq)
     data_new = eq.compute(data_keys, grid=grid)
+    data_flip = eq.compute(data_keys, grid=grid_flip)
+
+    # check that basis vectors did not change
+    np.testing.assert_allclose(data_old["e_rho"], data_flip["e_rho"])
+    np.testing.assert_allclose(data_old["e_theta"], data_flip["e_theta"])
+    np.testing.assert_allclose(data_old["e^zeta"], data_flip["e^zeta"])
 
     # check that Jacobian is still positive
     np.testing.assert_array_less(0, grid.compress(data_new["sqrt(g)"]))
 
-    # check that iota actually changed sign
-    np.testing.assert_array_less(grid.compress(data_old["iota"]), 0)
-    np.testing.assert_array_less(0, grid.compress(data_new["iota"]))
+    # check that iota changed sign
+    np.testing.assert_array_less(0, grid.compress(data_old["iota"]))  # old: +
+    np.testing.assert_array_less(grid.compress(data_new["iota"]), 0)  # new: -
 
-    # check that current actually changed sign
-    np.testing.assert_array_less(grid.compress(data_old["current"]), 0)
-    np.testing.assert_array_less(0, grid.compress(data_new["current"]))
-
-    # check that the total force balance error on each surface did not change
-    # (the collocation points are different because the boundary surface changed)
-    np.testing.assert_allclose(
-        surface_integrals(grid, data_old["|F|"], expand_out=False),
-        surface_integrals(grid, data_new["|F|"], expand_out=False),
-    )
+    # check that current changed sign
+    np.testing.assert_array_less(0, grid.compress(data_old["current"]))  # old: +
+    np.testing.assert_array_less(grid.compress(data_new["current"]), 0)  # new: -
 
     # check that stability did not change
     np.testing.assert_allclose(
         grid.compress(data_old["D_Mercier"]), grid.compress(data_new["D_Mercier"])
     )
+
+    # check that the total force balance error on each surface did not change
+    # (equivalent collocation points now corresond to the opposite zeta values)
+    np.testing.assert_allclose(data_old["|F|"], data_flip["|F|"])
 
 
 @pytest.mark.unit
 @pytest.mark.solve
-def test_ensure_positive_current():
-    """Test ensure_positive_iota on an Equilibrium with a current profile."""
-    # this is the DummyStellarator, except with a negative current profile
-    inputs = {
-        "sym": True,
-        "NFP": 3,
-        "Psi": 1.0,
-        "L": 4,
-        "M": 4,
-        "N": 4,
-        "pressure": np.array([[0, 1e4], [2, -2e4], [4, 1e4]]),
-        "current": np.array([[2, -1e5]]),
-        "surface": np.array(
-            [
-                [0, 0, 0, 3, 0],
-                [0, 1, 0, 1, 0],
-                [0, -1, 0, 0, -1],
-                [0, 1, 1, 0.3, 0],
-                [0, -1, -1, -0.3, 0],
-                [0, 1, -1, 0, 0.3],
-                [0, -1, 1, 0, 0.3],
-            ],
-        ),
-    }
-    eq = Equilibrium(**inputs)
-    eq.solve()
+def test_flip_helicity_current():
+    """Test flip_helicity on an Equilibrium with a current profile."""
+    # TODO: change this to use HSX example
+    eq = get("precise_QH")
 
     grid = QuadratureGrid(L=eq.L_grid, M=eq.M_grid, N=eq.N_grid, NFP=eq.NFP)
-    data_keys = ["iota", "current", "|F|", "D_Mercier"]
+    nodes = grid.nodes.copy()
+    nodes[:, -1] *= -1
+    grid_flip = Grid(nodes)  # grid with negative zeta values
+    data_keys = ["current", "|F|", "D_Mercier", "f_C"]
 
-    data_old = eq.compute(data_keys, grid=grid)
-    eq = ensure_positive_iota(eq)
-    data_new = eq.compute(data_keys, grid=grid)
+    data_old = eq.compute(data_keys, grid=grid, helicity=(1, eq.NFP))
+    eq = flip_helicity(eq)
+    data_new = eq.compute(data_keys, grid=grid, helicity=(-1, eq.NFP))
+    data_flip = eq.compute(data_keys, grid=grid_flip, helicity=(-1, eq.NFP))
+    # note that now the QH helicity is reversed: (M, N) -> (-M, N)
+
+    # check that basis vectors did not change
+    np.testing.assert_allclose(data_old["e_rho"], data_flip["e_rho"])
+    np.testing.assert_allclose(data_old["e_theta"], data_flip["e_theta"])
+    np.testing.assert_allclose(data_old["e^zeta"], data_flip["e^zeta"])
 
     # check that Jacobian is still positive
     np.testing.assert_array_less(0, grid.compress(data_new["sqrt(g)"]))
 
-    # check that iota actually changed sign
-    np.testing.assert_array_less(grid.compress(data_old["iota"]), 0)
-    np.testing.assert_array_less(0, grid.compress(data_new["iota"]))
+    # check that iota changed sign
+    np.testing.assert_array_less(0, grid.compress(data_old["iota"]))  # old: +
+    np.testing.assert_array_less(grid.compress(data_new["iota"]), 0)  # new: -
 
-    # check that current actually changed sign
-    np.testing.assert_array_less(grid.compress(data_old["current"]), 0)
-    np.testing.assert_array_less(0, grid.compress(data_new["current"]))
-
-    # check that the total force balance error on each surface did not change
-    # (the collocation points are different because the boundary surface changed)
-    np.testing.assert_allclose(
-        surface_integrals(grid, data_old["|F|"], expand_out=False),
-        surface_integrals(grid, data_new["|F|"], expand_out=False),
-    )
+    # check that current changed sign
+    """
+    # FIXME: include these tests for a QH case that has finite current, like HSX
+    np.testing.assert_array_less(0, grid.compress(data_old["current"]))  # old: +
+    np.testing.assert_array_less(grid.compress(data_new["current"]), 0)  # new: -
+    """
 
     # check that stability did not change
     np.testing.assert_allclose(
         grid.compress(data_old["D_Mercier"]), grid.compress(data_new["D_Mercier"])
     )
+
+    # check that the total force balance error on each surface did not change
+    # (equivalent collocation points now corresond to the opposite zeta values)
+    np.testing.assert_allclose(data_old["|F|"], data_flip["|F|"])
+
+    # check that the QH errors now need the opposite helicity
+    # (equivalent collocation points now corresond to the opposite zeta values)
+    np.testing.assert_allclose(data_old["f_C"], data_flip["f_C"], atol=1e-12)
+    # FIXME: can probably set atol=0 when switch to HSX example (b/c not precise QH)

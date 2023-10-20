3c6721dad7d1330a5ce4ac6b3ced44c5f0c00e25
Maxwell-Rosen <mrquell@gmail.com>
2023-10-20 08:48:53 -0400

    Coppied ForierZernikeBasis to ChebyshevZernikeBasi

--- desc/basis.py
+++ desc/basis.py
@@ -19,9 +19,6 @@ __all__ = [
     "ChebyshevDoubleFourierBasis",
     "FourierZernikeBasis",
 ]
- # Want FoureirZernikeChebyshev
- # theta r Z
- # implement differentiation
 
 class _Basis(IOAble, ABC):
     """Basis is an abstract base class for spectral basis sets."""
@@ -188,7 +185,6 @@ class _Basis(IOAble, ABC):
     def NFP(self):
         """int: Number of field periods."""
         return self.__dict__.setdefault("_NFP", 1)
-zsh:1: command not found: wq
     @property
     def sym(self):
         """str: {``'cos'``, ``'sin'``, ``False``} Type of symmetry."""
@@ -946,6 +942,243 @@ class ChebyshevDoubleFourierBasis(_Basis):
 
 
 class FourierZernikeBasis(_Basis):
+    """3D basis set for analytic functions in a toroidal volume.
+
+    Zernike polynomials in the radial & poloidal coordinates, and a Fourier
+    series in the toroidal coordinate.
+
+    Parameters
+    ----------
+    L : int
+        Maximum radial resolution. Use L=-1 for default based on M.
+    M : int
+        Maximum poloidal resolution.
+    N : int
+        Maximum toroidal resolution.
+    NFP : int
+        Number of field periods.
+    sym : {``'cos'``, ``'sin'``, ``False``}
+        * ``'cos'`` for cos(m*t-n*z) symmetry
+        * ``'sin'`` for sin(m*t-n*z) symmetry
+        * ``False`` for no symmetry (Default)
+    spectral_indexing : {``'ansi'``, ``'fringe'``}
+        Indexing method, default value = ``'ansi'``
+
+        For L=0, all methods are equivalent and give a "chevron" shaped
+        basis (only the outer edge of the zernike pyramid of width M).
+        For L>0, the indexing scheme defines order of the basis functions:
+
+        ``'ansi'``: ANSI indexing fills in the pyramid with triangles of
+        decreasing size, ending in a triangle shape. For L == M,
+        the traditional ANSI pyramid indexing is recovered. For L>M, adds rows
+        to the bottom of the pyramid, increasing L while keeping M constant,
+        giving a "house" shape.
+
+        ``'fringe'``: Fringe indexing fills in the pyramid with chevrons of
+        decreasing size, ending in a diamond shape for L=2*M where
+        the traditional fringe/U of Arizona indexing is recovered.
+        For L > 2*M, adds chevrons to the bottom, making a hexagonal diamond.
+
+    """
+
+    def __init__(self, L, M, N, NFP=1, sym=False, spectral_indexing="ansi"):
+        self.L = L
+        self.M = M
+        self.N = N
+        self._NFP = NFP
+        self._sym = sym
+        self._spectral_indexing = spectral_indexing
+
+        self._modes = self._get_modes(
+            L=self.L, M=self.M, N=self.N, spectral_indexing=self.spectral_indexing
+        )
+
+        super().__init__()
+
+    def _get_modes(self, L=-1, M=0, N=0, spectral_indexing="ansi"):
+        """Get mode numbers for Fourier-Zernike basis functions.
+
+        Parameters
+        ----------
+        L : int
+            Maximum radial resolution.
+        M : int
+            Maximum poloidal resolution.
+        N : int
+            Maximum toroidal resolution.
+        spectral_indexing : {``'ansi'``, ``'fringe'``}
+            Indexing method, default value = ``'ansi'``
+
+            For L=0, all methods are equivalent and give a "chevron" shaped
+            basis (only the outer edge of the zernike pyramid of width M).
+            For L>0, the indexing scheme defines order of the basis functions:
+
+            ``'ansi'``: ANSI indexing fills in the pyramid with triangles of
+            decreasing size, ending in a triangle shape. For L == M,
+            the traditional ANSI pyramid indexing is recovered. For L>M, adds rows
+            to the bottom of the pyramid, increasing L while keeping M constant,
+            giving a "house" shape.
+
+            ``'fringe'``: Fringe indexing fills in the pyramid with chevrons of
+            decreasing size, ending in a diamond shape for L=2*M where
+            the traditional fringe/U of Arizona indexing is recovered.
+            For L > 2*M, adds chevrons to the bottom, making a hexagonal diamond.
+
+        Returns
+        -------
+        modes : ndarray of int, shape(num_modes,3)
+            Array of mode numbers [l,m,n].
+            Each row is one basis function with modes (l,m,n).
+
+        """
+        assert spectral_indexing in [
+            "ansi",
+            "fringe",
+        ], "Unknown spectral_indexing: {}".format(spectral_indexing)
+        default_L = {"ansi": M, "fringe": 2 * M}
+        L = L if L >= 0 else default_L.get(spectral_indexing, M)
+        self.L = L
+
+        if spectral_indexing == "ansi":
+            pol_posm = [
+                [(m + d, m) for m in range(0, M + 1) if m + d < M + 1]
+                for d in range(0, L + 1, 2)
+            ]
+            if L > M:
+                pol_posm += [
+                    (l, m)
+                    for l in range(M + 1, L + 1)
+                    for m in range(0, M + 1)
+                    if (l - m) % 2 == 0
+                ]
+
+        elif spectral_indexing == "fringe":
+            pol_posm = [
+                [(m + d // 2, m - d // 2) for m in range(0, M + 1) if m - d // 2 >= 0]
+                for d in range(0, L + 1, 2)
+            ]
+            if L > 2 * M:
+                pol_posm += [
+                    [(l - m, m) for m in range(0, M + 1)]
+                    for l in range(2 * M, L + 1, 2)
+                ]
+
+        pol = [
+            [(l, m), (l, -m)] if m != 0 else [(l, m)] for l, m in flatten_list(pol_posm)
+        ]
+        pol = np.array(flatten_list(pol))
+        num_pol = len(pol)
+
+        pol = np.tile(pol, (2 * N + 1, 1))
+        tor = np.atleast_2d(
+            np.tile(np.arange(-N, N + 1), (num_pol, 1)).flatten(order="f")
+        ).T
+        return np.unique(np.hstack([pol, tor]), axis=0)
+
+    def evaluate(
+        self, nodes, derivatives=np.array([0, 0, 0]), modes=None, unique=False
+    ):
+        """Evaluate basis functions at specified nodes.
+
+        Parameters
+        ----------
+        nodes : ndarray of float, size(num_nodes,3)
+            Node coordinates, in (rho,theta,zeta).
+        derivatives : ndarray of int, shape(num_derivatives,3)
+            Order of derivatives to compute in (rho,theta,zeta).
+        modes : ndarray of int, shape(num_modes,3), optional
+            Basis modes to evaluate (if None, full basis is used).
+        unique : bool, optional
+            Whether to workload by only calculating for unique values of nodes, modes
+            can be faster, but doesn't work with jit or autodiff.
+
+        Returns
+        -------
+        y : ndarray, shape(num_nodes,num_modes)
+            Basis functions evaluated at nodes.
+
+        """
+        if modes is None:
+            modes = self.modes
+        if not len(modes):
+            return np.array([]).reshape((len(nodes), 0))
+
+        # TODO: avoid duplicate calculations when mixing derivatives
+        r, t, z = nodes.T
+        l, m, n = modes.T
+        lm = modes[:, :2]
+
+        if unique:
+            # TODO: can avoid this here by using grid.unique_idx etc
+            # and adding unique_modes attributes to basis
+            _, ridx, routidx = np.unique(
+                r, return_index=True, return_inverse=True, axis=0
+            )
+            _, tidx, toutidx = np.unique(
+                t, return_index=True, return_inverse=True, axis=0
+            )
+            _, zidx, zoutidx = np.unique(
+                z, return_index=True, return_inverse=True, axis=0
+            )
+            _, lmidx, lmoutidx = np.unique(
+                lm, return_index=True, return_inverse=True, axis=0
+            )
+            _, midx, moutidx = np.unique(
+                m, return_index=True, return_inverse=True, axis=0
+            )
+            _, nidx, noutidx = np.unique(
+                n, return_index=True, return_inverse=True, axis=0
+            )
+            r = r[ridx]
+            t = t[tidx]
+            z = z[zidx]
+            lm = lm[lmidx]
+            m = m[midx]
+            n = n[nidx]
+
+        radial = zernike_radial(r[:, np.newaxis], lm[:, 0], lm[:, 1], dr=derivatives[0])
+        poloidal = fourier(t[:, np.newaxis], m, dt=derivatives[1])
+        toroidal = fourier(z[:, np.newaxis], n, NFP=self.NFP, dt=derivatives[2])
+        if unique:
+            radial = radial[routidx][:, lmoutidx]
+            poloidal = poloidal[toutidx][:, moutidx]
+            toroidal = toroidal[zoutidx][:, noutidx]
+
+        return radial * poloidal * toroidal
+
+    def change_resolution(self, L, M, N, NFP=None, sym=None):
+        """Change resolution of the basis to the given resolutions.
+
+        Parameters
+        ----------
+        L : int
+            Maximum radial resolution.
+        M : int
+            Maximum poloidal resolution.
+        N : int
+            Maximum toroidal resolution.
+        NFP : int
+            Number of field periods.
+        sym : bool
+            Whether to enforce stellarator symmetry.
+
+        Returns
+        -------
+        None
+
+        """
+        self._NFP = NFP if NFP is not None else self.NFP
+        if L != self.L or M != self.M or N != self.N or sym != self.sym:
+            self.L = L
+            self.M = M
+            self.N = N
+            self._sym = sym if sym is not None else self.sym
+            self._modes = self._get_modes(
+                self.L, self.M, self.N, spectral_indexing=self.spectral_indexing
+            )
+            self._set_up()
+
+class ChebyshevZernikeBasis(_Basis):
     """3D basis set for analytic functions in a toroidal volume.
 
     Zernike polynomials in the radial & poloidal coordinates, and a Fourier

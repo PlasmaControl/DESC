8ed659c5b0c554200c0ee852c853142bf1bcc219
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2023-10-24 01:06:47 +0100

    test new equilibriu

--- dps/DPS.py
+++ dps/DPS.py
@@ -71,11 +71,12 @@ print("*************** START ***************")
 # Load Equilibrium
 
 print("\nStarting Equilibrium")
-eq_file = "input.LandremanPaul2021_QA_scaled_output.h5"
+# eq_file = "input.LandremanPaul2021_QA_scaled_output.h5"
+eq_file = "toroidal.h5"
 opt_file = "optimized_" + eq_file
 print(f"Loaded Equilibrium: {eq_file}\n")
 
-eq = desc.io.load(eq_file)[-1]
+eq = desc.io.load(eq_file)
 eq._iota = eq.get_profile("iota").to_powerseries(order=eq.L, sym=True)
 eq._current = None
 
@@ -154,7 +155,7 @@ print("\n*************** TRACING ***************")
 tracing_original = ParticleTracer(eq=eq, output_time=time, initial_conditions=ini_cond, initial_parameters=ini_param, compute_option="tracer", tolerance=1.4e-8)
 
 # Compute tracing original equilibrium
-eq_again = desc.io.load(eq_file)[-1]
+eq_again = desc.io.load(eq_file)
 eq_again._iota = eq_again.get_profile("iota").to_powerseries(order=eq_again.L, sym=True)
 eq_again._current = None
 

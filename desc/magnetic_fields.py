Fix edge case of derivative of 0**0

desc/magnetic_fields.py
@@ -1421,7 +1421,11 @@ def D_m_n(R, Z, m, n):
     # the sum comes from fact that D_mn = I_mn and the def of I_mn in eq 2 of the paper
 
     def body_fun(k, val):
-        return val + Z ** (n - 2 * k) / gamma(n - 2 * k + 1) * CD_m_k(R, m, k)
+        coef = CD_m_k(R, m, k) / gamma(n - 2 * k + 1)
+        exp = n - 2 * k
+        # derivative of 0**0 is ill defined, so we do this to enforce it being 0
+        exp = jnp.where((Z == 0) & (exp == 0), 1, exp)
+        return val + coef * Z**exp
 
     return fori_loop(0, n // 2 + 1, body_fun, jnp.zeros_like(R))
 
@@ -1432,7 +1436,11 @@ def N_m_n(R, Z, m, n):
     # the sum comes from fact that N_mn = I_mn and the def of I_mn in eq 2 of the paper
 
     def body_fun(k, val):
-        return val + Z ** (n - 2 * k) / gamma(n - 2 * k + 1) * CN_m_k(R, m, k)
+        coef = CN_m_k(R, m, k) / gamma(n - 2 * k + 1)
+        exp = n - 2 * k
+        # derivative of 0**0 is ill defined, so we do this to enforce it being 0
+        exp = jnp.where((Z == 0) & (exp == 0), 1, exp)
+        return val + coef * Z**exp
 
     return fori_loop(0, n // 2 + 1, body_fun, jnp.zeros_like(R))
 
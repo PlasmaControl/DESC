342b611dd42a44b20f92a0b0ddc2f609d05ca2ff
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2023-11-29 18:58:12 +0000

    Revert "trying to optimized with aspect ratio taken innto account"
    This reverts commit 977e2f1de2eeffc73a2d2581486e6bc1385b6f44.

--- dps/DPS.py
+++ dps/DPS.py
@@ -153,6 +153,10 @@ print(f"\nTime to build and compute solution: {intermediate_time_2 - intermediat
 ObjFunction = ObjectiveFunction((objective), deriv_mode="looped")
 ObjFunction.build()
 
+# gradient = ObjFunction.grad(ObjFunction.x(eq))
+
+# savetxt(gradient, "gradient")
+
 intermediate_time_3 = timet()
 print(f"\nTime to build and compile ObjFunction: {intermediate_time_3 - intermediate_time_2}s\n")
 
@@ -166,8 +170,8 @@ print(f"\nTime to build and compile ObjFunction: {intermediate_time_3 - intermed
 # Z_modes = eq.surface.Z_basis.modes[jnp.max(jnp.abs(eq.surface.Z_basis.modes), 1), :]
 
 R_modes = jnp.array([[0, 0, 0]])
-constraints = (ForceBalance(eq, bounds=(-1e-3, 1e-3)), FixBoundaryR(eq, modes=R_modes), FixBoundaryZ(eq, modes=False), 
-               FixPsi(eq), FixPressure(eq)) #FixPressure(eq), FixCurrent(eq), FixIota(eq), ForceBalance(eq, bounds=(-1e-3, 1e-3))
+constraints = (ForceBalance(eq, bounds=(-1e-3, 1e-3)), FixBoundaryR(eq, modes=R_modes), FixBoundaryZ(eq, modes=False), FixPsi(eq), FixPressure(eq), 
+               FixParameter(eq, "R0/a")) #FixPressure(eq), FixCurrent(eq), FixIota(eq), ForceBalance(eq, bounds=(-1e-3, 1e-3))
 eq.optimize(objective=ObjFunction, optimizer = "fmin-auglag-bfgs", constraints=constraints, verbose=3, maxiter=100, copy=False)
 eq.save(opt_file)
 

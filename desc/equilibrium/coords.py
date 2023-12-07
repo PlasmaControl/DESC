777332f8c2c91bd693f1b4c6dc5c6bf620f31be4
Rory Conlin <roryconlin@gmail.com>
2023-12-08 09:34:06 +1100

    Make compute_theta_coords reverse mode differentiable

--- desc/equilibrium/coords.py
+++ desc/equilibrium/coords.py
@@ -6,12 +6,11 @@ import warnings
 import numpy as np
 from termcolor import colored
 
-from desc.backend import fori_loop, jit, jnp, put, while_loop
+from desc.backend import fori_loop, jit, jnp, put, root_scalar, vmap, while_loop
 from desc.compute import compute as compute_fun
 from desc.compute import data_index, get_params, get_profiles, get_transforms
 from desc.grid import ConcentricGrid, Grid, LinearGrid, QuadratureGrid
 from desc.transform import Transform
-from desc.utils import Index
 
 
 def map_coordinates(  # noqa: C901
@@ -203,8 +202,10 @@ def map_coordinates(  # noqa: C901
     return out
 
 
-def compute_theta_coords(eq, flux_coords, L_lmn=None, tol=1e-6, maxiter=20):
-    """Find geometric theta for given straight field line theta.
+def compute_theta_coords(
+    eq, flux_coords, L_lmn=None, tol=1e-6, maxiter=20, full_output=False, **kwargs
+):
+    """Find theta_DESC for given straight field line theta_PEST.
 
     Parameters
     ----------
@@ -219,60 +220,67 @@ def compute_theta_coords(eq, flux_coords, L_lmn=None, tol=1e-6, maxiter=20):
         Stopping tolerance.
     maxiter : int > 0
         maximum number of Newton iterations
+    full_output : bool, optional
+        If True, also return a tuple where the first element is the residual from
+        the root finding and the second is the number of iterations.
+    kwargs : dict, optional
+        Additional keyword arguments to pass to ``root_scalar`` such as ``maxiter_ls``,
+        ``alpha``.
 
     Returns
     -------
     coords : ndarray, shape(k,3)
-        coordinates [rho,theta,zeta]. If Newton method doesn't converge for
-        a given coordinate nan will be returned for those values
-
+        coordinates [rho,theta,zeta].
+    info : tuple
+        2 element tuple containing residuals and number of iterations
+        for each point. Only returned if ``full_output`` is True
     """
+    kwargs.setdefault("maxiter", maxiter)
+    kwargs.setdefault("tol", tol)
+
     if L_lmn is None:
         L_lmn = eq.L_lmn
     rho, theta_star, zeta = flux_coords.T
-    if maxiter <= 0:
-        raise ValueError(f"maxiter must be a positive integer, got{maxiter}")
-    if jnp.any(rho < 0):
-        raise ValueError("rho values must be positive")
 
-    # Note: theta* (also known as vartheta) is the poloidal straight field-line
-    # angle in PEST-like flux coordinates
+    def rootfun(theta_DESC, theta_PEST, rho, zeta):
+        nodes = jnp.atleast_2d(
+            jnp.array([rho.squeeze(), theta_DESC.squeeze(), zeta.squeeze()])
+        )
+        A = eq.L_basis.evaluate(nodes)
+        lmbda = A @ L_lmn
+        theta_PESTk = theta_DESC + lmbda
+        r = (theta_PESTk % (2 * np.pi)) - (theta_PEST % (2 * np.pi))
+        # r should be between -pi and pi
+        r = jnp.where(r > np.pi, r - 2 * np.pi, r)
+        r = jnp.where(r < -np.pi, r + 2 * np.pi, r)
+        return r.squeeze()
 
-    nodes = flux_coords.copy()
-    A0 = eq.L_basis.evaluate(nodes, (0, 0, 0))
-
-    # theta* = theta + lambda
-    lmbda = jnp.dot(A0, L_lmn)
-    k = 0
-
-    def cond_fun(nodes_k_lmbda):
-        nodes, k, lmbda = nodes_k_lmbda
-        theta_star_k = nodes[:, 1] + lmbda
-        err = theta_star - theta_star_k
-        return jnp.any(jnp.abs(err) > tol) & (k < maxiter)
-
-    # Newton method for root finding
-    def body_fun(nodes_k_lmbda):
-        nodes, k, lmbda = nodes_k_lmbda
+    def jacfun(theta_DESC, theta_PEST, rho, zeta):
+        nodes = jnp.atleast_2d(
+            jnp.array([rho.squeeze(), theta_DESC.squeeze(), zeta.squeeze()])
+        )
         A1 = eq.L_basis.evaluate(nodes, (0, 1, 0))
         lmbda_t = jnp.dot(A1, L_lmn)
-        f = theta_star - nodes[:, 1] - lmbda
-        df = -1 - lmbda_t
-        nodes = put(nodes, Index[:, 1], nodes[:, 1] - f / df)
-        A0 = eq.L_basis.evaluate(nodes, (0, 0, 0))
-        lmbda = jnp.dot(A0, L_lmn)
-        k += 1
-        return (nodes, k, lmbda)
+        return 1 + lmbda_t.squeeze()
 
-    nodes, k, lmbda = jit(while_loop, static_argnums=(0, 1))(
-        cond_fun, body_fun, (nodes, k, lmbda)
+    def fixup(x):
+        return x % (2 * np.pi)
+
+    vecroot = jit(
+        vmap(
+            lambda x0, *p: root_scalar(
+                rootfun, x0, jac=jacfun, args=p, fixup=fixup, **kwargs
+            )
+        )
     )
-    theta_star_k = nodes[:, 1] + lmbda
-    err = theta_star - theta_star_k
-    noconverge = jnp.abs(err) > tol
-    nodes = jnp.where(noconverge[:, np.newaxis], jnp.nan, nodes)
+    theta_DESC, (res, niter) = vecroot(theta_star, theta_star, rho, zeta)
 
-    return nodes
+    nodes = jnp.array([rho, theta_DESC.squeeze(), zeta]).T
+
+    out = nodes
+    if full_output:
+        out = (out, (res, niter))
+    return out
 
 
 def compute_flux_coords(

3ab4bf04f2b822d68bd58505ad153d294a086d2d
Dario Panici <dpanici@princeton.edu>
2024-01-18 19:29:38 -0500

    create _coils.py file for coil related objectives

--- desc/objectives/__init__.py
+++ desc/objectives/__init__.py
@@ -1,6 +1,7 @@
 """Classes defining objectives for equilibrium and optimization."""
 
 from ._bootstrap import BootstrapRedlConsistency
+from ._coils import SurfaceCurrentRegularizedQuadraticFlux
 from ._equilibrium import (
     CurrentDensity,
     Energy,
@@ -18,7 +19,6 @@ from ._geometry import (
     MeanCurvature,
     PlasmaVesselDistance,
     PrincipalCurvature,
-    SurfaceCurrentRegularizedQuadraticFlux,
     Volume,
 )
 from ._profiles import Pressure, RotationalTransform, Shear, ToroidalCurrent

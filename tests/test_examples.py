8b275c0895d163cd1ceeccb023a6b0e56312e69d
Rory Conlin <roryconlin@gmail.com>
2024-02-02 14:27:01 -0500

    Add test for axisymmetic free boundary

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -18,6 +18,7 @@ from desc.io import load
 from desc.magnetic_fields import SplineMagneticField
 from desc.objectives import (
     AspectRatio,
+    BoundaryError,
     CurrentDensity,
     FixBoundaryR,
     FixBoundaryZ,
@@ -913,6 +914,75 @@ def test_freeb_vacuum():
     rho_err, _ = area_difference_vmec(eq, "tests/inputs/wout_test_freeb.nc")
 
     np.testing.assert_allclose(rho_err[:, -1], 0, atol=4e-2)  # only check rho=1
+
+
+@pytest.mark.regression
+@pytest.mark.solve
+@pytest.mark.slow
+def test_freeb_axisym():
+    """Test for free boundary finite beta tokamak."""
+    extcur = [
+        3.884526409876309e06,
+        -2.935577123737952e05,
+        -1.734851853677043e04,
+        6.002137016973160e04,
+        6.002540940490887e04,
+        -1.734993103183817e04,
+        -2.935531536308510e05,
+        -3.560639108717275e05,
+        -6.588434719283084e04,
+        -1.154387774712987e04,
+        -1.153546510755219e04,
+        -6.588300858364606e04,
+        -3.560589388468855e05,
+    ]
+    ext_field = SplineMagneticField.from_mgrid(
+        r"tests/inputs/mgrid_solovev.nc", extcur=extcur
+    )
+
+    pres = PowerSeriesProfile([1.25e-1, 0, -1.25e-1])
+    iota = PowerSeriesProfile([-4.9e-1, 0, 3.0e-1])
+    surf = FourierRZToroidalSurface(
+        R_lmn=[4.0, 1.0],
+        modes_R=[[0, 0], [1, 0]],
+        Z_lmn=[-1.0],
+        modes_Z=[[-1, 0]],
+        NFP=1,
+    )
+
+    eq = Equilibrium(M=10, N=0, Psi=1.0, surface=surf, pressure=pres, iota=iota)
+    eq.solve()
+    constraints = (
+        ForceBalance(eq=eq),
+        FixIota(eq=eq),
+        FixPressure(eq=eq),
+        FixPsi(eq=eq),
+    )
+    objective = ObjectiveFunction(BoundaryError(eq=eq, ext_field=ext_field))
+
+    # we know this is a pretty simple shape so we'll only use |m| <= 2
+    R_modes = (
+        eq.surface.R_basis.modes[np.max(np.abs(eq.surface.R_basis.modes), 1) > 2, :],
+    )
+
+    Z_modes = eq.surface.Z_basis.modes[
+        np.max(np.abs(eq.surface.Z_basis.modes), 1) > 2, :
+    ]
+
+    bdry_constraints = (
+        FixBoundaryR(eq=eq, modes=R_modes),
+        FixBoundaryZ(eq=eq, modes=Z_modes),
+    )
+    eq, out = eq.optimize(
+        objective,
+        constraints + bdry_constraints,
+        optimizer="proximal-lsq-exact",
+        verbose=3,
+        options={},
+    )
+    rho_err, _ = area_difference_vmec(eq, "tests/inputs/wout_solovev_freeb.nc")
+
+    np.testing.assert_allclose(rho_err[:, -1], 0, atol=1e-2)  # only check rho=1
 
 
 class TestGetExample:

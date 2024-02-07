ba9329e83287d0544ac7d09f0574bb5f14f31485
Rory Conlin <roryconlin@gmail.com>
2024-02-06 19:59:37 -0500

    Add missing args to free boundary objectives

--- desc/objectives/_free_boundary.py
+++ desc/objectives/_free_boundary.py
@@ -57,6 +57,15 @@ class VacuumBoundaryError(_Objective):
         Whether target and bounds should be normalized before comparing to computed
         values. If `normalize` is `True` and the target is in physical units,
         this should also be set to True.
+    loss_function : {None, 'mean', 'min', 'max'}, optional
+        Loss function to apply to the objective values once computed. This loss function
+        is called on the raw compute value, before any shifting, scaling, or
+        normalization.
+    deriv_mode : {"auto", "fwd", "rev"}
+        Specify how to compute jacobian matrix, either forward mode or reverse mode AD.
+        "auto" selects forward or reverse mode based on the size of the input and output
+        of the objective. Has no effect on self.grad or self.hess which always use
+        reverse mode and forward over reverse mode respectively.
     grid : Grid, optional
         Collocation grid containing the nodes to evaluate error at. Should be at rho=1.
     field_grid : Grid, optional
@@ -81,6 +90,8 @@ class VacuumBoundaryError(_Objective):
         weight=1,
         normalize=True,
         normalize_target=True,
+        loss_function=None,
+        deriv_mode="auto",
         grid=None,
         field_grid=None,
         name="Vacuum boundary error",
@@ -99,6 +110,8 @@ class VacuumBoundaryError(_Objective):
             weight=weight,
             normalize=normalize,
             normalize_target=normalize_target,
+            loss_function=loss_function,
+            deriv_mode=deriv_mode,
             name=name,
         )
 
@@ -342,6 +355,15 @@ class BoundaryError(_Objective):
         Whether target and bounds should be normalized before comparing to computed
         values. If `normalize` is `True` and the target is in physical units,
         this should also be set to True.
+    loss_function : {None, 'mean', 'min', 'max'}, optional
+        Loss function to apply to the objective values once computed. This loss function
+        is called on the raw compute value, before any shifting, scaling, or
+        normalization.
+    deriv_mode : {"auto", "fwd", "rev"}
+        Specify how to compute jacobian matrix, either forward mode or reverse mode AD.
+        "auto" selects forward or reverse mode based on the size of the input and output
+        of the objective. Has no effect on self.grad or self.hess which always use
+        reverse mode and forward over reverse mode respectively.
     s, q : integer
         Hyperparameters for singular integration scheme, s is roughly equal to the size
         of the local singular grid with respect to the global grid, q is the order of
@@ -376,6 +398,8 @@ class BoundaryError(_Objective):
         weight=1,
         normalize=True,
         normalize_target=True,
+        loss_function=None,
+        deriv_mode="auto",
         s=None,
         q=None,
         src_grid=None,
@@ -406,6 +430,8 @@ class BoundaryError(_Objective):
             weight=weight,
             normalize=normalize,
             normalize_target=normalize_target,
+            loss_function=loss_function,
+            deriv_mode=deriv_mode,
             name=name,
         )
 
@@ -761,6 +787,15 @@ class BoundaryErrorNESTOR(_Objective):
         Whether target and bounds should be normalized before comparing to computed
         values. If `normalize` is `True` and the target is in physical units,
         this should also be set to True.
+    loss_function : {None, 'mean', 'min', 'max'}, optional
+        Loss function to apply to the objective values once computed. This loss function
+        is called on the raw compute value, before any shifting, scaling, or
+        normalization.
+    deriv_mode : {"auto", "fwd", "rev"}
+        Specify how to compute jacobian matrix, either forward mode or reverse mode AD.
+        "auto" selects forward or reverse mode based on the size of the input and output
+        of the objective. Has no effect on self.grad or self.hess which always use
+        reverse mode and forward over reverse mode respectively.
     name : str
         Name of the objective function.
 
@@ -786,6 +821,8 @@ class BoundaryErrorNESTOR(_Objective):
         field_grid=None,
         normalize=True,
         normalize_target=True,
+        loss_function=None,
+        deriv_mode="auto",
         name="NESTOR Boundary",
     ):
         if target is None and bounds is None:
@@ -803,6 +840,8 @@ class BoundaryErrorNESTOR(_Objective):
             weight=weight,
             normalize=normalize,
             normalize_target=normalize_target,
+            loss_function=loss_function,
+            deriv_mode=deriv_mode,
             name=name,
         )
 

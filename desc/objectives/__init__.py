f5e81b38275669273b3e19023ab02b86eea84ad9
Dario G. Panici <dpanici@princeton.edu>
2024-02-12 16:51:31 -0500

    add puddle objective to avoid local min in current potential

--- desc/objectives/__init__.py
+++ desc/objectives/__init__.py
@@ -1,7 +1,7 @@
 """Classes defining objectives for equilibrium and optimization."""
 
 from ._bootstrap import BootstrapRedlConsistency
-from ._coils import QuadraticFlux, SurfaceCurrentRegularization
+from ._coils import CurrentPotentialPuddle, QuadraticFlux, SurfaceCurrentRegularization
 from ._equilibrium import (
     CurrentDensity,
     Energy,

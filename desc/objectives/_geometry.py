01c872baa56f76a6dab1e29b90ea75eb22319675
Kian Orr <orrk931@gmail.com>
2024-02-21 13:39:08 -0500

    address easy reviews
    docstrings, add args, don't use loop for compute

--- desc/objectives/_geometry.py
+++ desc/objectives/_geometry.py
@@ -606,25 +606,13 @@ class _CoilObjective(_Objective):
         if constants is None:
             constants = self._constants
 
-        if constants["is_mixed_coils"]:
-            coils = self.things[0].coils
-            data = [
-                coils[i].compute(
-                    self._data_keys,
-                    params=params[i],
-                    transforms=constants["transforms"][i],
-                    grid=self._grid[i],
-                )
-                for i, coil in enumerate(coils)
-            ]
-        else:
-            coils = self.things[0]
-            data = coils.compute(
-                self._data_keys,
-                params=params,
-                transforms=constants["transforms"],
-                grid=self._grid,
-            )
+        coils = self.things[0]
+        data = coils.compute(
+            self._data_keys,
+            params=params,
+            transforms=constants["transforms"],
+            grid=self._grid,
+        )
 
         return data
 
@@ -642,26 +630,60 @@ class CoilLength(_CoilObjective):
     bounds : tuple of {float, ndarray}, optional
         Lower and upper bounds on the objective. Overrides target.
         Both bounds must be broadcastable to to Objective.dim_f
+    weight : {float, ndarray}, optional
+        Weighting to apply to the Objective, relative to other Objectives.
+        Must be broadcastable to to Objective.dim_f
+    normalize : bool, optional
+        Whether to compute the error in physical units or non-dimensionalize.
+    normalize_target : bool, optional
+        Whether target and bounds should be normalized before comparing to computed
+        values. If `normalize` is `True` and the target is in physical units,
+        this should also be set to True.
+        be set to True.
+    loss_function : {None, 'mean', 'min', 'max'}, optional
+        Loss function to apply to the objective values once computed. This loss function
+        is called on the raw compute value, before any shifting, scaling, or
+        normalization. Note: Has no effect for this objective.
+    deriv_mode : {"auto", "fwd", "rev"}
+        Specify how to compute jacobian matrix, either forward mode or reverse mode AD.
+        "auto" selects forward or reverse mode based on the size of the input and output
+        of the objective. Has no effect on self.grad or self.hess which always use
+        reverse mode and forward over reverse mode respectively.
+    grid : Grid, optional
+        Collocation grid containing the nodes to evaluate at.
+    name : str, optional
+        Name of the objective function.
     """
 
-    def __init__(self, coil, target=None, bounds=None, **kwargs):
+    def __init__(
+        self,
+        coil,
+        target=None,
+        bounds=None,
+        weight=1,
+        normalize=True,
+        normalize_target=True,
+        loss_function=None,
+        deriv_mode="auto",
+        grid=None,
+        name=None,
+    ):
         if target is None and bounds is None:
             target = 2 * np.pi
 
-        super().__init__(coil, ["length"], target=target, bounds=bounds, **kwargs)
-
-    def build(self, use_jit=True, verbose=1):
-        """Build constant arrays.
-
-        Parameters
-        ----------
-        use_jit : bool, optional
-            Whether to just-in-time compile the objective and derivatives.
-        verbose : int, optional
-            Level of output.
-
-        """
-        super().build(use_jit=use_jit, verbose=verbose)
+        super().__init__(
+            coil,
+            ["length"],
+            target=target,
+            bounds=bounds,
+            weight=weight,
+            normalize=normalize,
+            normalize_target=normalize_target,
+            loss_function=loss_function,
+            deriv_mode=deriv_mode,
+            grid=grid,
+            name=name,
+        )
 
     def compute(self, params, constants=None):
         """Compute coil length.
@@ -699,26 +721,60 @@ class CoilCurvature(_CoilObjective):
     bounds : tuple of {float, ndarray}, optional
         Lower and upper bounds on the objective. Overrides target.
         Both bounds must be broadcastable to to Objective.dim_f
+    weight : {float, ndarray}, optional
+        Weighting to apply to the Objective, relative to other Objectives.
+        Must be broadcastable to to Objective.dim_f
+    normalize : bool, optional
+        Whether to compute the error in physical units or non-dimensionalize.
+    normalize_target : bool, optional
+        Whether target and bounds should be normalized before comparing to computed
+        values. If `normalize` is `True` and the target is in physical units,
+        this should also be set to True.
+        be set to True.
+    loss_function : {None, 'mean', 'min', 'max'}, optional
+        Loss function to apply to the objective values once computed. This loss function
+        is called on the raw compute value, before any shifting, scaling, or
+        normalization. Note: Has no effect for this objective.
+    deriv_mode : {"auto", "fwd", "rev"}
+        Specify how to compute jacobian matrix, either forward mode or reverse mode AD.
+        "auto" selects forward or reverse mode based on the size of the input and output
+        of the objective. Has no effect on self.grad or self.hess which always use
+        reverse mode and forward over reverse mode respectively.
+    grid : Grid, optional
+        Collocation grid containing the nodes to evaluate at.
+    name : str, optional
+        Name of the objective function.
     """
 
-    def __init__(self, coil, target=None, bounds=None, **kwargs):
+    def __init__(
+        self,
+        coil,
+        target=None,
+        bounds=None,
+        weight=1,
+        normalize=True,
+        normalize_target=True,
+        loss_function=None,
+        deriv_mode="auto",
+        grid=None,
+        name=None,
+    ):
         if target is None and bounds is None:
             target = 1 / 2
 
-        super().__init__(coil, ["curvature"], target=target, bounds=bounds, **kwargs)
-
-    def build(self, use_jit=True, verbose=1):
-        """Build constant arrays.
-
-        Parameters
-        ----------
-        use_jit : bool, optional
-            Whether to just-in-time compile the objective and derivatives.
-        verbose : int, optional
-            Level of output.
-
-        """
-        super().build(use_jit=use_jit, verbose=verbose)
+        super().__init__(
+            coil,
+            ["curvature"],
+            target=target,
+            bounds=bounds,
+            weight=weight,
+            normalize=normalize,
+            normalize_target=normalize_target,
+            loss_function=loss_function,
+            deriv_mode=deriv_mode,
+            grid=grid,
+            name=name,
+        )
 
     def compute(self, params, constants=None):
         """Compute coil curvature.

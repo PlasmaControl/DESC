0aa6e2000ce756807e6f4900e8db750ce03201b8
YigitElma <yigitelmacioglu@gmail.com>
2024-03-25 15:40:51 -0400

    fix requirements, move functions to io

--- desc/io/equilibrium_io.py
+++ desc/io/equilibrium_io.py
@@ -1,11 +1,13 @@
 """Functions and methods for saving and loading equilibria and other objects."""
 
 import copy
+import csv
 import os
 import pickle
 import pydoc
 import types
 from abc import ABC, ABCMeta
+from datetime import date
 
 import h5py
 import numpy as np
@@ -604,9 +606,6 @@ def desc_to_csv(  # noqa
     -------
         None
     """
-    import csv
-    from datetime import date
-
     from desc.equilibrium import EquilibriaFamily
     from desc.grid import LinearGrid
     from desc.vmec_utils import ptolemy_identity_rev, zernike_to_fourier
@@ -848,6 +847,63 @@ def desc_to_csv(  # noqa
             if not configurations_csv_exists:
                 writer.writeheader()  # only need header if file did not exist already
             writer.writerow(data_configurations)
+    except OSError:
+        print("I/O error")
+
+    return None
+
+
+def device_or_concept_to_csv(  # noqa
+    name,
+    device_class=None,
+    NFP=None,
+    description=None,
+    stell_sym=False,
+    deviceid=None,
+):
+    """Save DESC as a csv with relevant information.
+
+    Args
+    ----
+        eq (Equilibrium or str): DESC equilibrium to save or path to .h5 of
+         DESC equilibrium to save
+        device_class (str): class of device i.e. quasisymmetry (QA, QH, QP)
+            or omnigenity (QI, OT, OH) or axisymmetry (AS).
+        NFP (int): (Nominal) number of field periods for the device/concept
+        description (str): description of the device/concept
+        stell_sym (bool): (Nominal) stellarator symmetry of the device
+            (stellarator symmetry defined as R(theta, zeta) = R(-theta,-zeta)
+            and Z(theta, zeta) = -Z(-theta,-zeta))
+        deviceid (str): unique identifier for this device
+
+    Returns
+    -------
+        None
+    """
+    # data dicts for each table
+    devices_and_concepts = {}
+
+    devices_csv_name = "devices_and_concepts.csv"
+
+    devices_and_concepts["name"] = name
+    devices_and_concepts["class"] = device_class
+
+    devices_and_concepts["NFP"] = NFP
+    devices_and_concepts["stell_sym"] = bool(stell_sym)
+
+    devices_and_concepts["description"] = description
+    devices_and_concepts["deviceid"] = deviceid
+
+    csv_columns_desc_runs = list(devices_and_concepts.keys())
+    csv_columns_desc_runs.sort()
+    desc_runs_csv_exists = os.path.isfile(devices_csv_name)
+
+    try:
+        with open(devices_csv_name, "a+") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns_desc_runs)
+            if not desc_runs_csv_exists:
+                writer.writeheader()  # only need header if file did not exist already
+            writer.writerow(devices_and_concepts)
     except OSError:
         print("I/O error")
 

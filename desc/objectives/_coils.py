13596af2785ea778ec8f898e6567ad3b9c15fbd0
Dario Panici <dpanici@princeton.edu>
2024-04-15 15:36:22 -0500

    change to use the _CoilObjective API

--- desc/objectives/_coils.py
+++ desc/objectives/_coils.py
@@ -599,53 +599,55 @@ class CoilTorsion(_CoilObjective):
         return out
 
 
-class CoilsetMinDistance(_Objective):
+class CoilsetMinDistance(_CoilObjective):
     """Target the min distance btwn coils in the coilset.
+
+    Will yield one value per coil in the coilset, which is the
+    minimumm distance to the neighboring coils for that coilset.
 
     Parameters
     ----------
-    coilset : CoilSet, optional
-        Equilibrium that will be optimized to satisfy the Objective.
-    target : {float, ndarray}, optional
+    coil : CoilSet or Coil
+        Coil(s) that are to be optimized
+    target : float, ndarray, optional
         Target value(s) of the objective. Only used if bounds is None.
-        Must be broadcastable to Objective.dim_f.
-    bounds : tuple of {float, ndarray}, optional
+        Must be broadcastable to Objective.dim_f. If array, it has to
+        be flattened according to the number of inputs.
+    bounds : tuple of float, ndarray, optional
         Lower and upper bounds on the objective. Overrides target.
         Both bounds must be broadcastable to to Objective.dim_f
-    weight : {float, ndarray}, optional
+    weight : float, ndarray, optional
         Weighting to apply to the Objective, relative to other Objectives.
         Must be broadcastable to to Objective.dim_f
     normalize : bool, optional
         Whether to compute the error in physical units or non-dimensionalize.
-        Note: has no effect on this objective.
-    normalize_target : bool
-        Whether target should be normalized before comparing to computed values.
-        if `normalize` is `True` and the target is in physical units, this should also
+    normalize_target : bool, optional
+        Whether target and bounds should be normalized before comparing to computed
+        values. If `normalize` is `True` and the target is in physical units,
+        this should also be set to True.
         be set to True.
-        Note: has no effect on this objective.
     loss_function : {None, 'mean', 'min', 'max'}, optional
-        Loss function to apply to the objective values once computed. This function
+        Loss function to apply to the objective values once computed. This loss function
         is called on the raw compute value, before any shifting, scaling, or
-        normalization. Note: has no effect for this objective
+        normalization. Operates over all coils, not each individial coil.
     deriv_mode : {"auto", "fwd", "rev"}
         Specify how to compute jacobian matrix, either forward mode or reverse mode AD.
         "auto" selects forward or reverse mode based on the size of the input and output
         of the objective. Has no effect on self.grad or self.hess which always use
         reverse mode and forward over reverse mode respectively.
     grid : Grid, optional
-        Collocation grid containing the nodes to evaluate coils at.
+        Collocation grid containing the nodes to evaluate at.
     name : str, optional
         Name of the objective function.
     """
 
-    _coordinates = ""
-    _units = "m"
+    _scalar = False
+    _units = "(m)"
     _print_value_fmt = "CoilSet Minimum Distance: {:10.3e} "
-    _scalar = True
 
     def __init__(
         self,
-        coilset,
+        coil,
         target=None,
         bounds=None,
         weight=1,
@@ -654,15 +656,14 @@ class CoilsetMinDistance(_Objective):
         loss_function=None,
         deriv_mode="auto",
         grid=None,
-        name="minimum-coil-distance",
+        name="coil torsion",
     ):
         if target is None and bounds is None:
             target = 0
-        self._grid = grid
-        self._coilset = coilset
 
         super().__init__(
-            things=[coilset],
+            coil,
+            ["x"],
             target=target,
             bounds=bounds,
             weight=weight,
@@ -670,6 +671,7 @@ class CoilsetMinDistance(_Objective):
             normalize_target=normalize_target,
             loss_function=loss_function,
             deriv_mode=deriv_mode,
+            grid=grid,
             name=name,
         )
 
@@ -684,62 +686,44 @@ class CoilsetMinDistance(_Objective):
             Level of output.
 
         """
-        coilset = self.things[0]
-        # if field is different than self._field, update
-        if coilset != self._coilset:
-            self._coilset = coilset
-        if self._grid is None:
-            self._grid = LinearGrid(
-                N=2 * coilset[0].N + 5,
-                NFP=getattr(coilset, "NFP", 1),
-                endpoint=False,
-            )
-        transforms = get_transforms(
-            ["X", "Y", "Z"],
-            obj=coilset[0],
-            grid=self._grid,
-        )
-        self._dim_f = 1
-
-        timer = Timer()
-        if verbose > 0:
-            print("Precomputing transforms")
-        timer.start("Precomputing transforms")
-
-        self._constants = {
-            "quad_weights": 1.0,
-            "transforms": transforms,
-        }
-
-        timer.stop("Precomputing transforms")
-        if verbose > 1:
-            timer.disp("Precomputing transforms")
-
         super().build(use_jit=use_jit, verbose=verbose)
 
-    def compute(self, coil_params, constants=None):
-        """Compute quadratic flux.
+        if self._normalize:
+            self._normalization = 1 / self._scales["a"]
+
+    def compute(self, params, constants=None):
+        """Compute coil torsion.
 
         Parameters
         ----------
-        coil_params : dict
-            Dictionary of coilset degrees of freedom,
+        params : dict
+            Dictionary of the coil's degrees of freedom.
         constants : dict
             Dictionary of constant data, eg transforms, profiles etc. Defaults to
-            self.constants
+            self._constants.
 
         Returns
         -------
-        f : float
-            minimum distance between coils in the coilset
-
+        f : float or array of floats
+            Coil torsion.
         """
-        if constants is None:
-            constants = self.constants
+        data = super().compute(params, constants=constants, basis="xyz")
+        data = tree_flatten(data, is_leaf=lambda x: isinstance(x, dict))[0]
+        min_dists = []
+        for whichCoil1 in range(len(data)):
+            coords1 = data[whichCoil1]["x"]
+            mindist = 1e4
+            for whichCoil2 in range(len(data)):
+                coords2 = data[whichCoil2]["x"]
 
-        return self.things[0].compute_minimum_distance(
-            params=coil_params,
-            method="jitable",
-            grid=self._grid,
-            transforms=constants["transforms"],
-        )
+                d = jnp.linalg.norm(
+                    coords1[:, None, :] - coords2[None, :, :],
+                    axis=-1,
+                )
+
+                min_dists.append = jnp.min(jnp.array([jnp.min(d), mindist]))
+
+        return jnp.concatenate(min_dists)
+
+        out = jnp.concatenate([dat["torsion"] for dat in data])
+        return out

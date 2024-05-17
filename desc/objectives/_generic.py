456a02bb80e99f8608baf4e48693854c010ab08f
daniel-dudt <daniel.dudt@princetonstellarators.energy>
2024-05-17 17:22:13 -0400

    initial commit

--- desc/objectives/_generic.py
+++ desc/objectives/_generic.py
@@ -1,11 +1,12 @@
 """Generic objectives that don't belong anywhere else."""
 
+import functools
 import inspect
 import re
 
 import numpy as np
 
-from desc.backend import jnp
+from desc.backend import jax, jnp
 from desc.compute import compute as compute_fun
 from desc.compute import data_index
 from desc.compute.utils import get_profiles, get_transforms
@@ -13,6 +14,216 @@ from desc.grid import QuadratureGrid
 
 from .linear_objectives import _FixedObjective
 from .objective_funs import _Objective
+
+# Notes on deriv_mode:
+# batched = Derivative class calls jax.jacfwd on ObjectiveFunction.compute
+# looped = Derivative class calls jax.jvp on ObjectiveFunction.compute for each column
+# blocked = block matrix of _Objective.jac for each objective in ObjectiveFunction
+
+
+class ExternalObjective(_Objective):
+    """Wrap an external code.
+
+    Similar to ``ObjectiveFromUser``, except derivatives of the objective function are
+    computed with finite differences instead of AD.
+
+    The user supplied function should take one positional argument ``params``, which is
+    a list of the same length as `things` and corresponds to `thing.params_dict` for
+    each thing in things.
+
+    Parameters
+    ----------
+    fun : callable
+        Custom objective function.
+    things : Optimizable or tuple/list of Optimizable
+        Objects that will be optimized to satisfy the Objective.
+    target : {float, ndarray}, optional
+        Target value(s) of the objective. Only used if bounds is None.
+        Must be broadcastable to Objective.dim_f. Defaults to ``target=0``.
+    bounds : tuple of {float, ndarray}, optional
+        Lower and upper bounds on the objective. Overrides target.
+        Both bounds must be broadcastable to to Objective.dim_f.
+        Defaults to ``target=0``.
+    weight : {float, ndarray}, optional
+        Weighting to apply to the Objective, relative to other Objectives.
+        Must be broadcastable to to Objective.dim_f
+    normalize : bool, optional
+        Whether to compute the error in physical units or non-dimensionalize.
+        Has no effect for this objective.
+    normalize_target : bool, optional
+        Whether target and bounds should be normalized before comparing to computed
+        values. If `normalize` is `True` and the target is in physical units,
+        this should also be set to True.
+    loss_function : {None, 'mean', 'min', 'max'}, optional
+        Loss function to apply to the objective values once computed. This loss function
+        is called on the raw compute value, before any shifting, scaling, or
+        normalization.
+    deriv_mode : {"auto", "fwd", "rev"}  # TODO: edit this
+        Specify how to compute jacobian matrix, either forward mode or reverse mode AD.
+        "auto" selects forward or reverse mode based on the size of the input and output
+        of the objective. Has no effect on self.grad or self.hess which always use
+        reverse mode and forward over reverse mode respectively.
+    name : str, optional
+        Name of the objective function.
+
+    # TODO: add example
+
+    """
+
+    _units = "(Unknown)"
+    _print_value_fmt = "External objective value: {:10.3e}"
+
+    def __init__(
+        self,
+        fun,
+        things,
+        target=None,
+        bounds=None,
+        weight=1,
+        normalize=False,
+        normalize_target=False,
+        loss_function=None,
+        deriv_mode="auto",
+        fd_step=1e-4,  # TODO: generalize this to allow a vector of different scales
+        name="external",  # TODO: add kwargs to pass to external function
+    ):
+        if target is None and bounds is None:
+            target = 0
+        self._fun = fun
+        self._fd_step = fd_step
+        super().__init__(
+            things=things,
+            target=target,
+            bounds=bounds,
+            weight=weight,
+            normalize=normalize,
+            normalize_target=normalize_target,
+            loss_function=loss_function,
+            deriv_mode=deriv_mode,
+            name=name,
+        )
+
+    def build(self, use_jit=True, verbose=1):
+        """Build constant arrays.
+
+        Parameters
+        ----------
+        use_jit : bool, optional
+            Whether to just-in-time compile the objective and derivatives.
+        verbose : int, optional
+            Level of output.
+
+        """
+        self._dim_f = 1  # FIXME: does this need to be a user input?
+        self._scalar = self._dim_f == 1
+        self._constants = {"quad_weights": 1.0}
+
+        self._args = self._fun.__code__.co_varnames[: self._fun.__code__.co_argcount]
+
+        abstract_eval = lambda *args, **kwargs: jnp.array([1.0])  # FIXME: use dim_f?
+        self._fun_wrapped = self._jaxify(self._fun, abstract_eval)
+
+        # TODO: test if jaxify is actually working
+
+        super().build(use_jit=use_jit, verbose=verbose)
+
+    def compute(self, params, constants=None):
+        """Compute the quantity.
+
+        Parameters
+        ----------
+        params : list of dict
+            List of dictionaries of degrees of freedom, eg CoilSet.params_dict
+        constants : dict
+            Dictionary of constant data, eg transforms, profiles etc. Defaults to
+            self.constants
+
+        Returns
+        -------
+        f : ndarray
+            Computed quantity.
+
+        """
+        if constants is None:
+            constants = self.constants
+
+        new_params = {k: params[k] for k in self._args}
+        f = self._fun_wrapped(**new_params)
+        return f
+
+    def _jaxify(self, func, abstract_eval):
+        """Make an external (python) function work with JAX.
+
+        Positional arguments to func can be differentiated,
+        use keyword args for static values and non-differentiable stuff.
+
+        Note: Only forward mode differentiation is supported currently.
+
+        Parameters
+        ----------
+        func : callable
+            Function to wrap. Should be a "pure" function, in that it has no side
+            effects and doesn't maintain state. Does not need to be JAX transformable.
+        abstract_eval : callable
+            Auxilliary function that computes the output shape and dtype of func.
+            **Must be JAX transformable**. Should be of the form
+
+                abstract_eval(*args, **kwargs) -> Pytree with same shape and dtype as
+                func(*args, **kwargs)
+
+            For example, if func always returns a scalar:
+
+                abstract_eval = lambda *args, **kwargs: jnp.array(1.)
+
+            Or if func takes an array of shape(n) and returns a dict of arrays of
+            shape(n-2):
+
+                abstract_eval = lambda arr, **kwargs:
+                {"out1": jnp.empty(arr.size-2), "out2": jnp.empty(arr.size-2)}
+
+        Returns
+        -------
+        func : callable
+            New function that behaves as func but works with jit/vmap/jacfwd etc.
+
+        """
+
+        def wrap_pure_callback(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                result_shape_dtype = abstract_eval(*args, **kwargs)
+                return jax.pure_callback(func, result_shape_dtype, *args, **kwargs)
+
+            return wrapper
+
+        def define_fd_jvp(func):
+            func = jax.custom_jvp(func)
+
+            @func.defjvp
+            def func_jvp(primals, tangents):
+                primal_out = func(*primals)
+
+                # flatten everything into 1d vectors for easier finite differences
+                y, unflaty = jax.flatten_util.ravel_pytree(primal_out)
+                v, unflatv = jax.flatten_util.ravel_pytree(
+                    *tangents
+                )  # remember that primals/tangets are passed as tuples
+                x, unflatx = jax.flatten_util.ravel_pytree(*primals)
+                normv = jnp.linalg.norm(v)
+                # scale to unit norm if its nonzero
+                vh = jnp.where(normv == 0, v, v / normv)
+
+                def f(x):
+                    return jax.flatten_util.ravel_pytree(func(unflatx(x)))[0]
+
+                tangent_out = (f(x + self._fd_step * vh) - y) / self._fd_step * normv
+                tangent_out = unflaty(tangent_out)
+
+                return primal_out, tangent_out
+
+            return func
+
+        return define_fd_jvp(wrap_pure_callback(func))
 
 
 class GenericObjective(_Objective):
@@ -58,7 +269,7 @@ class GenericObjective(_Objective):
 
     """
 
-    _print_value_fmt = "GenericObjective value: {:10.3e} "
+    _print_value_fmt = "Generic objective value: {:10.3e} "
 
     def __init__(
         self,
@@ -207,7 +418,7 @@ class LinearObjectiveFromUser(_FixedObjective):
     _linear = True
     _fixed = True
     _units = "(Unknown)"
-    _print_value_fmt = "Custom linear Objective value: {:10.3e}"
+    _print_value_fmt = "Custom linear objective value: {:10.3e}"
 
     def __init__(
         self,
@@ -345,10 +556,9 @@ class ObjectiveFromUser(_Objective):
         def myfun(grid, data):
             # This will compute the flux surface average of the function
             # R*B_T from the Grad-Shafranov equation
-            f = data['R']*data['B_phi']
+            f = data['R'] * data['B_phi']
             f_fsa = surface_averages(grid, f, sqrt_g=data['sqrt_g'])
-            # this has the FSA values on the full grid, but we just want
-            # the unique values:
+            # this is the FSA on the full grid, but we only want the unique values:
             return grid.compress(f_fsa)
 
         myobj = ObjectiveFromUser(myfun)
@@ -356,7 +566,7 @@ class ObjectiveFromUser(_Objective):
     """
 
     _units = "(Unknown)"
-    _print_value_fmt = "Custom Objective value: {:10.3e}"
+    _print_value_fmt = "Custom objective value: {:10.3e}"
 
     def __init__(
         self,

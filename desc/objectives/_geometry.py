7b0c89a671f232336306623da6e07844fb2123b1
Dario Panici <dpanici@princeton.edu>
2024-05-22 11:30:46 -0400

    put logic inside the function

--- desc/objectives/_geometry.py
+++ desc/objectives/_geometry.py
@@ -832,26 +832,41 @@ class PlasmaVesselDistance(_Objective):
                     # R Z and surface points,
                     # Rtest Ztest are the point we wanna check is inside
                     # the surfaceor not
-
-                    # R Z can be vectors?
                     Rbool = R > Rtest
                     Zbool = Z > Ztest
-                    return_data = jnp.zeros_like(R)
-                    return_data = jnp.where(
-                        jnp.logical_and(Rbool, Zbool), 0, return_data
+                    quads = jnp.zeros_like(R)
+                    quads = jnp.where(jnp.logical_and(Rbool, Zbool), 0, quads)
+                    quads = jnp.where(
+                        jnp.logical_and(jnp.logical_not(Rbool), Zbool), 1, quads
                     )
-                    return_data = jnp.where(
-                        jnp.logical_and(jnp.logical_not(Rbool), Zbool), 1, return_data
-                    )
-                    return_data = jnp.where(
+                    quads = jnp.where(
                         jnp.logical_and(jnp.logical_not(Rbool), jnp.logical_not(Zbool)),
                         2,
-                        return_data,
+                        quads,
                     )
-                    return_data = jnp.where(
-                        jnp.logical_and(Rbool, jnp.logical_not(Zbool)), 3, return_data
+                    quads = jnp.where(
+                        jnp.logical_and(Rbool, jnp.logical_not(Zbool)), 3, quads
                     )
-                    return return_data
+                    deltas = quads[1:] - quads[0:-1]
+                    deltas = jnp.where(deltas == 3, -1, deltas)
+                    deltas = jnp.where(deltas == -3, 1, deltas)
+                    # then flip sign if the R intercept is > Rtest and the
+                    # quadrant flipped over a diagonal
+                    b = (Z[1:] / R[1:] - Z[0:-1] / R[0:-1]) / (Z[1:] - Z[0:-1])
+                    Rint = surf_pt[0, None] - b * (R[1:] - R[0:-1]) / (Z[1:] - Z[0:-1])
+                    deltas = jnp.where(
+                        jnp.logical_and(jnp.abs(deltas) == 2, Rint > surf_pt[0]),
+                        -deltas,
+                        deltas,
+                    )
+                    pt_sign = jnp.sum(deltas)
+                    # here, if pt_sign is +/-4,means that SURFACE is inside PLASMA
+                    # while if 0, means SURFACE is outside PLASMA
+
+                    # positive distance if the plasma pt is inside the surface, else
+                    # negative distance is assigned
+                    pt_sign = jnp.where(jnp.isclose(pt_sign, 0), 1, -1)
+                    return pt_sign
 
                 point_signs = jnp.zeros(surface_coords.shape[0])
                 for plasma_zeta_idx, surface_zeta_idx in zip(
@@ -863,35 +878,13 @@ class PlasmaVesselDistance(_Objective):
                         (plasma_pts_at_zeta_plane, plasma_pts_at_zeta_plane[0, :])
                     )
                     for i, surf_pt in enumerate(surface_pts_at_zeta_plane):
-                        quads = _find_angle_vec(
+                        pt_sign = _find_angle_vec(
                             plasma_pts_at_zeta_plane[:, 0],
                             plasma_pts_at_zeta_plane[:, 2],
                             surf_pt[0],
                             surf_pt[2],
                         )
-                        deltas = quads[1:] - quads[0:-1]
-                        deltas = jnp.where(deltas == 3, -1, deltas)
-                        deltas = jnp.where(deltas == -3, 1, deltas)
-                        # then flip sign if the R intercept is > Rtest and the
-                        # quadrant flipped over a diagonal
-                        R = plasma_pts_at_zeta_plane[:, 0]
-                        Z = plasma_pts_at_zeta_plane[:, 2]
-                        b = (Z[1:] / R[1:] - Z[0:-1] / R[0:-1]) / (Z[1:] - Z[0:-1])
-                        Rint = surf_pt[0, None] - b * (R[1:] - R[0:-1]) / (
-                            Z[1:] - Z[0:-1]
-                        )
-                        deltas = jnp.where(
-                            jnp.logical_and(jnp.abs(deltas) == 2, Rint > surf_pt[0]),
-                            -deltas,
-                            deltas,
-                        )
-                        pt_sign = jnp.sum(deltas)
-                        # here, if pt_sign is +/-4,means that SURFACE is inside PLASMA
-                        # while if 0, means SURFACE is outside PLASMA
 
-                        # positive distance if the plasma pt is inside the surface, else
-                        # negative distance is assigned
-                        pt_sign = jnp.where(jnp.isclose(pt_sign, 0), 1, -1)
                         # need to assign to correct index of the point on the surface
                         point_signs = point_signs.at[surface_zeta_idx[i]].set(pt_sign)
                 # at end here, point_signs is either +/- 1  with

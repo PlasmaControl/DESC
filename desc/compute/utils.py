cc82e1b2df4abd736e0d0747887171ca3f1e013a
YigitElma <yigitelmacioglu@gmail.com>
2024-06-05 16:37:14 -0400

    some quantities doesn't have rtz but tz or rt, change the condition to not 's'

--- desc/compute/utils.py
+++ desc/compute/utils.py
@@ -91,6 +91,31 @@ def compute(parameterization, names, params, transforms, profiles, data=None, **
         data=data,
         **kwargs,
     )
+    # By default each compute function will return in rpz basis. If the user
+    # wants the data in xyz basis, we will convert it here.
+    # TODO: think about compute functions for _curve
+    parameterization = _parse_parameterization(parameterization)
+    for name in data.keys():
+        if (
+            data_index[parameterization][name]["dim"] == 3  # it should be 3D
+            and kwargs.get("basis", "rpz").lower() == "xyz"  # user should ask in xyz
+            and ("phi" in data)  # phi is needed for conversion
+            and data_index[parameterization][name]["coordinates"] != "s"
+            and name != "x"  # x is not a vector, it is coordinates
+        ):
+            from .geom_utils import rpz2xyz_vec
+
+            data[name] = rpz2xyz_vec(data[name], phi=data["phi"])
+        elif (
+            kwargs.get("basis", "rpz").lower() == "xyz"  # user should ask in xyz
+            and data_index[parameterization][name]["coordinates"] != "s"  # not curve x
+            and name
+            == "x"  # x is the only none vector quantity that needs to be converted
+        ):
+            from .geom_utils import rpz2xyz
+
+            data[name] = rpz2xyz(data[name])
+
     return data
 
 
@@ -140,31 +165,6 @@ def _compute(
         data = data_index[parameterization][name]["fun"](
             params=params, transforms=transforms, profiles=profiles, data=data, **kwargs
         )
-
-    # By default each compute function will return in rpz basis. If the user
-    # wants the data in xyz basis, we will convert it here.
-    # TODO: think about compute functions for _curve
-    for name in data.keys():
-        if (
-            data_index[parameterization][name]["dim"] == 3  # it should be 3D
-            and kwargs.get("basis", "rpz").lower() == "xyz"  # user should ask in xyz
-            and ("phi" in data)  # phi is needed for conversion
-            and data_index[parameterization][name]["coordinates"] == "rtz"
-            and name != "x"  # x is not a vector, it is coordinates
-        ):
-            from .geom_utils import rpz2xyz_vec
-
-            data[name] = rpz2xyz_vec(data[name], phi=data["phi"])
-        elif (
-            kwargs.get("basis", "rpz").lower() == "xyz"  # user should ask in xyz
-            and data_index[parameterization][name]["coordinates"] != "s"  # not curve x
-            and name
-            == "x"  # x is the only none vector quantity that needs to be converted
-        ):
-            from .geom_utils import rpz2xyz
-
-            data[name] = rpz2xyz(data[name])
-
     return data
 
 

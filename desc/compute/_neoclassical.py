Fix wrong alpha_t calculation and add grad(|B|)*b test

desc/compute/_neoclassical.py
@@ -67,7 +67,7 @@ def _get_pitch(grid, data, num, for_adaptive=False):
     min_B = grid.compress(data["min_tz |B|"])
     max_B = grid.compress(data["max_tz |B|"])
     if for_adaptive:
-        pitch = jnp.expand_dims(1 / jnp.stack([max_B, min_B], axis=-1), axis=1)
+        pitch = jnp.reciprocal(jnp.stack([max_B, min_B], axis=-1))[:, jnp.newaxis]
         assert pitch.shape == (grid.num_rho, 1, 2)
     else:
         pitch = get_pitch(min_B, max_B, num)
@@ -108,7 +108,7 @@ def _L_ra_fsa(data, transforms, profiles, **kwargs):
     g = transforms["grid"].source_grid
     shape = (g.num_rho, g.num_alpha, g.num_zeta)
     L_ra = simpson(
-        jnp.reshape(1 / data["B^zeta"], shape),
+        jnp.reciprocal(data["B^zeta"]).reshape(shape),
         jnp.reshape(g.nodes[:, 2], shape),
         axis=-1,
     )
@@ -135,7 +135,7 @@ def _G_ra_fsa(data, transforms, profiles, **kwargs):
     g = transforms["grid"].source_grid
     shape = (g.num_rho, g.num_alpha, g.num_zeta)
     G_ra = simpson(
-        jnp.reshape(1 / (data["B^zeta"] * data["sqrt(g)"]), shape),
+        jnp.reciprocal(data["B^zeta"] * data["sqrt(g)"]).reshape(shape),
         jnp.reshape(g.nodes[:, 2], shape),
         axis=-1,
     )

desc/compute/bounce_integral.py
@@ -354,7 +354,7 @@ def plot_field_line(
     add(ax.plot(z, B(z), label=r"$\vert B \vert (\zeta)$"))
 
     if pitch is not None:
-        b = 1 / jnp.atleast_1d(pitch)
+        b = jnp.reciprocal(pitch)
         for val in b:
             add(
                 ax.axhline(
@@ -527,7 +527,7 @@ def bounce_points(pitch, knots, B_c, B_z_ra_c, check=False, plot=False, **kwargs
     P, S, N, degree = pitch.shape[0], B_c.shape[1], knots.size - 1, B_c.shape[0] - 1
     intersect = _poly_root(
         c=B_c,
-        k=1 / pitch[..., jnp.newaxis],
+        k=jnp.reciprocal(pitch)[..., jnp.newaxis],
         a_min=jnp.array([0]),
         a_max=jnp.diff(knots),
         sort=True,
@@ -624,7 +624,7 @@ def get_pitch(min_B, max_B, num, relative_shift=1e-6):
     # extrema. Shift values slightly to resolve this issue.
     min_B = (1 + relative_shift) * min_B
     max_B = (1 - relative_shift) * max_B
-    pitch = composite_linspace(1 / jnp.stack([max_B, min_B]), num)
+    pitch = composite_linspace(jnp.reciprocal(jnp.stack([max_B, min_B])), num)
     assert pitch.shape == (num + 2, *pitch.shape[1:])
     return pitch
 
@@ -949,6 +949,7 @@ def _interpolate_and_integrate(
     shape = Z.shape
     Z = Z.reshape(Z.shape[0], Z.shape[1], -1)
     f = [_interp1d_vec(Z, knots, f_i, method=method).reshape(shape) for f_i in f]
+    # TODO: Pass in derivative; add (∂e^ζ/∂_ζ) at constant ρ, α; use method_B.
     b_sup_z = _interp1d_vec(Z, knots, B_sup_z / B, method=method).reshape(shape)
     B = _interp1d_vec_with_df(Z, knots, B, B_z_ra, method=method_B).reshape(shape)
     pitch = jnp.expand_dims(pitch, axis=(2, 3) if len(shape) == 4 else 2)

tests/test_compute_funs.py
@@ -8,7 +8,8 @@ import pytest
 from scipy.signal import convolve2d
 
 from desc.coils import FourierPlanarCoil, FourierRZCoil, FourierXYZCoil, SplineXYZCoil
-from desc.compute import data_index, rpz2xyz_vec
+from desc.compute import data_index, get_data_deps, rpz2xyz_vec
+from desc.compute.utils import dot
 from desc.equilibrium import Equilibrium
 from desc.examples import get
 from desc.geometry import (
@@ -1688,3 +1689,14 @@ def test_surface_equilibrium_geometry():
                 rtol=3e-13,
                 atol=1e-13,
             )
+
+
+@pytest.mark.unit
+def test_parallel_gradient():
+    """Test different ways of computing this partial derivative agree."""
+    eq = get("W7-X")
+    assert {"B_r", "iota_r"}.isdisjoint(get_data_deps("|B|_z|r,a", eq))
+    data = eq.compute(["|B|_z|r,a", "grad(|B|)", "b"])
+    #  df = ∇f . dR
+    #  ∂f/∂ζ (constant ρ and α) = ∇f . ∂R/∂ζ (constant ρ and α)
+    np.testing.assert_allclose(data["|B|_z|r,a"], dot(data["grad(|B|)"], data["b"]))
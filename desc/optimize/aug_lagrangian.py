Fix scaling of g_norm

desc/optimize/aug_lagrangian.py
@@ -355,7 +355,9 @@ def fmin_auglag(  # noqa: C901 - FIXME: simplify this
 
     g_h = g * d
     H_h = d * H * d[:, None]
-    g_norm = jnp.linalg.norm((g_h if scaled_termination else g * v), ord=jnp.inf)
+    g_norm = jnp.linalg.norm(
+        (g * v * scale if scaled_termination else g * v), ord=jnp.inf
+    )
 
     # conngould : norm of the cauchy point, as recommended in ch17 of Conn & Gould
     # scipy : norm of the scaled x, as used in scipy
@@ -539,7 +541,7 @@ def fmin_auglag(  # noqa: C901 - FIXME: simplify this
             v, dv = cl_scaling_vector(z, g, lb, ub)
             v = jnp.where(dv != 0, v * scale_inv, v)
             g_norm = jnp.linalg.norm(
-                (g_h if scaled_termination else g * v), ord=jnp.inf
+                (g * v * scale if scaled_termination else g * v), ord=jnp.inf
             )
 
             # updating augmented lagrangian params
@@ -570,7 +572,7 @@ def fmin_auglag(  # noqa: C901 - FIXME: simplify this
                 v, dv = cl_scaling_vector(z, g, lb, ub)
                 v = jnp.where(dv != 0, v * scale_inv, v)
                 g_norm = jnp.linalg.norm(
-                    (g_h if scaled_termination else g * v), ord=jnp.inf
+                    (g * v * scale if scaled_termination else g * v), ord=jnp.inf
                 )
 
             z_norm = jnp.linalg.norm(

desc/optimize/fmin_scalar.py
@@ -240,7 +240,9 @@ def fmintr(  # noqa: C901 - FIXME: simplify this
 
     g_h = g * d
     H_h = d * H * d[:, None]
-    g_norm = jnp.linalg.norm((g_h if scaled_termination else g * v), ord=jnp.inf)
+    g_norm = jnp.linalg.norm(
+        (g * v * scale if scaled_termination else g * v), ord=jnp.inf
+    )
 
     # conngould : norm of the cauchy point, as recommended in ch17 of Conn & Gould
     # scipy : norm of the scaled x, as used in scipy
@@ -417,7 +419,7 @@ def fmintr(  # noqa: C901 - FIXME: simplify this
                 ((x * scale_inv) if scaled_termination else x), ord=2
             )
             g_norm = jnp.linalg.norm(
-                (g_h if scaled_termination else g * v), ord=jnp.inf
+                (g * v * scale if scaled_termination else g * v), ord=jnp.inf
             )
 
             if g_norm < gtol:
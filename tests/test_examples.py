0c22a103326991513c604b1aa16cb072f135a699
Dario Panici <dpanici@princeton.edu>
2024-07-11 12:29:11 -0500

    avoid intersect warning in test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1312,7 +1312,7 @@ def test_optimize_with_all_coil_types(DummyCoilSet, DummyMixedCoilSet):
     mixed_coils = load(
         load_from=str(DummyMixedCoilSet["output_path"]), file_format="hdf5"
     )
-    nested_coils = MixedCoilSet(sym_coils, mixed_coils)
+    nested_coils = MixedCoilSet(sym_coils, mixed_coils, check_intersection=False)
     eq = Equilibrium()
     # not attempting to accurately calc B for this test,
     # so make the grids very coarse

bd543a2cfafdcd758d59fd07eec7b8fd73678478
Dario Panici <dpanici@princeton.edu>
2024-08-21 20:07:17 -0400

    change num of pts

--- desc/grid.py
+++ desc/grid.py
@@ -1243,9 +1243,10 @@ class QuadratureGrid(_Grid):
         self._N = check_nonnegint(N, "N", False)
         self._NFP = check_posint(NFP, "NFP", False)
         self._period = (np.inf, 2 * np.pi, 2 * np.pi / self._NFP)
-        # divide (L+1) by 2 bc only need (L+1)/2  points to
-        # integrate L-th order jacobi polynomial exactly
-        L = round((L + 1) / 2)
+        # floor divide L by 2 bc only need (L-1)/2  points to
+        # integrate L-th order jacobi polynomial exactly, so this
+        # ensures we have enough pts for both odd and even L
+        L = L // 2
         M = 2 * M + 1
         N = 2 * N + 1
 

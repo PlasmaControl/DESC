bc1d49815595fb25a3d697cdb8240d617b0cbc0e
Dario Panici <dpanici@princeton.edu>
2024-09-10 12:11:54 +0900

    simplify function slightly

--- desc/utils_batched_vectorize.py
+++ desc/utils_batched_vectorize.py
@@ -122,12 +122,14 @@ _tree_zeros_like = functools.partial(
 # TODO put it somewhere
 def _multimap(f, *args):
     try:
+        print("one")
         return tuple(map(lambda a: f(*a), zip(*args)))
     except TypeError:
+        print("two")
         return f(*args)
 
 
-def scan_append_reduce(f, x, append_cond, op=_tree_add, zero_fun=_tree_zeros_like):
+def scan_append(f, x):
     """Evaluate f element by element in x while appending and/or reducing the results.
 
     Parameters
@@ -177,38 +179,22 @@ def scan_append_reduce(f, x, append_cond, op=_tree_add, zero_fun=_tree_zeros_lik
     # special code path if there is only one element
     # to avoid having to rely on xla/llvm to optimize the overhead away
     if jax.tree_util.tree_leaves(x)[0].shape[0] == 1:
-        return _multimap(
-            lambda c, x: jnp.expand_dims(x, 0) if c else x, append_cond, f(x0)
-        )
+        return _multimap(lambda c, x: jnp.expand_dims(x, 0) if c else x, True, f(x0))
 
     # the original idea was to use pytrees,
     # however for now just operate on the return value tuple
-    _get_append_part = functools.partial(
-        _multimap, lambda c, x: x if c else None, append_cond
-    )
-    _get_op_part = functools.partial(
-        _multimap, lambda c, x: x if not c else None, append_cond
-    )
-    _tree_select = functools.partial(
-        _multimap, lambda c, t1, t2: t1 if c else t2, append_cond
-    )
+    _get_append_part = functools.partial(_multimap, lambda c, x: x if c else None, True)
 
-    carry_init = True, _get_op_part(zero_fun(jax.eval_shape(f, x0)))
+    carry_init = True
 
     def f_(carry, x):
-        is_first, y_carry = carry
         y = f(x)
-        y_op = _get_op_part(y)
         y_append = _get_append_part(y)
-        y_reduce = op(y_carry, y_op)
-        return (False, y_reduce), y_append
+        return False, y_append
 
-    (_, res_op), res_append = jax.lax.scan(f_, carry_init, x, unroll=1)
+    _, res_append = jax.lax.scan(f_, carry_init, x, unroll=1)
     # reconstruct the result from the reduced and appended parts in the two trees
-    return _tree_select(res_append, res_op)
-
-
-scan_append = functools.partial(scan_append_reduce, append_cond=True)
+    return res_append  # _tree_select(res_append, res_op)
 
 
 # TODO in_axes a la vmap?

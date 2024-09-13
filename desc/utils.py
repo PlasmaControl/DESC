090d2622c1b66da856bd976053562b4cff98c8a8
YigitElma <yigitelmacioglu@gmail.com>
2024-09-13 10:59:54 +0900

    remove debugging function print_modes_coefs

--- desc/utils.py
+++ desc/utils.py
@@ -619,50 +619,6 @@ def unique_list(thelist):
     return unique, inds
 
 
-def print_modes_coefs(thing, coord):
-    """Print mode numbers and coefficients as a table.
-
-    Parameters
-    ----------
-    thing : equilibrium or surface
-        object to print
-    coord : string
-        Coordinate to print. It can be only "R", "Z" or "L"
-
-    """
-    if coord == "R":
-        basis = thing.R_basis
-        coeff = thing.R_lmn
-        name = "R_lmn = "
-    elif coord == "Z":
-        basis = thing.Z_basis
-        coeff = thing.Z_lmn
-        name = "Z_lmn = "
-    elif coord == "L":
-        basis = thing.L_basis
-        coeff = thing.L_lmn
-        name = "L_lmn = "
-    else:
-        raise ValueError(
-            f"Coordinate can only be 'R', 'Z' or 'L'. Given value is {coord}"
-        )
-
-    table = np.empty([basis.num_modes, basis.num_modes + 1])
-    table[:, :3] = basis.modes
-    table[:, 3] = coeff
-    print(
-        f"Printing {basis.num_modes} results for {coord} coordinate of "
-        + f"{thing.__class__.__name__}"
-    )
-    print("-------------------------------------")
-    for row in table:
-        print(
-            "{:}{:<{w}.0f} {:}{:<{w}.0f} {:}{:<{w}.0f} {:}{:>{w2}.8f}".format(
-                "l = ", row[0], "m = ", row[1], "n = ", row[2], name, row[3], w=4, w2=12
-            )
-        )
-
-
 def is_any_instance(things, cls):
     """Check if any of things is an instance of cls."""
     return any([isinstance(t, cls) for t in things])

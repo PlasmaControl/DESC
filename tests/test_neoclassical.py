1edd34961e4ea4dd9c47e95e3cb26a673d9f3629
unalmis <kayaunalmis@proton.me>
2024-09-22 00:46:58 -0400

    Complete @f0uriest 's request to compare against Neo
    https://github.com/PlasmaControl/DESC/pull/1003/files#r1655597705

--- tests/test_neoclassical.py
+++ tests/test_neoclassical.py
@@ -7,6 +7,8 @@ from tests.test_plotting import tol_1d
 
 from desc.examples import get
 from desc.grid import LinearGrid
+from desc.utils import setdefault
+from desc.vmec import VMECIO
 
 
 @pytest.mark.unit
@@ -50,6 +52,93 @@ def test_effective_ripple():
     grid = eq.get_rtz_grid(rho, alpha, zeta, coordinates="raz")
     data = eq.compute("effective ripple", grid=grid)
     assert np.isfinite(data["effective ripple"]).all()
+    eps_eff = grid.compress(data["effective ripple"])
+
+    neo_rho, neo_eps = NeoIO.read_neo_out("tests/inputs/neo_out.w7x")
+    np.testing.assert_allclose(
+        eps_eff, np.interp(rho, neo_rho, neo_eps), rtol=0.16, atol=1e-5
+    )
     fig, ax = plt.subplots()
-    ax.plot(rho, grid.compress(data["effective ripple"]), marker="o")
+    ax.plot(rho, eps_eff, marker="o")
     return fig
+
+
+class NeoIO:
+    """Class to interface with NEO."""
+
+    def __init__(self, name, eq, ns=256, M_booz=None, N_booz=None):
+        self.name = name
+        self.vmec_file = f"{name}/wout_{name}.nc"
+        self.booz_out_file = f"{name}/boozmn_{name}.nc"
+        self.neo_in_file = f"{name}/neo_in.{name}"
+        self.neo_out_file = f"neo_out.{name}"
+
+        self.eq = eq
+        self.ns = ns  # number of surfaces
+        self.M_booz = setdefault(M_booz, 3 * eq.M + 1)
+        self.N_booz = setdefault(N_booz, 3 * eq.N)
+
+    @staticmethod
+    def read_neo_out(name):
+        """Return ρ and ε¹ᐧ⁵ from given NEO output with given name."""
+        with open(name) as f:
+            array = np.array([[float(x) for x in line.split()] for line in f])
+
+        neo_eps = array[:, 1]
+        # replace nan values with linear interpolation
+        is_nan = np.isnan(neo_eps)
+        neo_eps[is_nan] = np.interp(
+            np.nonzero(is_nan)[0], np.nonzero(~is_nan)[0], neo_eps[~is_nan]
+        )
+        neo_eps = np.array(neo_eps)
+        neo_rho = np.sqrt(np.linspace(1 / (neo_eps.size + 1), 1, neo_eps.size))
+        return neo_rho, neo_eps
+
+    def write_boozmn(self):
+        """Write booz_xform's boozmn output file."""
+        self._write_VMEC()
+        self._write_neo()
+
+        import booz_xform as bx
+
+        b = bx.Booz_xform()
+        b.read_wout(self.vmec_file)
+        b.mboz = self.M_booz
+        b.nboz = self.N_booz
+        b.run()
+        b.write_boozmn(self.booz_out_file)
+
+    def _write_VMEC(self):
+        self.eq.solved = True  # must set this for NEO to run correctly
+        print(f"Writing VMEC wout to {self.vmec_file}")
+        VMECIO.save(self.eq, self.vmec_file, surfs=self.ns, verbose=0)
+
+    def _write_neo(self, N_particles=125):
+        """Write NEO input file."""
+        print(f"Writing NEO input file to {self.neo_in_file}")
+        with open(self.neo_in_file, "w+") as f:
+            f.write("'#'\n'#'\n'#'\n")
+            f.write(f" {self.booz_out_file}\n")
+            f.write(f" {self.neo_out_file}\n")
+            f.write(f" {self.ns-1}\n")
+            # surface indices
+            # https://github.com/PrincetonUniversity/STELLOPT/blob/develop/NEO/Sources/neo.f90
+            f.write(" ".join([str(x) for x in range(2, self.ns + 1)]) + "\n")
+            f.write(
+                " 300 ! number of theta points\n "
+                "300 ! number of zeta points\n "
+                + f"\n 0\n 0\n {N_particles} ! number of test particles\n "
+                f"50 ! 1 = singly trapped particles\n "
+                + "0.001 ! integration accuracy\n 100 ! number of poloidal bins\n "
+                "50 ! integration steps per field period\n "
+                + "500 ! min number of field periods\n "
+                "2000 ! max number of field periods\n"
+            )
+            f.write(
+                " 0\n 1\n 0\n 0\n 2 ! 2 = reference |B| used is max on each surface"
+                " \n 0\n 0\n 0\n 0\n 0\n"
+            )
+            f.write("'#'\n'#'\n'#'\n")
+            f.write(" 0\n")
+            f.write(f"neo_cur_{self.name}\n")
+            f.write(" 200\n 2\n 0\n")

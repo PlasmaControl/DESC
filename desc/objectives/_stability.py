f019d325d1954b579ecf94e8b07ef4e6dbc64843
Rahul <rgaur@terpmail.umd.edu>
2024-10-30 10:01:10 -0400

    trying to pass more tests

--- desc/objectives/_stability.py
+++ desc/objectives/_stability.py
@@ -596,7 +596,7 @@ class BallooningStability(_Objective):
             "a": len_data["a"],
         }
 
-        grid = eq._get_rtz_grid(
+        grid = eq.get_rtz_grid(
             rho,
             alpha,
             zeta,

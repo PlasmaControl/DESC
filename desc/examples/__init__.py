7eb5a9d18548b10290845838682d427ef9585402
Rahul <rgaur@terpmail.umd.edu>
2024-10-31 11:40:58 -0400

    removing NFP_umbilic_factor from the axis FourierRZCurve object

--- desc/examples/__init__.py
+++ desc/examples/__init__.py
@@ -1,7 +1,6 @@
 """Module for getting precomputed example equilibria."""
 
 import os
-import pdb
 
 import desc.io
 
@@ -43,7 +42,6 @@ def get(name, data=None):
     h5s = [f for f in os.listdir(here) if f.endswith(".h5")]
     h5s_lower = [f.lower() for f in h5s]
     filename = name.lower() + "_output.h5"
-    # --no-verify pdb.set_trace()
     try:
         idx = h5s_lower.index(filename)
     except ValueError as e:

99a6ab4ebfe798a922d5115ad2fc750c65ca18ec
kfrybes <kfrybes24@thea.energy>
2024-10-31 15:34:37 -0700

    remove NFP from get_rtz_grid and update notebook

--- desc/objectives/_turbulence.py
+++ desc/objectives/_turbulence.py
@@ -226,7 +226,7 @@ class EffectiveRadius(_Objective):
             "shear": iota_data["shear"][0],
             "a": len_data["a"],
         }
-        n_tor = jnp.abs(self._n_pol / (data["iota"] * eq.NFP))
+        n_tor = jnp.abs(self._n_pol / (data["iota"]))
         zeta = jnp.linspace(
             0, 2 * jnp.pi * n_tor, self._n_pol * self._knots_per_transit
         )

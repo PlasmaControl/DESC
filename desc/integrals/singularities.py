Correct normalization from previous commit

desc/integrals/singularities.py
@@ -1382,26 +1382,14 @@ def compute_K_mn(
         n = eq.compute("n_rho", grid=grid)["n_rho"]
         return dot(K_fourier + K_secular, n)
 
-    # LHS is expensive, so it is better to construct full Jacobian once
-    # rather than iterative solves like jax.scipy.sparse.linalg.cg.
     A = jacfwd(LHS)(jnp.ones(basis.num_modes * 3))
-    # Fourier coefficients of K on boundary
     K_mn, _, _, _ = jnp.linalg.lstsq(A, jnp.zeros(grid.num_nodes))
     np.testing.assert_allclose(LHS(K_mn), A @ K_mn, atol=1e-8)  # noqa: E801
     return K_mn, K_sec, transform
 
 
 def compute_B_dot_n_from_K(eq, eval_grid, source_grid, K_mn, K_sec, basis):
-    """Computes vacuum field ∇Φ ⋅ n on ∂D from surface current.
-
-    Let D, D^∁ denote the interior, exterior of a toroidal region with
-    boundary ∂D. Computes the magnetic field 𝐁 in units of Tesla such that
-
-    - 𝐁 = 𝐁₀ + ∇Φ     on D
-    - ∇ × 𝐁 = ∇ × 𝐁₀  on D ∪ D^∁ (i.e. ∇Φ is single-valued or periodic)
-    - ∇ × 𝐁₀ = μ₀ 𝐉    on D ∪ D^∁
-    - 𝐁 * ∇ρ = 0      on ∂D
-    - ∇²Φ = 0         on D
+    """Computes B ⋅ n on ∂D from surface current.
 
     Parameters
     ----------
@@ -1420,9 +1408,8 @@ def compute_B_dot_n_from_K(eq, eval_grid, source_grid, K_mn, K_sec, basis):
 
     Returns
     -------
-    dPhi_dn : jnp.ndarray
+    B_dot_n : jnp.ndarray
         Shape (``eval_grid.grid.num_nodes``, 3).
-        Vacuum field ∇Φ ⋅ n on ∂D.
 
     """
     k = min(source_grid.num_theta, source_grid.num_zeta * source_grid.NFP)
@@ -1455,8 +1442,7 @@ def compute_B_dot_n_from_K(eq, eval_grid, source_grid, K_mn, K_sec, basis):
     # ∇Φ(x ∈ ∂D) = 1/2π ∫_∂D df' K_vc × ∇G(x,x')
     # (Same instructions but divide by 2 for x ∈ D).
     Bn = dot(
-        1e7  # Biot-Savart kernel assumes Φ in units of amperes but ours is Tesla-meters
-        * singular_integral(
+        singular_integral(
             evl_data, src_data, _kernel_biot_savart, interpolator, loop=True
         ),
         evl_data["n_rho"],

tests/test_integrals.py
@@ -787,28 +787,26 @@ class TestSingularities:
     def test_laplace_bdotn_from_K(self, eq):
         """Test that Laplace solution from fit of K satisfies boundary condition."""
         MN = 40
-        src_grid = LinearGrid(M=MN, N=MN, sym=False, NFP=eq.NFP)
-        data = eq.compute(["G"], grid=src_grid)
+        grid = LinearGrid(M=MN, N=MN, sym=False, NFP=eq.NFP)
+        data = eq.compute(["G"], grid=grid)
 
         def test(G):
             G_amp = 2 * np.pi * G / mu_0
             K_mn, K_sec, K_transform = compute_K_mn(
-                eq=eq, G=G_amp, grid=src_grid, K_N=max(eq.N, 1)
+                eq=eq, G=G_amp, grid=grid, K_N=max(eq.N, 1)
             )
-            evl_grid = K_transform.grid
             Bn = compute_B_dot_n_from_K(
                 eq=eq,
-                eval_grid=evl_grid,
-                source_grid=src_grid,
+                eval_grid=grid,
+                source_grid=grid,
                 K_mn=K_mn,
                 K_sec=K_sec,
                 basis=K_transform.basis,
             )
-
             # Get data as function of theta, zeta on the only flux surface (LCFS).
-            Bn = evl_grid.meshgrid_reshape(Bn, "rtz")[0]
-            theta = evl_grid.meshgrid_reshape(evl_grid.nodes[:, 1], "rtz")[0]
-            zeta = evl_grid.meshgrid_reshape(evl_grid.nodes[:, 2], "rtz")[0]
+            Bn = grid.meshgrid_reshape(Bn, "rtz")[0]
+            theta = grid.meshgrid_reshape(grid.nodes[:, 1], "rtz")[0]
+            zeta = grid.meshgrid_reshape(grid.nodes[:, 2], "rtz")[0]
 
             fig, ax = plt.subplots()
             contour = ax.contourf(theta, zeta, Bn)
@@ -822,7 +820,7 @@ class TestSingularities:
             return fig, ax
 
         test(0)
-        fig, ax = test(src_grid.compress(data["G"])[-1])
+        fig, ax = test(grid.compress(data["G"])[-1])
         return fig
 
 
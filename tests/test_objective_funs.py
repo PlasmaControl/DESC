277148709358f02c1001372d6bc1714d32eb2e7f
unalmis <kayaunalmis@proton.me>
2024-12-31 16:17:52 -0600

    Mark todo as resolved

--- tests/test_objective_funs.py
+++ tests/test_objective_funs.py
@@ -1665,11 +1665,8 @@ class TestObjectiveFunction:
             num_pitch=num_pitch,
         )
         obj.build()
-        # TODO(#1094)
         np.testing.assert_allclose(
-            obj.compute(eq.params_dict),
-            grid.compress(data["effective ripple"]),
-            rtol=0.004,
+            obj.compute(eq.params_dict), grid.compress(data["effective ripple"])
         )
         obj = GammaC(
             eq,

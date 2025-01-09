6667c61a771ac1d10445ed614d9219325182fe7b
Dario Panici <dpanici@princeton.edu>
2025-01-08 22:28:32 -0500

    Replace 4pi*1e-7 with mu0

--- desc/integrals/singularities.py
+++ desc/integrals/singularities.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 
 import numpy as np
 import scipy
+import scipy.constants
 from interpax import fft_interp2d
 
 from desc.backend import fori_loop, jnp, put, vmap
@@ -672,7 +673,7 @@ def _kernel_biot_savart(eval_data, source_data, diag=False):
         r = r[:, None]
     else:
         r = r[:, :, None]
-    return 1e-7 * safediv(num, r**3)
+    return scipy.constants.mu_0 / 4 / jnp.pi * safediv(num, r**3)
 
 
 _kernel_biot_savart.ndim = 3
@@ -697,7 +698,7 @@ def _kernel_biot_savart_A(eval_data, source_data, diag=False):
         r = r[:, None]
     else:
         r = r[:, :, None]
-    return 1e-7 * safediv(K, r)
+    return scipy.constants.mu_0 / 4 / jnp.pi * safediv(K, r)
 
 
 _kernel_biot_savart_A.ndim = 3

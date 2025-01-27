0d93e04afde05670f30bee203db9f5a51674e0e2
Dario Panici <dpanici@princeton.edu>
2025-01-27 17:51:46 -0500

    fix error in lambda basis for nae asym

--- desc/objectives/nae_utils.py
+++ desc/objectives/nae_utils.py
@@ -47,7 +47,7 @@ def calc_zeroth_order_lambda(qsc, desc_eq, N=None):
     if desc_eq.sym:
         Lbasis_sin = FourierSeries(N=N, NFP=nfp, sym="sin")
     else:
-        Lbasis_sin = FourierSeries(N=desc_eq.N, NFP=nfp, sym=False)
+        Lbasis_sin = FourierSeries(N=N, NFP=nfp, sym=False)
     if qsc is not None:
         phi = qsc.phi
         dphi = phi[1] - phi[0]

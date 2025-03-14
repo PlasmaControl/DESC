bf076a56ff4e4469c65620aa4ec47c8fa3657e2b
dpanici <dpanici@princeton.edu>
2025-03-14 14:56:32 -0400

    update script

--- dps/optimization.py
+++ dps/optimization.py
@@ -1,15 +1,25 @@
-from desc import set_device
-set_device("gpu")
-import desc.equilibrium
-from desc.objectives import ParticleTracer, ObjectiveFunction
-from desc.grid import Grid
-import desc.io
-from desc.objectives import ForceBalance, FixBoundaryR, FixBoundaryZ, FixPressure, FixIota, FixPsi
-from desc.backend import jnp
+from time import time as timet
+
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
-from time import time as timet
+
+# set_device("gpu")
+import desc.equilibrium
+import desc.io
+from desc import set_device
+from desc.backend import jnp
+from desc.grid import Grid
+from desc.objectives import (
+    FixBoundaryR,
+    FixBoundaryZ,
+    FixIota,
+    FixPressure,
+    FixPsi,
+    ForceBalance,
+    ObjectiveFunction,
+    ParticleTracer,
+)
 
 ####################################################### Set if single or multi particle #################################################################
 """
@@ -37,28 +47,30 @@ print("****************************************")
 
 # Load Equilibrium
 eq = desc.io.load(filename)
-eq._iota = eq.get_profile("iota").to_powerseries(order=eq.L, sym=True)
+eq._iota = eq.get_profile("iota").to_powerseries(order=1, sym=True)
 eq._current = None
 # eq.solve()
 
 # Energy and Mass info
 Energy_eV = 100
-Energy_SI = Energy_eV*scipy.constants.elementary_charge
-Mass = 4*scipy.constants.proton_mass
-Charge = 2*scipy.constants.elementary_charge
+Energy_SI = Energy_eV * scipy.constants.elementary_charge
+Mass = 4 * scipy.constants.proton_mass
+Charge = 2 * scipy.constants.elementary_charge
 
 # Initial State - (psi, theta, zeta, vpar)
 zeta_i = 0.5
-theta_i = jnp.pi/2
-vpar_i = 0.7*jnp.sqrt(2*Energy_SI/Mass)
+theta_i = jnp.pi / 2
+vpar_i = 0.7 * jnp.sqrt(2 * Energy_SI / Mass)
 
 if SINGLE_PARTICLE:
     psi_i = 0.9
     ini_cond = jnp.array([psi_i, theta_i, zeta_i, vpar_i])
     gridnodes = jnp.array([psi_i, theta_i, zeta_i])
 else:
-    psi_i = jnp.linspace(0.1, 0.9, 5) 
-    ini_cond = jnp.array([[float(psi_i), theta_i, zeta_i, float(vpar_i)] for psi_i in psi_i])
+    psi_i = jnp.linspace(0.1, 0.9, 5)
+    ini_cond = jnp.array(
+        [[float(psi_i), theta_i, zeta_i, float(vpar_i)] for psi_i in psi_i]
+    )
     gridnodes = jnp.array([[float(psi_i), theta_i, zeta_i] for psi_i in psi_i])
 
 grid = Grid(nodes=gridnodes.T, jitable=False, sort=False)
@@ -66,14 +78,14 @@ grid = Grid(nodes=gridnodes.T, jitable=False, sort=False)
 # Time
 tmin = 0
 tmax = 1e-3
-nt = 150
+nt = 15
 time = jnp.linspace(tmin, tmax, nt)
 
-Mass_Charge_Ratio = Mass/Charge
+Mass_Charge_Ratio = Mass / Charge
 
 data = eq.compute(["|B|", "R"], grid=grid)
 
-mu = Energy_SI/(Mass*data["|B|"]) - (vpar_i**2)/(2*data["|B|"])
+mu = Energy_SI / (Mass * data["|B|"]) - (vpar_i**2) / (2 * data["|B|"])
 
 if SINGLE_PARTICLE:
     ini_param = jnp.array([mu[0], Mass_Charge_Ratio])
@@ -84,7 +96,14 @@ intermediate_time = timet()
 
 print(f"Time from beginning until here: {intermediate_time - initial_time}s")
 
-objective = ParticleTracer(eq=eq, output_time=time, initial_conditions=ini_cond, initial_parameters=ini_param, compute_option="optimization", tolerance=1.4e-8)
+objective = ParticleTracer(
+    eq=eq,
+    output_time=time,
+    initial_conditions=ini_cond,
+    initial_parameters=ini_param,
+    compute_option="optimization",
+    tolerance=1.4e-8,
+)
 
 objective.build()
 solution = objective.compute(*objective.xs(eq))
@@ -99,8 +118,21 @@ intermediate_time_3 = timet()
 print(f"Time to build and compile: {intermediate_time_3 - intermediate_time_2}s")
 
 R_modes = jnp.array([[0, 0, 0]])
-constraints = (ForceBalance(eq, bounds=(-1e-2, 1e-2)), FixBoundaryR(eq, modes=R_modes), FixBoundaryZ(eq, modes=False), FixPsi(eq), FixPressure(eq)) #FixPressure(eq), FixCurrent(eq), FixIota(eq), ForceBalance(eq, bounds=(-1e-3, 1e-3))
-eq.optimize(objective=ObjFunction, optimizer = "fmin-auglag-bfgs", constraints=constraints, verbose=3, maxiter=100, copy=False)
+constraints = (
+    ForceBalance(eq, bounds=(-1e-2, 1e-2)),
+    FixBoundaryR(eq, modes=R_modes),
+    FixBoundaryZ(eq, modes=False),
+    FixPsi(eq),
+    FixPressure(eq),
+)  # FixPressure(eq), FixCurrent(eq), FixIota(eq), ForceBalance(eq, bounds=(-1e-3, 1e-3))
+eq.optimize(
+    objective=ObjFunction,
+    optimizer="fmin-auglag-bfgs",
+    constraints=constraints,
+    verbose=3,
+    maxiter=100,
+    copy=False,
+)
 eq.save(savename)
 
 intermediate_time_4 = timet()
@@ -111,4 +143,3 @@ print(f"Optimized Filename: {savename}")
 final_time = timet()
 print(f"Total time: {final_time - initial_time}s")
 print("*********************** END ***********************")
-

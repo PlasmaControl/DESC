fd77bd749623eb83a88fff23473e3053700fa9a3
Dario Panici <dpanici@princeton.edu>
2025-03-26 13:20:30 -0400

    update test to actually test fix

--- tests/test_objective_funs.py
+++ tests/test_objective_funs.py
@@ -3467,8 +3467,9 @@ class TestObjectiveNaNGrad:
             modes_Z=[[-1, 0]],
             NFP=1,
         )
-
-        obj = ObjectiveFunction(SurfaceQuadraticFlux(surf, ext_field), use_jit=False)
+        # have use_jit=True here to check that runs correctly with spline
+        # coils, see PR #1656
+        obj = ObjectiveFunction(SurfaceQuadraticFlux(surf, ext_field), use_jit=True)
         obj.build()
         g = obj.grad(obj.x(surf, ext_field))
         assert not np.any(np.isnan(g)), "quadratic flux"

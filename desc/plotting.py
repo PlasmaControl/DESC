fix the units latex rendering

desc/plotting.py
@@ -1545,11 +1545,8 @@ def plot_section(
     ), f"plot section got unexpected keyword argument: {kwargs.keys()}"
 
     cax_kwargs = {"size": "5%", "pad": 0.05}
-    units = (
-        f"(${data_index['desc.equilibrium.equilibrium.Equilibrium'][name]['units']}$)"
-        if data_index["desc.equilibrium.equilibrium.Equilibrium"][name]["units"]
-        else ""
-    )
+    data_index_name = data_index["desc.equilibrium.equilibrium.Equilibrium"][name]
+    units = f"$(${data_index_name['units']}$)" if data_index_name["units"] else "$"
     for i in range(nphi):
         divider = make_axes_locatable(ax[i])
 
@@ -1566,7 +1563,7 @@ def plot_section(
 
         ax[i].set_title(
             "$"
-            + data_index["desc.equilibrium.equilibrium.Equilibrium"][name]["label"]
+            + data_index_name["label"]
             + units
             + ", $\\phi \\cdot N_{{FP}}/2\\pi = {:.3f}$".format(
                 eq.NFP * phi[i] / (2 * np.pi)
@@ -1576,16 +1573,8 @@ def plot_section(
             ax[i].set_title(
                 "%s / %s, %s"
                 % (
-                    "$"
-                    + data_index["desc.equilibrium.equilibrium.Equilibrium"][name][
-                        "label"
-                    ]
-                    + "$",
-                    "$"
-                    + data_index["desc.equilibrium.equilibrium.Equilibrium"][normalize][
-                        "label"
-                    ]
-                    + "$",
+                    "$" + data_index_name[name]["label"] + "$",
+                    "$" + data_index_name[normalize]["label"] + "$",
                     "$\\phi \\cdot N_{{FP}}/2\\pi = {:.3f}$".format(
                         eq.NFP * phi[i] / (2 * np.pi)
                     ),
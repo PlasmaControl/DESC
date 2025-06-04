solve concatenation bug for scalar and non-sclaar objective grad/jacobian

desc/backend.py
@@ -5,6 +5,7 @@ import os
 import warnings
 
 import numpy as np
+import nvtx
 from packaging.version import Version
 from termcolor import colored
 
@@ -503,6 +504,7 @@ if use_jax:  # noqa: C901
         """
         # we will use either CPU or GPU[0] for the matrix decompositions, so the
         # array of float64 should fit into single device
+        rng_pconcat = nvtx.start_range(message="Print Last", color="red")
         size = jnp.array([x.size for x in arrays])
         size = jnp.sum(size)
         if (
@@ -527,9 +529,13 @@ if use_jax:  # noqa: C901
         if mode == "concat":
             out = jnp.concatenate([jax.device_put(x, device=device) for x in arrays])
         elif mode == "hstack":
-            out = jnp.hstack([jax.device_put(x, device=device) for x in arrays])
+            out = jnp.hstack(
+                [jnp.atleast_2d(jax.device_put(x, device=device)) for x in arrays]
+            )
         elif mode == "vstack":
             out = jnp.vstack([jax.device_put(x, device=device) for x in arrays])
+
+        nvtx.end_range(rng_pconcat)
         return out
 
     def jit_with_device(method):
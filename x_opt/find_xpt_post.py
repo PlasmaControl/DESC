1f47efb44178030f5987bcdf9c1f7c09bb4e63e6
Maya Avida <maya.avida@princeton.edu>
2025-08-03 20:30:49 -0400

    change to xpt_param_scan

--- x_opt/find_xpt_post.py
+++ x_opt/find_xpt_post.py
@@ -1,12 +1,25 @@
 from find_xpt import *
 
+weights = {
+    'b_dot_n': 10,
+    'bxdl': 2e-2,
+    'xpt_dist': 1e-1
+}
+
 root_dir = "/global/homes/m/mavida/DESC/xpt_dist_sweep/xpt_dist_sweep/"
 eq_path = root_dir + "equil_div-opt_98_DESC_fixed.h5"
 eq = load(eq_path)
 
 grid = LinearGrid(rho=[1.0], theta=[3 * np.pi / 2], N=24, NFP=eq.NFP)
 l = eq.compute(["R", "phi", "Z"], grid=grid)
-out_tag = "shaping_div_98_60cm_6MA_b_dot_n10_bxdl0.02_xpt_dist1"
+rad = 0.6
+offset = 0.3
+max_current = 6e6
+
+out_tag = (
+        f"shaping_div_98_{int(rad * 100)}cm_{int(max_current / 1e6)}MA_"
+        + "_".join([f"{key}{value}" for key, value in weights.items()])
+    )
 encircling = load(root_dir + "encircling_div_98.h5")
 shaping = load(root_dir + "xpt_param_scan/" + out_tag + "_shaping_coils_final.h5")
 

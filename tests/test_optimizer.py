779a61f757ec2d780eb5b2c963f8a839bcd15af2
Dario Panici <dpanici@princeton.edu>
2025-08-20 10:59:22 -0400

    change name of method

--- tests/test_optimizer.py
+++ tests/test_optimizer.py
@@ -409,7 +409,7 @@ def test_overstepping():
     np.random.seed(0)
     objective = ObjectiveFunction(DummyObjective(things=eq), use_jit=False)
     # make gradient super noisy so it stalls
-    objective.jac_scaled_error = lambda x, *args: objective._jac_scaled_error(
+    objective.jac_scaled_error = lambda x, *args: objective.jac_scaled_error(
         x
     ) + 1e2 * (np.random.random((objective._dim_f, x.size)) - 0.5)
 

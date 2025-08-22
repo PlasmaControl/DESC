641487db28501296c7d65723e942ce0ddf82109d
Maya Avida <maya.avida@princeton.edu>
2025-08-22 12:48:11 -0400

    Moved PlasmaField to magnetic_fields

--- desc/magnetic_fields/__init__.py
+++ desc/magnetic_fields/__init__.py
@@ -23,3 +23,4 @@ from ._current_potential import (
     solve_regularized_surface_current,
 )
 from ._dommaschk import DommaschkPotentialField, dommaschk_potential
+from ._plasma_field import PlasmaField

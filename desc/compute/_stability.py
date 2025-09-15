fixing conditioning of the Gram matrix by normalizing the matrix C_rows

desc/compute/_stability.py
@@ -714,7 +714,6 @@ def _Newcomb_ball_metric(params, transforms, profiles, data, **kwargs):
         "(sqrt(g)_PEST_p)|PEST",
         "finite-n instability drive",
         "iota",
-        "iota_r",
         "psi_r",
         "p",
         "a",
@@ -1278,16 +1277,20 @@ def _AGNI(params, transforms, profiles, data, **kwargs):
         keep_2 = jnp.arange(n_total, 2 * n_total)
         keep = jnp.concatenate([keep_1, keep_2])
 
+        d_r = 1.0 / jnp.diag(D)[rho_idx]      
+        d_t = 1.0 / jnp.diag(D)[theta_idx]    
+        d_z = 1.0 / jnp.diag(D)[zeta_idx]     
+
         # Because ∂√g/∂ζ = 0
-        C_zeta_inv = D_zeta0_inv[None, ...]
+        C_zeta_inv = D_zeta0_inv[None, ...] * d_z[None, :]
 
         # batched inversion
         C_rho_reshaped = C_rho.reshape(n_rho_max * n_theta_max, n_zeta_max, n_total)
-        C_zeta_inv_C_rho_reshaped = C_zeta_inv @ C_rho_reshaped
+        C_zeta_inv_C_rho_reshaped = C_zeta_inv @ (C_rho_reshaped * d_r[None, :])
         C_zeta_inv_C_rho = C_zeta_inv_C_rho_reshaped.reshape(n_total, n_total)
 
         C_theta_reshaped = C_theta.reshape(n_rho_max * n_theta_max, n_zeta_max, n_total)
-        C_zeta_inv_C_theta_reshaped = C_zeta_inv @ C_theta_reshaped
+        C_zeta_inv_C_theta_reshaped = C_zeta_inv @ (C_theta_reshaped * d_v[None, :])
         C_zeta_inv_C_theta = C_zeta_inv_C_theta_reshaped.reshape(n_total, n_total)
 
         ## Incompressibility gives us
@@ -1454,10 +1457,12 @@ def _AGNI(params, transforms, profiles, data, **kwargs):
             Chat = Chat_node.reshape(n_total, 3 * n_total)[:, pinv]
 
             Chat = Chat[keep_1][:, keep]
+            row_norm = jnp.clip(jnp.linalg.norm(Chat, axis=1, keepdims=True), 1e-300, jnp.inf)
+            Chat = Chat/row_norm
 
             # Orthogonal projector P = I - C^T (L_G L_G^T)⁻¹ Ĉ
             G = Chat @ Chat.T
-            G = (G + G.T) / 2 + 1e-12 * jnp.eye(
+            G = (G + G.T) / 2 + 1e-14 * jnp.eye(
                 n_total - 2 * n_theta_max * n_zeta_max
             )  # Gram matrix w ridge
 
@@ -1491,6 +1496,11 @@ def _AGNI(params, transforms, profiles, data, **kwargs):
 
             # Small for modes far from marginality
             print(Chat @ v[:, 0])
+
+            #--no-verify P = jnp.eye(CTS.shape[0], CTS.dtype) - CTS
+            #--no-verify print("sym=", float(jnp.linalg.norm(P - P.T)),
+            #--no-verify       "idem=", float(jnp.linalg.norm(P@P - P)),
+            #--no-verify        "CP=", float(jnp.linalg.norm(Chat @ P)))
 
         else:
             ## Shift the diagonal of A to ensure positive definiteness
@@ -1542,7 +1552,6 @@ def _AGNI(params, transforms, profiles, data, **kwargs):
         "sqrt(g)_PEST",
         "finite-n instability drive",
         "iota",
-        "iota_r",
         "psi_r",
         "p",
         "a",
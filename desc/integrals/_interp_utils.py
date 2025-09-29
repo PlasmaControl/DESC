6a63a864017890a4151f5f500c3ab9788fd6c87c
unalmis <kayaunalmis@proton.me>
2025-09-29 14:56:11 -0700

    Replace finufft's warning with ours as demanded

--- desc/integrals/_interp_utils.py
+++ desc/integrals/_interp_utils.py
@@ -24,6 +24,7 @@ except ImportError:
         "computations that involve bounce integrals.\n"
     )
 
+
 from desc.backend import dct, jnp, rfft, rfft2, take
 from desc.integrals.quad_utils import bijection_from_disc
 from desc.utils import Index, errorif, safediv
@@ -972,3 +973,22 @@ def trig_vander(x, n, domain=(0, 2 * jnp.pi)):
     return jnp.concatenate(
         [jnp.sin(nx[..., n_rfft.size - is_even - 1 : 0 : -1]), jnp.cos(nx)], axis=-1
     )
+
+
+def _test_gpu_jax_finufft():
+    """Replacing jax-finufft's warning with ours."""
+    from tests.test_interp_utils import TestFastInterp, _test_inputs_1D
+
+    try:
+        TestFastInterp.test_non_uniform_real_FFT(*_test_inputs_1D[0])
+    except NotImplementedError:
+        warnings.warn(
+            "\njax-finufft is not installed.\n"
+            "If you want to use NUFFTs, follow the DESC installation instructions.\n"
+            "Otherwise you must set the parameter nufft_eps to zero\n"
+            "when computing effective ripple, Gamma_c, and any other\n"
+            "computations that involve bounce integrals.\n"
+        )
+
+
+_test_gpu_jax_finufft()

44f3cac90f9315c6ffbbd3b43cc1b3b4fdce4b32
Dario Panici <dpanici@princeton.edu>
2025-10-03 14:22:59 -0400

    compute a normalization explicitly

--- desc/objectives/normalization.py
+++ desc/objectives/normalization.py
@@ -14,30 +14,11 @@ def compute_scaling_factors(thing):
 
     scales = {}
 
-    def get_lowest_mode(basis, coeffs):
-        """Return lowest order coefficient (excluding m=0 modes)."""
-        # lowest order modes: [0, +1, -1, +2, -2, ...]
-        m_modes = np.arange(1, thing.M + 1)
-        m_modes = np.vstack((m_modes, -m_modes)).flatten(order="F")
-        n_modes = np.arange(thing.N + 1)
-        n_modes = np.vstack((n_modes, -n_modes)).flatten(order="F")
-        for n in n_modes:
-            for m in m_modes:
-                try:
-                    x = coeffs[basis.get_idx(M=m, N=n)]
-                    if not np.isclose(x, 0):  # mode exists and coefficient is non-zero
-                        return x
-                except ValueError:
-                    pass
-        raise ValueError("No modes found, geometry is unphysical.")
-
     if isinstance(thing, Equilibrium):
         R00 = thing.Rb_lmn[thing.surface.R_basis.get_idx(M=0, N=0)]
-        R10 = get_lowest_mode(thing.surface.R_basis, thing.Rb_lmn)
-        Z10 = get_lowest_mode(thing.surface.Z_basis, thing.Zb_lmn)
 
         scales["R0"] = R00
-        scales["a"] = np.sqrt(np.abs(R10 * Z10))
+        scales["a"] = thing.compute(["a"])["a"].squeeze()
         scales["Psi"] = abs(thing.Psi)
         scales["A"] = np.pi * scales["a"] ** 2
         scales["V"] = 2 * np.pi * scales["R0"] * scales["A"]
@@ -67,11 +48,9 @@ def compute_scaling_factors(thing):
 
     elif isinstance(thing, FourierRZToroidalSurface):
         R00 = thing.R_lmn[thing.R_basis.get_idx(M=0, N=0)]
-        R10 = get_lowest_mode(thing.R_basis, thing.R_lmn)
-        Z10 = get_lowest_mode(thing.Z_basis, thing.Z_lmn)
 
         scales["R0"] = R00
-        scales["a"] = np.sqrt(np.abs(R10 * Z10))
+        scales["a"] = thing.compute(["a"])["a"].squeeze()
         scales["A"] = np.pi * scales["a"] ** 2
         scales["V"] = 2 * np.pi * scales["R0"] * scales["A"]
 

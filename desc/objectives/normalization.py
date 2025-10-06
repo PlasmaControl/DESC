6dfc845bcda8de6d08a3f94731d7416a854d2449
Dario Panici <dpanici@princeton.edu>
2025-10-06 12:38:50 -0400

    fix bug, use or not and

--- desc/objectives/normalization.py
+++ desc/objectives/normalization.py
@@ -19,12 +19,12 @@ def compute_scaling_factors(thing):
         # lowest order modes: [0, +1, -1, +2, -2, ...]
         if basis.N > 0:
             inds_not_00 = np.where(
-                np.logical_and(abs(basis.modes[:, 1]) != 0, abs(basis.modes[:, 2]) != 0)
+                np.logical_pr(
+                    np.abs(basis.modes[:, 1]) > 0, np.abs(basis.modes[:, 2]) > 0
+                )
             )[0]
         else:
-            inds_not_00 = np.where(np.abs(basis.modes[:, 1]) != 0)[0]
-        print(coeffs[inds_not_00])
-        print(inds_not_00)
+            inds_not_00 = np.where(np.abs(basis.modes[:, 1]) > 0)[0]
         val = np.max(np.abs(coeffs[inds_not_00]))
         if not np.isclose(val, 0):
             return val
@@ -32,8 +32,8 @@ def compute_scaling_factors(thing):
 
     if isinstance(thing, Equilibrium):
         R00 = thing.Rb_lmn[thing.surface.R_basis.get_idx(M=0, N=0)]
-        R10 = get_lowest_mode(thing.surface.R_basis, thing.Rb_lmn)
-        Z10 = get_lowest_mode(thing.surface.Z_basis, thing.Zb_lmn)
+        R10 = get_lowest_mode(thing.surface.R_basis, thing.surface.R_lmn)
+        Z10 = get_lowest_mode(thing.surface.Z_basis, thing.surface.Z_lmn)
 
         scales["R0"] = R00
         scales["a"] = np.sqrt(np.abs(R10 * Z10))

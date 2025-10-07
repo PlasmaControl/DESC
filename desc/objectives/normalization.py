71d60e13645db3f463668131ee526dc7e28fd2c7
Dario Panici <dpanici@princeton.edu>
2025-10-07 12:07:36 -0400

    name and doc change

--- desc/objectives/normalization.py
+++ desc/objectives/normalization.py
@@ -14,9 +14,8 @@ def compute_scaling_factors(thing):
 
     scales = {}
 
-    def get_lowest_mode(basis, coeffs):
-        """Return average (over n) of M=+/1 modes."""
-        # lowest order modes: [0, +1, -1, +2, -2, ...]
+    def get_max_mode(basis, coeffs):
+        """Return max boundary mode that is not M=0 N=0."""
         if basis.N > 0:
             inds_not_00 = np.where(
                 np.logical_or(
@@ -32,8 +31,8 @@ def compute_scaling_factors(thing):
 
     if isinstance(thing, Equilibrium):
         R00 = thing.Rb_lmn[thing.surface.R_basis.get_idx(M=0, N=0)]
-        R10 = get_lowest_mode(thing.surface.R_basis, thing.surface.R_lmn)
-        Z10 = get_lowest_mode(thing.surface.Z_basis, thing.surface.Z_lmn)
+        R10 = get_max_mode(thing.surface.R_basis, thing.surface.R_lmn)
+        Z10 = get_max_mode(thing.surface.Z_basis, thing.surface.Z_lmn)
 
         scales["R0"] = R00
         scales["a"] = np.sqrt(np.abs(R10 * Z10))
@@ -68,8 +67,8 @@ def compute_scaling_factors(thing):
 
     elif isinstance(thing, FourierRZToroidalSurface):
         R00 = thing.R_lmn[thing.R_basis.get_idx(M=0, N=0)]
-        R10 = get_lowest_mode(thing.R_basis, thing.R_lmn)
-        Z10 = get_lowest_mode(thing.Z_basis, thing.Z_lmn)
+        R10 = get_max_mode(thing.R_basis, thing.R_lmn)
+        Z10 = get_max_mode(thing.Z_basis, thing.Z_lmn)
 
         scales["R0"] = R00
         scales["a"] = np.sqrt(np.abs(R10 * Z10))

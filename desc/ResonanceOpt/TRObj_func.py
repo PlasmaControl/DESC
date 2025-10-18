a94d6867d4663c8ace1010da3e8aaa8760d80807
paullab <paullab@paullabs-MacBook-Air.local>
2025-10-18 12:18:14 -0400

    connection with poincare plotting in firm3d is working

--- desc/ResonanceOpt/TRObj_func.py
+++ desc/ResonanceOpt/TRObj_func.py
@@ -10,5 +10,5 @@ def TrappedResonanceObj(eq,rhos,pitch_invs,KE_frac,alphas,N):
     out = obj.compute(eq.params_dict) # when not flattened, this shape is (rho,pitch,energy)
 
     # Save objective function values to the firm3d directory for plotting with Poincare plots
-    np.savetxt('/Users/paullab/codes/firm3d_fork_10132025/firm3d/examples/trapped_map/obj_DESC.txt',out['obj'][:,0,0]) # value at all surfaces and one pitch (and one energy)
+    # np.savetxt('/Users/paullab/codes/firm3d_fork_10132025/firm3d/examples/trapped_map/obj_DESC.txt',out['obj'][:,0,0]) # value at all surfaces and one pitch (and one energy)
     return out

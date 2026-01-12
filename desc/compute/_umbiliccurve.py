c1d88fded872a41333d73c5b7f406bdbac941023
Jaydeep Singh <jaydeepsingh15@gmail.com>
2026-01-12 11:51:48 -0800

    Add theta compute function to FourierUmbilicCurve

--- desc/compute/_umbiliccurve.py
+++ desc/compute/_umbiliccurve.py
@@ -9,9 +9,9 @@ from .data_index import register_compute_fun
     units="~",
     units_long="None",
     description="angular difference between theta and phi on a surface"
-    + "determines the shape of an umbilic curve",
+    + r", equal to n_umbilic \theta - m_umbilic NFP \zeta",
     dim=1,
-    params=["UC_n"],
+    params=["a_n"],
     transforms={"UC": [[0, 0, 0]], "grid": []},
     profiles=[],
     coordinates="",
@@ -20,7 +20,7 @@ from .data_index import register_compute_fun
 )
 def _UC_FourierUmbilicCurve(params, transforms, profiles, data, **kwargs):
     # If the grid is non-uniform, the transform has to be modified
-    data["UC"] = transforms["UC"].transform(params["UC_n"], dz=0)
+    data["UC"] = transforms["UC"].transform(params["a_n"], dz=0)
     return data
 
 
@@ -29,7 +29,7 @@ def _UC_FourierUmbilicCurve(params, transforms, profiles, data, **kwargs):
     label="phi",
     units="~",
     units_long="Radians",
-    description="Toroidal phi position along curve",
+    description="Values of toroidal angle phi along curve",
     dim=1,
     params=[],
     transforms={"grid": []},
@@ -40,4 +40,27 @@ def _UC_FourierUmbilicCurve(params, transforms, profiles, data, **kwargs):
 )
 def _phi(params, transforms, profiles, data, **kwargs):
     data["phi"] = transforms["grid"].nodes[:, 2]
+    return data
+
+
+@register_compute_fun(
+    name="theta",
+    label="theta",
+    units="~",
+    units_long="Radians",
+    description="Values of poloidal angle theta along curve",
+    dim=1,
+    params=["n_umbilic", "m_umbilic", "NFP"],
+    transforms={"grid": []},
+    profiles=[],
+    coordinates="phi",
+    data=["UC", "phi"],
+    parameterization="desc.geometry.umbiliccurve.FourierUmbilicCurve",
+)
+def _theta(params, transforms, profiles, data, **kwargs):
+    data["theta"] = (
+        1
+        / params["n_umbilic"]
+        * (params["m_umbilic"] * params["NFP"] * data["phi"] + data["UC"])
+    )
     return data

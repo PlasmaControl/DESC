b6fea159d3d10d3678fceb1e35f607ef38b1ee98
Lanke Fu <lf2869@nyu.edu>
2026-02-12 17:48:06 -0500

    Moving `_set_tight_layout` from line 988 to 973 to avoid RuntimeError: Colorbar layout of new layout engine not compatible with old engine, and a colorbar has been created. Engine not changed.

--- desc/plotting.py
+++ desc/plotting.py
@@ -970,6 +970,7 @@ def plot_2d(  # noqa : C901
     else:
         im = ax.contourf(xx, yy, data, **contourf_kwargs)
     cax = divider.append_axes("right", **cax_kwargs)
+    _set_tight_layout(fig)
     cbar = fig.colorbar(im, cax=cax)
     cbar.update_ticks()
     xlabel = _AXIS_LABELS_RTZ[plot_axes[1]]
@@ -985,7 +986,6 @@ def plot_2d(  # noqa : C901
                 "$" + data_index[parameterization][normalize]["label"] + "$",
             )
         )
-    _set_tight_layout(fig)
     plot_data = {
         xlabel.strip("$").strip("\\"): xx,
         ylabel.strip("$").strip("\\"): yy,

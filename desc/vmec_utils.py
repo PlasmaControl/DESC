1710c2aedbe414063498322d3a12722cf8abfc03
Dario Panici <37969854+dpanici@users.noreply.github.com>
2026-02-20 11:01:06 -0500

    Apply suggestion from @dpanici

--- desc/vmec_utils.py
+++ desc/vmec_utils.py
@@ -695,7 +695,7 @@ def make_boozmn_output(  # noqa: C901
         np.array(
             [" " * (100 - len(version_str))],
             "S" + str(file.dimensions["dim_00100"].size),
-        ),
+        )
     )
 
     nfp = file.createVariable("nfp_b", np.int32)

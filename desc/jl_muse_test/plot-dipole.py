220f984e80c4ebd53c85d5b21a993c1563fa75ec
Tony Qian <tony.m.qian@gmail.com>
2026-03-03 17:43:14 -0600

    updating pyvista plot

--- desc/jl_muse_test/plot-dipole.py
+++ desc/jl_muse_test/plot-dipole.py
@@ -7,8 +7,9 @@ fin = sys.argv[1]
 data = np.genfromtxt(fin, delimiter=',', skip_header=1)
 x,y,z,m,phi,theta = data.T
 
-
-cloud = pv.PolyData(np.transpose([x,y,z]))
+idx_nonzero = np.argwhere(np.array(m!=0, int))[:,0]
+points = np.transpose([x,y,z])[idx_nonzero]
+cloud = pv.PolyData(points)
 
 # optionally attach a scalar for coloring
 #cloud["values"] = some_scalar_array  # shape (10000,)
@@ -17,7 +18,7 @@ pl = pv.Plotter()
 pl.add_points(
     cloud,
     #    scalars="values",
-    point_size=5,
+    point_size=8,
     render_points_as_spheres=True,
     cmap="viridis",
 )

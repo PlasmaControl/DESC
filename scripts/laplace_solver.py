42ad3eaebd20ae3467cd936bb499c660b7b6447a
maya-avida <maya.avida@princeton.edu>
2026-03-14 13:01:27 -0400

    Revert "using built-in vectorization to see if this makes it faster"
    This reverts commit 98f39ed450e48b1addf3222712b05ef62e974256.

--- scripts/laplace_solver.py
+++ scripts/laplace_solver.py
@@ -1,6 +1,3 @@
-from desc import set_device
-set_device("gpu")
-
 import numpy as np
 from desc.geometry import FourierRZToroidalSurface
 from desc.utils import rpz2xyz
@@ -20,9 +17,12 @@ aspect_ratio = 2
 # Define poloidal and toroidal resolution
 grid = LinearGrid(M=32, N=32)
 
+# Define field type
+field_type = "coil"
+
 # Save path
 save_path = "phi_matrix/"
-fname = f"M_{grid.M}_N_{grid.N}_phi_matrix.npy"
+fname = f"{field_type}_M_{grid.M}_N_{grid.N}_phi_matrix.npy"
 os.makedirs(save_path, exist_ok=True)
 
 # Make surface
@@ -44,6 +44,25 @@ assert surface.NFP == 1
 # Compute on-surface data
 data_keys = ["x", "n_rho", "a"]
 data = surface.compute(data_keys, grid=grid, basis="xyz")
+
+
+if field_type == "greens_function":
+    # This field type corresponds to a magnetic monopole
+    # It's the field used in Kaya's original test
+    # But it's not physical
+    x0 = rpz2xyz(np.array([R0, 0, 0]))
+    B = _grad_G(data["x"] - x0)
+elif field_type == "coil":
+    # The field type should be a quadrupole,
+    # since it is two currents with opposing sign
+    # This also means there should be no net current in ~\mathcal{X}
+    # So there are no secular terms in \Phi
+    I = 1e6
+    coil1 = FourierRZCoil(current=I, R_n=[R0], Z_n=[data["a"] / 2], NFP=1)
+    coil2 = FourierRZCoil(current=-I, R_n=[R0], Z_n=[-data["a"] / 2], NFP=1)
+    coil = CoilSet([coil1, coil2])
+
+    B = coil.compute_magnetic_field(data["x"], source_grid=LinearGrid(N=grid.N), basis="xyz")
 
 # Precompute interpolator and surface values
 field = SourceFreeField(surface, grid.M, grid.N)

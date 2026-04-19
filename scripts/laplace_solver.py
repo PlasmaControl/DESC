677504d690351951436d2cba46659eab3625e9f7
maya-avida <maya.avida@princeton.edu>
2026-04-19 19:40:53 -0400

    forgot to actually set pest false

--- scripts/laplace_solver.py
+++ scripts/laplace_solver.py
@@ -58,7 +58,7 @@ for i, res in enumerate(resolutions):
     if fixed_point:
         pest = False
     else:
-        pest = True
+        pest = False
     coords = "($\\rho, \\vartheta, \\phi$)" if pest else "($\\rho, \\theta, \\zeta$)" 
     from_scratch = False
 

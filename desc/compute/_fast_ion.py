cdb9bf11d1584b656c0c3125580007044c07ad70
unalmis <kayaunalmis@proton.me>
2026-04-22 22:06:30 -0700

    .

--- desc/compute/_fast_ion.py
+++ desc/compute/_fast_ion.py
@@ -148,9 +148,6 @@ def _Gamma_c(params, transforms, profiles, data, **kwargs):
     opts = Options.guess(-2, grid, **kwargs)
 
     def Gamma_c(data):
-        pitch_inv, weight = Bounce2D.get_pitch_inv_quad(
-            data["min_tz |B|"], data["max_tz |B|"], opts.speed_quad
-        )
         bounce = Bounce2D(grid, data, data["angle"], **opts)
 
         def fun(pitch_inv):
@@ -177,6 +174,9 @@ def _Gamma_c(params, transforms, profiles, data, **kwargs):
             )
             return (v_tau * gamma_c**2).sum(-1).mean(-2)
 
+        pitch_inv, weight = Bounce2D.get_pitch_inv_quad(
+            data["min_tz |B|"], data["max_tz |B|"], opts.speed_quad
+        )
         return jnp.sum(
             batch_map(fun, pitch_inv, opts.pitch_batch_size) * weight / pitch_inv**2,
             axis=-1,
@@ -318,9 +318,6 @@ def _Gamma_c_Velasco(params, transforms, profiles, data, **kwargs):
     opts = Options.guess(-1, grid, **kwargs)
 
     def Gamma_c(data):
-        pitch_inv, weight = Bounce2D.get_pitch_inv_quad(
-            data["min_tz |B|"], data["max_tz |B|"], opts.speed_quad
-        )
         bounce = Bounce2D(grid, data, data["angle"], **opts)
 
         def fun(pitch_inv):
@@ -336,6 +333,9 @@ def _Gamma_c_Velasco(params, transforms, profiles, data, **kwargs):
             gamma_c = jnp.arctan(safediv(radial_drift, poloidal_drift))
             return (v_tau * gamma_c**2).sum(-1).mean(-2)
 
+        pitch_inv, weight = Bounce2D.get_pitch_inv_quad(
+            data["min_tz |B|"], data["max_tz |B|"], opts.speed_quad
+        )
         return jnp.sum(
             batch_map(fun, pitch_inv, opts.pitch_batch_size) * weight / pitch_inv**2,
             axis=-1,

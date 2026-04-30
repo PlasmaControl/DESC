3314e9ad64ba9dde14acf83db9986b460e51c4c5
Yigit Gunsur Elmacioglu <102380275+YigitElma@users.noreply.github.com>
2026-04-30 16:02:34 -0400

    Minor changes from MPI branch (#2153)
    Some changes that were required to make MPI work, and I think are useful
    apart from MPI.
    
    - Adds a helper function for all different compute methods (similar to
    jvp, jac wrappers we have)
    - Adds `use_jit_wrapper` logic, in case any of the sub-objectives
    shouldn't be compiled or they shouldn't be compiled together (as in the
    case of MPI)
    - Moves `constants` assignment to proper place (this was causing a bug
    in MPI PR, don't remember why exactly though)
    - Adds jit decorator to `ObjectiveFunction.unpack_params()` (this makes
    the difference in benchmarks, specifically [this
    line](https://github.com/PlasmaControl/DESC/blob/7238beae389ea906fac5ff90bc3a607af051dcc7/desc/objectives/utils.py#L156))
    and to `ObjectiveFunction.x()`. The substantial speed-up (~1.5s) comes
    from the compilation overhead, but even after that, each call is 3-4
    times faster (but these take couple milliseconds)
    
    Resolves #2165
    
    ---------
    
    Co-authored-by: Daniel Dudt <33005725+ddudt@users.noreply.github.com>

--- desc/optimize/_constraint_wrappers.py
+++ desc/optimize/_constraint_wrappers.py
@@ -1310,7 +1310,23 @@ class ProximalProjection(ObjectiveFunction):
 # define these helper functions that are stateless so we can safely jit them
 
 
-@functools.partial(jit, static_argnames=["op"])
+def jit_if_possible(func):
+    """Jit a function if use_jit."""
+    jitted_func = functools.partial(jit, static_argnames=["op"])(func)
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # first arg has to be ObjectiveFunction
+        obj = args[0]
+        if getattr(obj, "_use_jit", False):
+            return jitted_func(*args, **kwargs)
+        else:
+            return func(*args, **kwargs)
+
+    return wrapper
+
+
+@jit_if_possible
 def _proximal_jvp_f_pure(constraint, xf, constants, dc, eq_feasible_tangents, dxdc, op):
     # Note: This function is called by _get_tangent which is vectorized over v
     # (v is called dc in this function). So, dc is expected to be 1D array
@@ -1333,7 +1349,7 @@ def _proximal_jvp_f_pure(constraint, xf, constants, dc, eq_feasible_tangents, dx
     return vtf.T @ (sfi * (uf.T @ Fc))
 
 
-@functools.partial(jit, static_argnames=["op"])
+@jit_if_possible
 def _proximal_jvp_blocked_pure(objective, vgs, xgs, op):
     # Note: This function is not vectorized and takes the full set of tangents, and
     # returns a matrix.

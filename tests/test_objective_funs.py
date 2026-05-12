ba928070a29602830286720d3ca8e2cdbb84f28b
daniel-dudt <daniel.dudt@princetonstellarators.energy>
2026-05-12 17:07:13 -0400

    actually fix it this time

--- tests/test_objective_funs.py
+++ tests/test_objective_funs.py
@@ -4514,7 +4514,7 @@ def test_deflation_operator_all_Nones():
     # wrapped deflation operator should return the sub-objective value untouched
     np.testing.assert_allclose(
         val2,
-        surf.compute("R", grid=LinearGridToroidalSurface(rho=1.0, theta=0.0, zeta=0.0))[
+        surf.compute("R", grid=LinearGridToroidalSurface(theta=0.0, zeta=0.0))[
             "R"
         ].squeeze(),
     )
